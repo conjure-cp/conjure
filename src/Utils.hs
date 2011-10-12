0de320c7a8b24cd3448af17e5af8127e59a06237
Özgür Akgün <ozgurakgun@gmail.com>
2011-10-12 02:35:16 +0100

    created: Utils.hs

---
+++ src/Utils.hs
@@ -0,0 +1,18 @@
+
+--------------------------------------------------------------------------------
+-- Generic utility functions ---------------------------------------------------
+--------------------------------------------------------------------------------
+
+module Utils where
+
+
+import Test.HUnit ( Test, (~:), (~=?) )
+import qualified Test.QuickCheck as QC ( Testable )
+import qualified Test.QuickCheck.Test as QC ( isSuccess, quickCheckResult )
+
+
+-- given a string and a QuickCheck property, constructs a HUnit test
+quickTest :: QC.Testable prop => String -> prop -> IO Test
+quickTest s p = do
+    testResult <- QC.quickCheckResult p
+    return $ "quickTest: " ++ s ~: True ~=? QC.isSuccess testResult
