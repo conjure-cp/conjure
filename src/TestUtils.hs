469f45fb69b50b31b2c1a71fda0b7c992b698f8a
Özgür Akgün <ozgurakgun@gmail.com>
2011-10-22 23:34:35 +0100

    modified quickTest

--- src/TestUtils.hs
+++ src/TestUtils.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module TestUtils ( quickTest, runTest ) where
 
 
 import Test.HUnit ( Test, (~:), (~=?), runTestTT )
-import qualified Test.QuickCheck as QC ( Testable )
-import qualified Test.QuickCheck.Test as QC ( isSuccess, quickCheckResult )
+import qualified Test.QuickCheck.Arbitrary as QC ( Arbitrary, arbitrary )
+import qualified Test.QuickCheck.Gen as QC ( sample' )
 
 
 -- given a string and a QuickCheck property, constructs a HUnit test
-quickTest :: QC.Testable prop => String -> prop -> IO Test
-quickTest s p = do
-    testResult <- QC.quickCheckResult p
-    return $ "quickTest: " ++ s ~: True ~=? QC.isSuccess testResult
-
+quickTest :: forall a . QC.Arbitrary a => String -> (a -> Bool) -> IO [Test]
+quickTest str f = do
+    samples :: [a] <- QC.sample' QC.arbitrary
+    return [ msg ~: f s ~=? True
+           | (i,s) <- zip [(1::Int)..] samples
+           , let msg = "quickTest: " ++ str ++ "[" ++ show i ++ "]"
+           ]
 
 runTest :: IO Test -> IO ()
 runTest t = print =<< runTestTT =<< t
