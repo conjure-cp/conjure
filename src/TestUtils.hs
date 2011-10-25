a5b115d199921cef057d962280ad491e0560d7b5
Özgür Akgün <ozgurakgun@gmail.com>
2011-10-25 16:22:01 +0100

    for random tests

--- src/TestUtils.hs
+++ src/TestUtils.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module TestUtils ( quickTest, runTest, runTestNTimes ) where
+module TestUtils where
 
 
 import Test.HUnit ( Test, (~:), (~=?), runTestTT, test )
@@ -10,13 +10,21 @@ import qualified Test.QuickCheck.Gen as QC ( sample' )
 
 
 -- given a string and a QuickCheck property, constructs a HUnit test
-quickTest :: forall a . QC.Arbitrary a => String -> (a -> Bool) -> IO [Test]
-quickTest str f = do
+quickTestBool :: forall a . QC.Arbitrary a => String -> (a -> Bool) -> IO [Test]
+quickTestBool str f = do
     samples :: [a] <- QC.sample' QC.arbitrary
     return [ msg ~: f s ~=? True
            | (i,s) <- zip [(1::Int)..] samples
            , let msg = "quickTest: " ++ str ++ "[" ++ show i ++ "]"
            ]
+
+
+-- will use the Arbitrary instance to generate a random value of type a, and generate a sample of tests.
+quickTest :: forall a . QC.Arbitrary a => (a -> Test) -> IO [Test]
+quickTest f = do
+    samples :: [a] <- QC.sample' QC.arbitrary
+    return $ map f samples
+
 
 runTest :: Test -> IO ()
 runTest t = print =<< runTestTT t
