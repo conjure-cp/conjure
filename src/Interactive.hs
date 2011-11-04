dc70e0d5cb1cccced397cbd7ef8df731b2f11722
Özgür Akgün <ozgurakgun@gmail.com>
2011-11-04 14:31:51 +0000

    more intelligent while resolving which action to run

--- src/Interactive.hs
+++ src/Interactive.hs
@@ -5,6 +5,9 @@ import Control.Applicative
 import Control.Monad ( when )
 import Control.Monad.IO.Class ( liftIO )
 import Control.Monad.Trans.State.Lazy ( StateT, evalStateT, gets )
+import Data.Char ( toLower )
+import Data.List ( intercalate, isPrefixOf )
+import Data.Maybe ( fromJust )
 import System.Console.Readline ( addHistory, readline )
 
 import Language.EssenceEvaluator ( runEvaluateExpr )
@@ -13,7 +16,7 @@ import Language.EssenceParsers ( pExpr )
 import Language.EssencePrinters ( prExpr )
 import ParsecUtils ( parseEither, eof )
 import PrintUtils ( render )
-import Utils ( strip )
+import Utils ( ppPrint, strip )
 
 
 data Command = Eval String
@@ -21,7 +24,7 @@ data Command = Eval String
              | Save FilePath
              | Record String        -- might record a declaration, where clause, objective, or constraint
              | RmDeclaration String
-             | RmConstraint Int
+             | RmConstraint String
              | RmObjective
              | Undo
              | Redo
@@ -29,14 +32,29 @@ data Command = Eval String
     deriving (Eq, Ord, Read, Show)
 
 
-parseCommand :: String -> Maybe Command
+parseCommand :: String -> Either String Command
 parseCommand s = do
-    firstWord <- case words s of [] -> Nothing; (i:_) -> Just i
-    case firstWord of
-        ":eval" -> return . Eval . strip . drop (length firstWord) $ s
-        ":quit" -> return Quit
-        ":q"    -> return Quit
-        _       -> Nothing
+    case strip s of
+        (':':ss) -> do
+            firstWord <- case words ss of []    -> Left "Cannot parse command."
+                                          (i:_) -> Right $ map toLower i
+            let restOfLine = strip $ drop (length firstWord) ss
+            let actions = [ ( "evaluate"      , Eval restOfLine          )
+                          , ( "load"          , Load restOfLine          )
+                          , ( "save"          , Save restOfLine          )
+                          , ( "record"        , Record restOfLine        )
+                          , ( "rmdeclaration" , RmDeclaration restOfLine )
+                          , ( "rmconstraint"  , RmConstraint restOfLine  )
+                          , ( "rmobjective"   , RmObjective              )
+                          , ( "undo"          , Undo                     )
+                          , ( "redo"          , Redo                     )
+                          , ( "quit"          , Quit                     )
+                          ]
+            case filter (\ (i,_) -> isPrefixOf firstWord i ) actions of
+                []        -> Left "no such action"
+                [(_,act)] -> Right act
+                xs        -> Left $ "ambigious: " ++ intercalate ", " (map fst xs) ++ "?"
+        line -> Right $ Eval line
 
 
 data REPLState = REPLState { currentSpec :: Spec
@@ -73,13 +91,13 @@ step (Eval s) = do
             case prExpr x' of
                 Nothing  -> liftIO $ putStrLn $ "Error while printing this: " ++ show x'
                 Just doc -> do
-                    liftIO $ print x
-                    liftIO $ print x'
+                    -- liftIO $ ppPrint x
+                    liftIO $ ppPrint x'
                     liftIO $ putStrLn $ render doc
     return True
 step Quit = return False
-step _ = do
-    liftIO $ putStrLn "what?"
+step c = do
+    liftIO $ putStrLn $ "feature not implemented yet: " ++ show c
     return True
 
 
@@ -89,12 +107,12 @@ main = evalStateT repl initREPLState
         repl :: StateT REPLState IO ()
         repl = do
             maybeLine <- liftIO $ readline "# "
-            case (maybeLine, parseCommand =<< maybeLine) of
-                (Nothing  , _           ) -> return () -- EOF / control-d
-                (Just ""  , _           ) -> repl
-                (Just line, Nothing     ) -> do liftIO $ addHistory line
-                                                liftIO $ putStrLn "Cannot parse command."
-                                                repl
-                (Just line, Just command) -> do liftIO $ addHistory line
-                                                c <- step command
-                                                when c repl
+            case (maybeLine, parseCommand (fromJust maybeLine)) of
+                (Nothing  , _            ) -> return () -- EOF / control-d
+                (Just ""  , _            ) -> repl
+                (Just line, Left msg     ) -> do liftIO $ addHistory line
+                                                 liftIO $ putStrLn msg
+                                                 repl
+                (Just line, Right command) -> do liftIO $ addHistory line
+                                                 c <- step command
+                                                 when c repl
