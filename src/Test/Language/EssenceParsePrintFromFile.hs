34e32a4b906bc58bd5e947a10c86bc5327689725
Özgür Akgün <ozgurakgun@gmail.com>
2011-11-22 15:57:25 +0000

    ability to include bindings in unit tests

--- src/Test/Language/EssenceParsePrintFromFile.hs
+++ src/Test/Language/EssenceParsePrintFromFile.hs
@@ -1,16 +1,24 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Test.Language.EssenceParsePrintFromFile where
 
-
-import Data.List.Split
+import Control.Monad ( join )
+import Data.List.Split ( splitOn )
+import Data.Maybe ( maybeToList )
 import Test.HUnit ( Test(..), test )
+import Text.Parsec.Char ( noneOf, string, spaces )
 
+import Language.Essence
+import Language.EssenceParsers
+import ParsecUtils
 import Test.Language.EssenceParsePrintCommon
 import Utils ( strip )
 
 
 -- parses tests from a given String (possibly containing contents of a file)
 -- a single test looks like: "*** Keyword Arg1 [~~ Arg2]"
--- possible keywords are: ShouldParse/1, NoParse/1, ShouldParseTo/2, ParsePrint/2
+-- possible keywords are: ShouldParse/1, NoParse/1, ShouldParseTo/2,
+--                        ParsePrint/2, ParsePrintIso/1, Eval/2, TypeOf/2
 allTests :: String -> (Test, Int)
 allTests file = (test (map toTest ls), length ls)
     where
@@ -30,15 +38,33 @@ allTests file = (test (map toTest ls), length ls)
 
         toTest :: String -> Test
         toTest line = case parseLine line of
-            ("ShouldParse"  , [i]  ) -> cmdShouldParse i
-            ("NoParse"      , [i]  ) -> cmdNoParse i
-            ("ShouldParseTo", [i,j]) -> cmdShouldParseTo i j
-            ("ParsePrint"   , [i,j]) -> cmdParsePrint i j
-            ("ParsePrintIso", [i]  ) -> cmdParsePrintIso i
-            ("Eval"         , [i,j]) -> cmdEval i j
+            ("ShouldParse"  , _bindings, [i]  ) -> cmdShouldParse i
+            ("NoParse"      , _bindings, [i]  ) -> cmdNoParse i
+            ("ShouldParseTo", _bindings, [i,j]) -> cmdShouldParseTo i j
+            ("ParsePrint"   , _bindings, [i,j]) -> cmdParsePrint i j
+            ("ParsePrintIso", _bindings, [i]  ) -> cmdParsePrintIso i
+            ("Eval"         ,  bindings, [i,j]) -> cmdEval bindings i j
+            ("TypeOf"       ,  bindings, [i,j]) -> cmdTypeOf bindings i j
             _     -> error $ "unknown line format: " ++ line
 
-parseLine :: String -> (String,[String])
-parseLine s = case words s of
-    (a:_) -> (a, map strip $ splitOn "~~" $ drop (length a) s )
-    _     -> error $ "unknown line format: " ++ s
+
+parseLine :: String -> (String,[Binding],[String])
+parseLine (strip -> s) = case parseEither pLine s of
+    Left msg        -> error $ "unknown line format: " ++ s ++ "\n\n" ++ msg
+    Right (c,bs,as) -> (c,join (maybeToList bs), as)
+    where
+        pLine :: Parser (String, Maybe [Binding], [String])
+        pLine = do
+            cmd      <- pStrings $ words "ShouldParseTo ShouldParse NoParse ParsePrintIso ParsePrint Eval TypeOf"
+            spaces
+            bindings <- optionMaybe $ braces $ fmap fst $ pTopLevels
+            spaces
+            args     <- pArg `sepBy1` reservedOp "~~"
+            eof
+            return (cmd, bindings, map strip args)
+            where
+                pStrings :: [String] -> Parser String
+                pStrings ss = choiceTry $ map string ss
+
+                pArg :: Parser String
+                pArg = many1 (noneOf "~")
