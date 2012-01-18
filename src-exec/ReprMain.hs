0ddac1ba98a84c3de9a2567475f3639a078b61d9
Özgür Akgün <ozgurakgun@gmail.com>
2012-01-18 11:37:43 +0000

    Output to directory in ReprMain.

--- src-exec/ReprMain.hs
+++ src-exec/ReprMain.hs
@@ -7,28 +7,37 @@ import Control.Monad ( when )
 import Control.Monad.Error ( runErrorT )
 import Data.List ( isSuffixOf )
 import System.Environment ( getArgs )
+import System.FilePath ( dropExtension )
+import System.Directory ( createDirectoryIfMissing )
 
 import Language.EssenceParsers ( pSpec, pRuleRepr )
 import Language.EssencePrinters ( prSpec )
 import ParsecUtils ( parseFromFile )
 import Phases.Repr
 import PrintUtils ( render )
+import Utils ( padLeft )
 
 
 main :: IO ()
 main = do
     args <- getArgs
-    spec <- case filter (".essence" `isSuffixOf`) args of
-                [t] -> parseFromFile pSpec id t id
-                _   -> error "Only 1 *.essence file."
+    specFilename <- case filter (".essence" `isSuffixOf`) args of
+                        [t] -> return t
+                        _   -> error "Only 1 *.essence file."
+    spec  <- parseFromFile pSpec id specFilename id
     reprs <- mapM (\ r -> parseFromFile pRuleRepr id r id ) $ filter (".repr" `isSuffixOf`) args
 
     when (null reprs) $ putStrLn "Warning: no *.repr file is given."
 
     specs <- runErrorT $ applyToSpec reprs spec
 
+    let specBaseFilename = dropExtension specFilename
+
+    createDirectoryIfMissing True specBaseFilename
+
     case specs of
         Left err -> error err
-        Right ss -> mapM_ (putStrLn . render prSpec) ss
-
-    putStrLn "Done."
+        Right ss -> flip mapM_ (zip [(1::Int)..] ss) $ \ (i,s) -> do
+            let outFilename = specBaseFilename ++ "/" ++ padLeft '0' 6 (show i) ++ ".essence"
+            putStrLn $ "Outputting: " ++ outFilename
+            writeFile outFilename $ render prSpec s
