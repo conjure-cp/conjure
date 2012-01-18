961c4ae0248ad7b07499e2c87a2d23fad7f65a5f
Özgür Akgün <ozgurakgun@gmail.com>
2012-01-18 11:29:42 +0000

    line width changed to 160.

--- src/PrintUtils.hs
+++ src/PrintUtils.hs
@@ -13,7 +13,7 @@ render f x = case f x of Nothing -> error ("Cannot render: " ++ show x)
                          Just o  -> renderDoc o
 
 renderDoc :: Doc -> String
-renderDoc = renderStyle style { lineLength = 120 }
+renderDoc = renderStyle style { lineLength = 160 }
 
 wrapIf :: (Doc -> Doc) -> Bool -> Doc -> Doc
 wrapIf wrap c = if c then wrap else id
