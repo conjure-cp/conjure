6ad18cbeee146a64a11a094e1fe2a3502db81ae5
Özgür Akgün <ozgurakgun@gmail.com>
2012-04-01 23:26:04 +0100

    update precedences

--- src/Language/Essence/OpDescriptor.hs
+++ src/Language/Essence/OpDescriptor.hs
@@ -51,19 +51,20 @@ opDescriptor = helper
             | all (\ i -> isLetter i || isNumber i || i == '_' ) s = reserved s <?> "operator"
             | otherwise = reservedOp s <?> "operator"
 
-        genLispy :: Op -> Int -> OpDescriptor
-        genLispy op _cardinality = OpLispy
+        genLispy :: Op -> [Int] -> OpDescriptor
+        genLispy op cards = OpLispy
             ( do
                 reserved (opFace op)
-                -- is <- parens (countSep cardinality parse comma)
                 is <- parens (parse `sepBy1` comma)
-                return $ EOp op is
+                if length is `elem` cards
+                    then fail ("Unexpected number of arguments in " ++ opFace op)
+                    else return $ EOp op is
             )
             ( \ xs -> text (opFace op) <> prettyList Pr.parens Pr.comma xs)
 
         genInfix :: Op -> Int -> Assoc -> OpDescriptor
         genInfix op prec assoc = OpInfix
-            ( prec
+            ( 10000 - prec
             , Infix ( do pFace (opFace op)
                          return $ \ x y -> EOp op [x,y]
                     )
@@ -75,64 +76,67 @@ opDescriptor = helper
                     AssocRight -> Pr.parensIf (envPrec < prec) $ prettyPrec (prec-1) x <+> text (opFace op) <+> prettyPrec  prec    y
             )
 
-        genPrefix :: Op -> Int -> OpDescriptor
-        genPrefix op _prec = OpPrefix
+        genPrefix :: Op -> OpDescriptor
+        genPrefix op = OpPrefix
             ( do pFace (opFace op)
                  return $ \ x -> EOp op [x]
             )
             ( \ x -> text (opFace op) <> Pr.parensIf (not $ isAtomicExpr x) (pretty x) )
 
-        genPostfix :: Op -> Int -> OpDescriptor
-        genPostfix op _prec = OpPostfix
+        genPostfix :: Op -> OpDescriptor
+        genPostfix op = OpPostfix
             ( do pFace (opFace op)
                  return $ \ x -> EOp op [x]
             )
             ( \ x -> Pr.parensIf (not $ isAtomicExpr x) (pretty x) <> text (opFace op) )
 
         helper :: Op -> OpDescriptor
-        helper op@Plus         = genInfix    op   400  AssocLeft
-        helper op@Minus        = genInfix    op   400  AssocLeft
-        helper op@Times        = genInfix    op   300  AssocLeft
-        helper op@Div          = genInfix    op   300  AssocLeft
-        helper op@Mod          = genInfix    op   300  AssocLeft
-        helper op@Pow          = genInfix    op   200  AssocRight
-        helper op@Negate       = genPrefix   op    50
-        helper op@Factorial    = genPostfix  op   100
-        helper op@Lt           = genInfix    op   800  AssocNone
-        helper op@Leq          = genInfix    op   800  AssocNone
-        helper op@Gt           = genInfix    op   800  AssocNone
-        helper op@Geq          = genInfix    op   800  AssocNone
-        helper op@Neq          = genInfix    op   800  AssocNone
-        helper op@Eq           = genInfix    op   800  AssocNone
-        helper op@Not          = genPrefix   op  1300
-        helper op@Or           = genInfix    op  1000  AssocLeft
-        helper op@And          = genInfix    op   900  AssocLeft
-        helper op@Imply        = genInfix    op  1100  AssocNone
-        helper op@Iff          = genInfix    op  1200  AssocNone
-        helper op@Union        = genInfix    op   300  AssocLeft
+        helper op@Plus         = genInfix    op   600  AssocLeft
+        helper op@Minus        = genInfix    op   600  AssocLeft
+        helper op@Times        = genInfix    op   700  AssocLeft
+        helper op@Div          = genInfix    op   700  AssocLeft
+        helper op@Mod          = genInfix    op   700  AssocLeft
+        helper op@Pow          = genInfix    op   800  AssocRight
+        helper op@Negate       = genPrefix   op
+        helper op@Factorial    = genPostfix  op
+        helper op@Lt           = genInfix    op   400  AssocNone
+        helper op@Leq          = genInfix    op   400  AssocNone
+        helper op@Gt           = genInfix    op   400  AssocNone
+        helper op@Geq          = genInfix    op   400  AssocNone
+        helper op@Neq          = genInfix    op   400  AssocNone
+        helper op@Eq           = genInfix    op   400  AssocNone
+        helper op@Not          = genPrefix   op
+        helper op@Or           = genInfix    op   200  AssocRight
+        helper op@And          = genInfix    op   300  AssocRight
+        helper op@Imply        = genInfix    op   100  AssocNone
+        helper op@Iff          = genInfix    op   100  AssocNone
+        helper op@Union        = genInfix    op   200  AssocLeft
         helper op@Intersect    = genInfix    op   300  AssocLeft
-        helper op@Subset       = genInfix    op   700  AssocNone
-        helper op@SubsetEq     = genInfix    op   700  AssocNone
-        helper op@Supset       = genInfix    op   700  AssocNone
-        helper op@SupsetEq     = genInfix    op   700  AssocNone
-        helper op@In           = genInfix    op   700  AssocNone
-        helper op@Max          = genLispy    op     1
-        helper op@Min          = genLispy    op     1
-        helper op@ToSet        = genLispy    op     1
-        helper op@ToMSet       = genLispy    op     1
-        helper op@ToRel        = genLispy    op     1
-        helper op@Defined      = genLispy    op     1
-        helper op@Range        = genLispy    op     1
-        helper op@Image        = genLispy    op     2
-        helper op@PreImage     = genLispy    op     2
-        helper op@Inverse      = genLispy    op     2
-        helper op@Together     = genLispy    op     3
-        helper op@Apart        = genLispy    op     3
-        helper op@Party        = genLispy    op     2
-        helper op@Participants = genLispy    op     1
-        helper op@Parts        = genLispy    op     1
-        helper op@Freq         = genLispy    op     2
-        helper op@Hist         = genLispy    op     2
+        helper op@Subset       = genInfix    op   400  AssocNone
+        helper op@SubsetEq     = genInfix    op   400  AssocNone
+        helper op@Supset       = genInfix    op   400  AssocNone
+        helper op@SupsetEq     = genInfix    op   400  AssocNone
+        helper op@In           = genInfix    op   150  AssocNone
+        helper op@Max          = genLispy    op   [1,2]
+        helper op@Min          = genLispy    op   [1,2]
+        helper op@ToSet        = genLispy    op   [1]
+        helper op@ToMSet       = genLispy    op   [1]
+        helper op@ToRel        = genLispy    op   [1]
+        helper op@Defined      = genLispy    op   [1]
+        helper op@Range        = genLispy    op   [1]
+        helper op@Image        = genLispy    op   [2]
+        helper op@PreImage     = genLispy    op   [2]
+        helper op@Inverse      = genLispy    op   [2]
+        helper op@Together     = genLispy    op   [3]
+        helper op@Apart        = genLispy    op   [3]
+        helper op@Party        = genLispy    op   [2]
+        helper op@Participants = genLispy    op   [1]
+        helper op@Parts        = genLispy    op   [1]
+        helper op@Freq         = genLispy    op   [2]
+        helper op@Hist         = genLispy    op   [2]
+        helper op@HasType      = genInfix    op    10 AssocNone
+        helper op@HasDomain    = genInfix    op    10 AssocNone
+        helper op@AllDiff      = genLispy    op   [1]
         helper TwoBars         = OpSpecial pa pr
             where
                 pa = between (symbol "|") (symbol "|") $ do i <- parse; return $ EOp TwoBars [i]
@@ -160,8 +164,6 @@ opDescriptor = helper
                 pr x = error $ "pretty Index: " ++ show x
                 prettyIndexProject (D (DInt i)) = pretty i
                 prettyIndexProject i = pretty i
-        helper op@HasType   = genInfix op 1500 AssocNone
-        helper op@HasDomain = genInfix op 1500 AssocNone
         helper Replace = OpPostfix pa pr
             where
                 pa = braces $ do
@@ -176,6 +178,5 @@ opDescriptor = helper
                     return $ \ x -> go x ijs
                 pr (EOp Replace [a,b,c]) =
                     (if isAtomicExpr a then id else Pr.parens) (pretty a)
-                    <+> Pr.braces (pretty b <+> "->" <+> pretty c)
+                    <+> Pr.braces (pretty b <+> "-->" <+> pretty c)
                 pr x = error $ "pretty Replace: " ++ show x
-        helper op@AllDiff = genLispy op 1
