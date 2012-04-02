9b3325b038daf0dc9e3f6a0a9696c91207443765
Özgür Akgün <ozgurakgun@gmail.com>
2012-04-02 20:02:16 +0100

    use modified Phases.ReadIn

--- src-exec/RefnMain.hs
+++ src-exec/RefnMain.hs
@@ -1,15 +1,15 @@
 module Main where
 
-import Control.Monad ( forM_, when )
+import Control.Monad ( forM_, when, zipWithM )
 import Control.Monad.Error ( runErrorT )
-import Control.Monad.Writer ( runWriterT )
+import Control.Monad.Writer ( runWriter )
 import Data.List ( isSuffixOf )
 import System.Directory ( createDirectoryIfMissing )
 import System.Environment ( getArgs )
 import System.FilePath ( dropExtension )
 
 import Language.Essence.RuleRefn ( callRefn )
-import Language.Essence.Phases.ReadIn ( readInSpec, readInRefn )
+import Language.Essence.Phases.ReadIn ( readIn )
 
 import ParsePrint ( pretty )
 import Utils ( padLeft )
@@ -25,19 +25,20 @@ main = do
     let refnFilenames = filter (".rule" `isSuffixOf`) args
     when (null refnFilenames) $ putStrLn "Warning: no *.rule file is given."
 
-    (mspecs, logs) <- runWriterT $ runErrorT $ do
-        spec  <- readInSpec specFilename
-        refns <- mapM readInRefn refnFilenames
-        callRefn refns spec
+    specFile  <- readFile specFilename
+    refnFiles <- mapM readFile refnFilenames
+    let (mspecs, logs) = runWriter $ runErrorT $ do
+            spec  <- readIn specFilename specFile
+            refns <- zipWithM readIn refnFilenames refnFiles
+            callRefn refns spec
 
     mapM_ print logs
 
-    let dirName = dropExtension specFilename ++ "-refn"
-    createDirectoryIfMissing True dirName
-
     case mspecs of
         Left err -> error (show err)
-        Right specs ->
+        Right specs -> do
+            let dirName = dropExtension specFilename ++ "-refn"
+            createDirectoryIfMissing True dirName
             forM_ (zip [(1::Int)..] specs) $ \ (i,s) -> do
                 let outFilename = dirName ++ "/" ++ padLeft '0' 6 (show i) ++ ".essence"
                 putStrLn outFilename
--- src-exec/ReprMain.hs
+++ src-exec/ReprMain.hs
@@ -1,15 +1,15 @@
 module Main where
 
-import Control.Monad ( forM_, when )
+import Control.Monad ( forM_, when, zipWithM )
 import Control.Monad.Error ( runErrorT )
-import Control.Monad.Writer ( runWriterT )
+import Control.Monad.Writer ( runWriter )
 import Data.List ( isSuffixOf )
 import System.Directory ( createDirectoryIfMissing )
 import System.Environment ( getArgs )
 import System.FilePath ( dropExtension )
 
 import Language.Essence.RuleRepr ( callRepr )
-import Language.Essence.Phases.ReadIn ( readInSpec, readInRepr )
+import Language.Essence.Phases.ReadIn ( readIn )
 
 import ParsePrint ( pretty )
 import Utils ( padLeft )
@@ -22,22 +22,23 @@ main = do
     specFilename <- case filter (".essence" `isSuffixOf`) args of
                         [t] -> return t
                         _   -> error "Only 1 *.essence file."
-    let reprFilenames = filter (".rule" `isSuffixOf`) args
+    let reprFilenames = filter (".repr" `isSuffixOf`) args
     when (null reprFilenames) $ putStrLn "Warning: no *.rule file is given."
 
-    (mspecs, logs) <- runWriterT $ runErrorT $ do
-        spec  <- readInSpec specFilename
-        reprs <- mapM readInRepr reprFilenames
-        callRepr reprs spec
+    specFile  <- readFile specFilename
+    reprFiles <- mapM readFile reprFilenames
+    let (mspecs, logs) = runWriter $ runErrorT $ do
+            spec  <- readIn specFilename specFile
+            reprs <- zipWithM readIn reprFilenames reprFiles
+            callRepr reprs spec
 
     mapM_ print logs
 
-    let dirName = dropExtension specFilename ++ "-repr"
-    createDirectoryIfMissing True dirName
-
     case mspecs of
         Left err -> error (show err)
-        Right specs ->
+        Right specs -> do
+            let dirName = dropExtension specFilename ++ "-repr"
+            createDirectoryIfMissing True dirName
             forM_ (zip [(1::Int)..] specs) $ \ (i,s) -> do
                 let outFilename = dirName ++ "/" ++ padLeft '0' 6 (show i) ++ ".essence"
                 putStrLn outFilename
