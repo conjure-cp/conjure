b11f75d3bbbd897c363e16237946ff38a6e1b8f0
Özgür Akgün <ozgurakgun@gmail.com>
2012-04-02 13:34:14 +0100

    PostParse, almost a simple renaming of the file EnumIdentifiers.

---
+++ src/Language/Essence/Phases/PostParse.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Language.Essence.Phases.PostParse where
+
+import Control.Monad.Error ( MonadError, throwError )
+import Data.Maybe ( mapMaybe )
+
+import GenericOps.Core ( bottomUp, bottomUpM )
+import ParsePrint ( pretty )
+import PrintUtils ( Doc, nest, vcat )
+
+import Language.Essence.Binding
+import Language.Essence.Domain
+import Language.Essence.Expr
+import Language.Essence.Identifier
+import Language.Essence.Range
+import Language.Essence.Spec
+import Language.Essence.Type
+import Language.Essence.Value
+
+
+
+-- [enumIdentifiers]
+-- an identifier in a spec is always parsed as "EHole (Identifier nm)"
+-- when it refers to an element of an enum type it should be: "V (VEnum nm)"
+
+-- [enumDomains]
+-- a domain identifier is always parsed as "DHole (Identifier nm)"
+-- when it refers to an enum type, it should be: "DEnum (Identifier nm) RAll"
+-- see: enumIdentifiersDom
+
+postParse :: forall m . MonadError Doc m => Spec -> m Spec
+postParse spec = bottomUpM enumIdentifiers (bottomUp enumDomains spec)
+    where
+
+        typeBindings :: ([String],[Type])
+        typeBindings = unzip $ flip mapMaybe (topLevels spec) $ \ b ->
+            case b of
+                Left (LettingType (Identifier i) t) -> Just (i,t)
+                _ -> Nothing
+
+        enumIdentifiers :: Expr -> m Expr
+        enumIdentifiers p@(EHole i) = do
+            let
+                check :: Identifier -> Type -> Maybe Type
+                check j t@(TEnum (Just js)) | j `elem` js = Just t
+                check _ _ = Nothing
+
+                rs = mapMaybe (check i) (snd typeBindings)
+            case rs of
+                [ ] -> return p
+                [_] -> return $ V $ VEnum i
+                _   -> throwError $ vcat $ "Same enum value used in multiple enumerated types: "
+                                         : map (nest 4 . pretty) rs
+        enumIdentifiers p = return p
+
+        enumDomains :: Domain -> Domain
+        enumDomains p@(DHole i@(Identifier nm)) =
+            if nm `elem` fst typeBindings
+                then DEnum i RAll
+                else p
+        enumDomains p = p
