24fbbee261a387dcb05802c74c33697d00938de9
Özgür Akgün <ozgurakgun@gmail.com>
2012-06-11 17:18:11 +0100

    temporarily move the executable to bundle

--- scripts/tocore-upload.sh
+++ scripts/tocore-upload.sh
@@ -1,3 +1,6 @@
 
-cd ~/src/conjure-wd/bundles
+cd ~/src/conjure-wd/bundles/conjure-toCore
+cp ~/.cabal/bin/conjure-toCore .
+upx conjure-toCore
 zip -r ~/Dropbox/Public/conjure-toCore.zip conjure-toCore
+rm -r conjure-toCore
