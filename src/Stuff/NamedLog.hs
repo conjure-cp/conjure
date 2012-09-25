09c21f2704f44103c41ec43697e9633dd74c6203
Özgür Akgün <ozgurakgun@gmail.com>
2012-09-25 20:49:45 +0100

    don't print the same log twice

--- src/Stuff/NamedLog.hs
+++ src/Stuff/NamedLog.hs
@@ -2,12 +2,16 @@
 
 module Stuff.NamedLog where
 
+import Data.Hashable
 import Data.List ( isPrefixOf )
 import Text.PrettyPrint ( Doc, (<+>), vcat, brackets, text )
 
 
 data NamedLog = NamedLog String Doc
     deriving Show
+
+instance Hashable NamedLog where
+    hash (NamedLog a b) = hash (a, show b)
 
 suppress :: [String]
 suppress = [ "restoreState"
@@ -30,4 +34,15 @@ buildLog nm _ | nm `elem` suppress       = Nothing
 buildLog nm doc = Just (NamedLog nm doc)
 
 prettyLogs :: [NamedLog] -> Doc
-prettyLogs = vcat . map (\ (NamedLog nm doc) -> brackets (text nm) <+> doc )
+-- prettyLogs = vcat . map (\ (NamedLog nm doc) -> brackets (text nm) <+> doc )
+prettyLogs = vcat . map (\ (NamedLog nm doc) -> brackets (text nm) <+> doc ) . nubKeepOrder
+
+nubKeepOrder :: Hashable a => [a] -> [a]
+nubKeepOrder = go []
+    where
+        go :: Hashable a => [Int] -> [a] -> [a]
+        go _ [] = []
+        go seen (x:xs) = let hashx = hash x in
+            if hashx `elem` seen
+                then go seen xs
+                else x : go (hashx : seen) xs
