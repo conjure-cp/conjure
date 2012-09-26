fe82d647063653b01b60702e0f472f1f9106d18d
Özgür Akgün <ozgurakgun@gmail.com>
2012-09-26 16:27:05 +0100

    use a set instead of a list

--- src/Stuff/NamedLog.hs
+++ src/Stuff/NamedLog.hs
@@ -5,6 +5,8 @@ module Stuff.NamedLog where
 import Data.Hashable
 import Data.List ( isPrefixOf )
 import Text.PrettyPrint ( Doc, (<+>), vcat, brackets, text )
+import qualified Data.Set as S
+import qualified Data.DList as DList
 
 
 data NamedLog = NamedLog String Doc
@@ -13,29 +15,33 @@ data NamedLog = NamedLog String Doc
 instance Hashable NamedLog where
     hash (NamedLog a b) = hash (a, show b)
 
-suppress :: [String]
-suppress = [ "restoreState"
-           , "patternMatch.core"
-           , "patternMatch"
-           , "rule-fail"
-           , "Simplify"
-           , "Evaluator"
-           , "Evaluator.hasType"
-           , "Evaluator.hasDomain"
-           , "Evaluator.hasRepr"
-           , "Evaluator.domSize"
-           , "Evaluator.indices"
-           , "Evaluator.replace"
-           ]
+suppress :: S.Set String
+suppress = S.fromList
+    [ "restoreState"
+    , "patternMatch.core"
+    , "patternMatch"
+    , "rule-fail"
+    , "Simplify"
+    , "Evaluator"
+    , "Evaluator.hasType"
+    , "Evaluator.hasDomain"
+    , "Evaluator.hasRepr"
+    , "Evaluator.domSize"
+    , "Evaluator.indices"
+    , "Evaluator.replace"
+    ]
 
 buildLog :: String -> Doc -> Maybe NamedLog
 -- buildLog nm _ | "debug:" `isPrefixOf` nm = Nothing
-buildLog nm _ | nm `elem` suppress       = Nothing
+buildLog nm _ | nm `S.member` suppress       = Nothing
 buildLog nm doc = Just (NamedLog nm doc)
 
-prettyLogs :: [NamedLog] -> Doc
--- prettyLogs = vcat . map (\ (NamedLog nm doc) -> brackets (text nm) <+> doc )
-prettyLogs = vcat . map (\ (NamedLog nm doc) -> brackets (text nm) <+> doc ) . nubKeepOrder
+prettyLogs :: DList.DList NamedLog -> Doc
+prettyLogs = id
+    . vcat
+    . map (\ (NamedLog nm doc) -> brackets (text nm) <+> doc )
+    . nubKeepOrder
+    . DList.toList
 
 nubKeepOrder :: Hashable a => [a] -> [a]
 nubKeepOrder = go []
