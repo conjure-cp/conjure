f73991911bcd56c30d646bccaadc9ae2a348ee81
Özgür Akgün <ozgurakgun@gmail.com>
2012-10-02 01:18:50 +0100

    ignore ls errors.

--- run-all2.sh
+++ run-all2.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 
 echo "repr $1"
 ./run-repr.sh $1
-ls $1-repr/*.essence   | parallel "echo 'refn {}' ; ./run-refn.sh {}"
-ls $1-repr/*/*.essence | parallel "./run-all2.sh {}"
+ls $1-repr/*.essence   2> /dev/null | parallel "echo 'refn {}' ; ./run-refn.sh {}"
+ls $1-repr/*/*.essence 2> /dev/null | parallel "./run-all2.sh {}"
