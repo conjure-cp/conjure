f156c576e8a4e7b650b5778af0deea9c268f5d4b
Özgür Akgün <ozgurakgun@gmail.com>
2012-10-06 00:25:25 +0100

    removeUnused before repr.

--- src/Language/E/Pipeline/ConjureRepr.hs
+++ src/Language/E/Pipeline/ConjureRepr.hs
@@ -2,6 +2,7 @@ module Language.E.Pipeline.ConjureRepr where
 
 import Language.E
 import Language.E.Pipeline.ApplyRepr ( applyRepr )
+import Language.E.Pipeline.RemoveUnused ( removeUnused )
 import Language.E.Pipeline.Groom ( groomSpec )
 
 
@@ -12,6 +13,7 @@ conjureRepr :: (Monad m, Functor m)
     -> CompE m Spec
 conjureRepr isFinal spec rules =
     let pipeline = trySimplifySpec      -- to remove any unnecessary occurrences of variables
+                >=> removeUnused        -- and remove the declarations from the model too
                 >=> applyRepr rules
                 >=> (if isFinal then groomSpec else return)
     in  pipeline spec
