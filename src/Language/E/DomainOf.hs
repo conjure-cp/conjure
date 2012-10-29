2880e5bff48f33470d02046522e1c91717b546fb
Özgür Akgün <ozgurakgun@gmail.com>
2012-10-29 15:51:39 +0000

    domainOf for tuple indexing

--- src/Language/E/DomainOf.hs
+++ src/Language/E/DomainOf.hs
@@ -8,6 +8,7 @@ import Language.E.Imports
 import Language.E.Definition
 import Language.E.CompE
 import Language.E.Pretty
+import {-# SOURCE #-} Language.E.Evaluator.ToInt
 
 
 
@@ -15,7 +16,7 @@ errDomainOf :: (Monad m, Pretty p) => p -> CompE m a
 errDomainOf p = err ErrFatal $ "Cannot calculate the domain of" <+> pretty p
 
 
-domainOf :: Monad m => E -> CompE m E
+domainOf :: (Functor m, Monad m) => E -> CompE m E
 
 domainOf [xMatch| [Prim (S i')] := reference |] = do
     let i = head $ splitOn "#" i'
@@ -28,22 +29,31 @@ domainOf [xMatch| [Prim (S i')] := reference |] = do
                     let bsText = sep $ map (\ (Binder nm _) -> stringToDoc nm ) bs
                     err ErrFatal $ "Undefined reference: " <+> pretty i $$ bsText
 
-domainOf [xMatch| [Prim (S i)] := metavar |] = do
+domainOf p@[xMatch| [Prim (S i)] := metavar |] = do
     let j = '&' : i
     bs <- getsLocal binders
     case [ x | Binder nm x <- bs, nm == j ] of
         [x] -> domainOf x
-        _   -> err ErrFatal $ "Undefined reference: " <+> pretty j
+        _   -> return p -- this is for hasDomain pattern matching in rules to work
 
 domainOf [xMatch| [x] := topLevel.declaration.find .domain |] = domainOf x
 domainOf [xMatch| [x] := topLevel.declaration.given.domain |] = domainOf x
 
 domainOf [xMatch| [x] := domainInExpr |] = domainOf x
 
-domainOf p@[xMatch| [x] := operator.index.left |] = do
+domainOf p@[xMatch| [x] := operator.index.left
+                  | [y] := operator.index.right
+                  |] = do
     xDom <- domainOf x
     case xDom of
         [xMatch| [innerDom] := domain.matrix.inner |] -> return innerDom
+        [xMatch|  innerDoms := domain.tuple.inners |] -> do
+            yInt <- toInt y
+            case yInt of
+                Just int
+                    | int >= 1 && int <= genericLength innerDoms
+                    -> return $ innerDoms `genericIndex` (int - 1)
+                _ -> errDomainOf p
         _ -> errDomainOf p
 
 domainOf x = return x
