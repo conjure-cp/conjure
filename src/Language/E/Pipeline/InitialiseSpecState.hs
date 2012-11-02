59ecfa8c8385dc6e59ee58850aaae711f53e0748
Özgür Akgün <ozgurakgun@gmail.com>
2012-11-02 11:50:13 +0000

    move initialisation to Traversals.hs. This module still re-exports the function.

--- src/Language/E/Pipeline/InitialiseSpecState.hs
+++ src/Language/E/Pipeline/InitialiseSpecState.hs
@@ -1,17 +1,4 @@
-{-# LANGUAGE QuasiQuotes, ViewPatterns, OverloadedStrings #-}
+module Language.E.Pipeline.InitialiseSpecState (initialiseSpecState ) where
 
-module Language.E.Pipeline.InitialiseSpecState where
-
-import Language.E
-import qualified Data.Set as S
-
-
-initialiseSpecState :: Monad m => Spec -> CompE m ()
-initialiseSpecState (Spec _ statements) = do
-    let names = [ nm
-                | statement <- statements
-                , [xMatch| [Prim (S nm)] := reference |] <- universe statement
-                ]
-    modifyGlobal $ \ st -> st { allNamesPreConjure = S.fromList names }
-    mapM_ processStatement statements
+import Language.E.Traversals ( initialiseSpecState )
 
