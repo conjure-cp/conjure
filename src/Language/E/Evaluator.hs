5375650780e087eda6c2321d7699960ef39e37a5
Özgür Akgün <ozgurakgun@gmail.com>
2012-11-18 18:40:00 +0000

    MonadConjure change + simplifyIdempotent

--- src/Language/E/Evaluator.hs
+++ src/Language/E/Evaluator.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
-module Language.E.Evaluator ( simplify, trySimplifySpec, trySimplifyE ) where
+module Language.E.Evaluator ( simplify, simplifySpec ) where
 
 import Stuff.Pretty
 
 import Language.E.Imports
 import Language.E.Definition
 import Language.E.CompE
-import Language.E.Helpers
 import Language.E.Traversals
-import Language.E.Parser
 import Language.E.Evaluator.Full    ( fullEvaluator
                                     , evalHasType, evalHasDomain
                                     , evalHasRepr, evalDomSize
@@ -18,92 +17,84 @@ import Language.E.Evaluator.Full    ( fullEvaluator
                                     )
 import Language.E.Evaluator.Partial ( partialEvaluator )
 
-import qualified Data.Text as T
+
+-- _testSimplify :: T.Text -> IO ()
+-- _testSimplify t = do
+    -- let res = runLexerAndParser (inCompleteFile parseExpr) "" t
+    -- case res of
+        -- Left  e -> print e
+        -- Right x -> do
+            -- print $ pretty x
+            -- ys <- runCompEIO $ runWriterT $ simplify x
+            -- mapM_ (print . pretty . fst) ys
 
 
-_testSimplify :: T.Text -> IO ()
-_testSimplify t = do
-    let res = runLexerAndParser (inCompleteFile parseExpr) "" t
-    case res of
-        Left  e -> print e
-        Right x -> do
-            print $ pretty x
-            ys <- runCompEIO $ runWriterT $ simplify x
-            mapM_ (print . pretty . fst) ys
+simplifySpec :: MonadConjure m => Spec -> m Spec
+simplifySpec = liftM fst . runWriterT . bottomUpSpec simplify
 
 
-trySimplifySpec :: (Functor m, Monad m) => Spec -> CompE m Spec
-trySimplifySpec (Spec v xs) = withBindingScope' $ do
-    mapM_ introduceStuff xs
-    let
-        loggedSimplify i = do
-            (i', (Any b, _)) <- listen (simplify i)
-            when b $ lift $ mkLog "simplified" $ vcat [pretty i, "~~>", pretty i']
-            return i'
-    (xs', (Any flag, _)) <- runWriterT (mapM loggedSimplify xs)
-    (if flag then trySimplifySpec else return) (Spec v xs')
-
-trySimplifyE :: (Functor m, Monad m) => E -> CompE m (E, [Binder])
-trySimplifyE x = do
-    (x', (Any flag, bs)) <- runWriterT (simplify x)
-    if flag
-        then do
-            (x'', bs2) <- trySimplifyE x'
-            return (x'', bs ++ bs2)
-        else
-            return (x', bs)
-
-
-simplify :: (Functor m, Monad m) => E -> WriterT (Any, [Binder]) (CompEMOnly m) E
-simplify = traverseE allCombined
+simplify :: MonadConjure m => E -> WriterT (Any, [Binder]) m E
+simplify = bottomUpE allIdempotent
     where
         allCombined i =
             firstJustOr i
-                $ map ($ i) [ loggedFullEvaluator
-                            , loggedEvalHasRepr
-                            , loggedEvalHasType
-                            , loggedEvalHasDomain
-                            , loggedEvalDomSize
-                            , loggedEvalIndices
-                            , loggedEvalReplace
-                            , loggedPartialEvaluator
-                            , logged "Evaluator.tupleEq"  tupleEq
-                            , logged "Evaluator.matrixEq" matrixEq
+                $ map ($ i) [ logged "Evaluator"           fullEvaluator
+                            , logged "Evaluator.hasRepr"   evalHasRepr
+                            , logged "Evaluator.hasType"   evalHasType
+                            , logged "Evaluator.hasDomain" evalHasDomain
+                            , logged "Evaluator.domSize"   evalDomSize
+                            , logged "Evaluator.indices"   evalIndices
+                            , logged "Evaluator.replace"   evalReplace
+                            , logged "Evaluator.tupleEq"   tupleEq
+                            , logged "Evaluator.matrixEq"  matrixEq
+                            , logged "Simplify"            (adapter partialEvaluator)
                             ]
 
-        loggedFullEvaluator    = logged "Evaluator"                 fullEvaluator
-        loggedEvalHasRepr      = logged "Evaluator.hasRepr"         evalHasRepr
-        loggedEvalHasType      = logged "Evaluator.hasType"         evalHasType
-        loggedEvalHasDomain    = logged "Evaluator.hasDomain"       evalHasDomain
-        loggedEvalDomSize      = logged "Evaluator.domSize"         evalDomSize
-        loggedEvalIndices      = logged "Evaluator.indices"         evalIndices
-        loggedEvalReplace      = logged "Evaluator.replace"         evalReplace
-        loggedPartialEvaluator = logged "Simplify"                  (adapter partialEvaluator)
+        allIdempotent i = do
+            (i', (Any flag, _)) <- listen $ allCombined i
+            if flag
+                then do
+                    lift $ mkLog "simplified" $ vcat [pretty i, "~~>", pretty i']
+                    tell (Any True, [])
+                    allIdempotent i'
+                else return i'
 
+        adapter
+            :: MonadConjure m
+            => (E -> m (Maybe E))
+            -> E
+            -> m (Maybe (E, [Binder]))
         adapter comp x = do
             y <- comp x
             case y of
                 Nothing -> return Nothing
                 Just z  -> return (Just (z, []))
 
-        logged :: (Functor m, Monad m)
-               => String
-               -> (E -> CompE m (Maybe (E,[Binder])))
-               -> E
-               -> WriterT (Any, [Binder]) (CompEMOnly m) (Maybe E)
+        logged
+            :: MonadConjure m
+            => String
+            -> (E -> m (Maybe (E,[Binder])))
+            -> E
+            -> WriterT (Any, [Binder]) m (Maybe E)
         logged str act inp = do
             moutp <- lift $ act inp
             case moutp of
                 Nothing         -> return Nothing
                 Just (outp, bs) -> do
-                    lift $ mkLog str $ pretty inp <+> "~~>" <+> pretty outp
+                    lift $ mkLog str $ sep [pretty inp, "~~>", pretty outp]
                     tell (Any True, bs)
                     return (Just outp)
 
-        firstJustOr i []     = return i
-        firstJustOr i (a:as) = do
-            mr <- a
-            case mr of
-                Nothing -> firstJustOr i as
-                Just r  -> return r
+
+firstJustOr
+    :: Monad m
+    => a
+    -> [m (Maybe a)]
+    -> m a
+firstJustOr i []     = return i
+firstJustOr i (a:as) = do
+    mr <- a
+    case mr of
+        Nothing -> firstJustOr i as
+        Just r  -> return r
 
