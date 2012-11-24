73817f9da711707f675bff95b030226565a78953
Özgür Akgün <ozgurakgun@gmail.com>
2012-11-24 16:42:57 +0000

    fixes #46. reorder phases so simplification happens before tuple-explode. test file added.

--- src/Language/E/Pipeline/ConjureRepr.hs
+++ src/Language/E/Pipeline/ConjureRepr.hs
@@ -20,9 +20,9 @@ conjureRepr isFinal spec rules = {-# SCC "conjureRepr" #-} withBindingScope' $ d
     initialiseSpecState spec
     let pipeline =  return
                 >=> recordSpec >=> inlineLettings
+                >=> recordSpec >=> simplifySpec           -- to remove any unnecessary occurrences of variables
                 >=> recordSpec >=> conjureNoTuples
                 >=> recordSpec >=> introduceRegions
-                >=> recordSpec >=> simplifySpec           -- to remove any unnecessary occurrences of variables
                 >=> recordSpec >=> applyRepr rules
                 >=> recordSpec >=> (if isFinal then groomSpec else return)
                 >=> recordSpec
