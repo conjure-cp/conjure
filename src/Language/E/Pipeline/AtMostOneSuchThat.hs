2c40f1c365046bb6f4755d3fc9967817ee616bd4
Özgür Akgün <ozgurakgun@gmail.com>
2012-12-05 14:38:56 +0000

    output a "such that true" if no constraints

--- src/Language/E/Pipeline/AtMostOneSuchThat.hs
+++ src/Language/E/Pipeline/AtMostOneSuchThat.hs
@@ -9,7 +9,7 @@ import Language.E
 
 atMostOneSuchThat :: Spec -> Spec
 atMostOneSuchThat (Spec lang statements)
-    = Spec lang $ listAsStatement $ others ++ toSuchThat suchthats
+    = Spec lang $ listAsStatement $ others ++ [toSuchThat suchthats]
     where
         f [] = ([],[])
         f (x:xs) = case isSuchThat x of
@@ -22,10 +22,15 @@ isSuchThat :: E -> Maybe [E]
 isSuchThat [xMatch| xs := topLevel.suchThat |] = Just xs
 isSuchThat _ = Nothing
 
-toSuchThat :: [E] -> [E]
-toSuchThat [] = []
-toSuchThat xs = [ [xMake| topLevel.suchThat := nub $ concatMap conjunctOut xs |] ]
+toSuchThat :: [E] -> E
+toSuchThat xs =
+    let
+        constraints' = nub $ concatMap conjunctOut xs
+        trueCons = [eMake| true |]
+        constraints = if null constraints' then [trueCons] else constraints'
+    in  [xMake| topLevel.suchThat := constraints |]
     where
         conjunctOut :: E -> [E]
         conjunctOut [eMatch| &a /\ &b |] = conjunctOut a ++ conjunctOut b
         conjunctOut a = [a]
+
