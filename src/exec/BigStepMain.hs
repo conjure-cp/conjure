89abdae14e2eb1333657819414fa1f8599eb03fe
Özgür Akgün <ozgurakgun@gmail.com>
2012-12-21 02:34:37 +0000

    option to use ekg for conjureDF. to see what ekg is, check: http://hackage.haskell.org/package/ekg

--- src/exec/BigStepMain.hs
+++ src/exec/BigStepMain.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -10,12 +12,21 @@ import Language.E.Pipeline.ReadIn
 import Language.E.Pipeline.ConjureAll
 import Language.E.Pipeline.Driver
 
+#ifdef USE_EKG
+import System.Remote.Monitoring
+#endif
+
 
 rulesdbLoc :: IO FilePath
 rulesdbLoc = liftM (++ "/conjure.rulesdb") getBinDir
 
 main :: IO ()
 main = do
+
+#if USE_EKG
+    _ <- forkServer "localhost" 8000
+#endif
+
     args <- getArgs
 
     specFilename <- case filter (".essence" `isSuffixOf`) args of
