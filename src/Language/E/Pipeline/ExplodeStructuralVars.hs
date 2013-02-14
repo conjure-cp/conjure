46949bae21487cd5011eaf3fac94a6b89ebb26aa
Özgür Akgün <ozgurakgun@gmail.com>
2013-02-14 00:44:11 +0000

    handling of nested structural variables

--- src/Language/E/Pipeline/ExplodeStructuralVars.hs
+++ src/Language/E/Pipeline/ExplodeStructuralVars.hs
@@ -11,37 +11,29 @@ explodeStructuralVars :: MonadConjure m => Spec -> m Spec
 explodeStructuralVars = bottomUpSpec' helper
     where
 
-        -- inlining a structural variable, tuple here.
+        helper x@[xMatch| _ := quantified.quanVar.structural.single |] = return x
 
+        -- inlining a structural variable
         helper
             [xMatch| quantifiers   := quantified.quantifier
-                   | ts            := quantified.quanVar.structural.tuple
+                   | [quanVar]     := quantified.quanVar
                    | quanOverDoms  := quantified.quanOverDom
                    | quanOverOps   := quantified.quanOverOp
                    | quanOverExprs := quantified.quanOverExpr
                    | guards        := quantified.guard
                    | bodys         := quantified.body
                    |] = do
-            i' <- nextUniqueName
-            let
-                -- the new quanVar
-                i = [xMake| structural.single.reference := [Prim (S i')] |]
-
-                -- index at position j, an integer.
-                indexIt j' = let j = [xMake| value.literal := [Prim (I j')] |]
-                             in  [eMake| &i[&j] |]
-
-                -- list of indexed quanVars, instead of the input ts
-                indexeds = map indexIt [1 .. genericLength ts]
-
-                replacerFunc = replaceAll [ (old, new)
-                                          | (old, new) <- zip ts indexeds
+            uniq' <- nextUniqueName
+            -- the new quanVar
+            let uniq = [xMake| structural.single.reference := [Prim (S uniq')] |]
+            let mappings = genMappings quanVar
+            let replacerFunc = replaceAll [ (old, new)
+                                          | (old, is) <- mappings
+                                          , let new = mkIndexedExpr (map intToE is) uniq
                                           , old /= [eMake| _ |]
                                           ]
-
-            let
-                result = [xMake| quantified.quantifier   := quantifiers
-                               | quantified.quanVar      := [i]
+            let result = [xMake| quantified.quantifier   := quantifiers
+                               | quantified.quanVar      := [uniq]
                                | quantified.quanOverDom  := quanOverDoms
                                | quantified.quanOverOp   := quanOverOps
                                | quantified.quanOverExpr := quanOverExprs
@@ -51,4 +43,29 @@ explodeStructuralVars = bottomUpSpec' helper
             return result
 
         helper x = return x
+
+
+-- i         --> i -> []
+-- (i,j)     --> i -> [1]
+--               j -> [2]
+-- (i,(j,k)) --> i -> [1]
+--               j -> [2,1]
+--               k -> [2,2]
+genMappings :: E -> [(E,[Integer])]
+genMappings [xMatch| xs  := structural.tuple  |]
+    = concat
+        [ [ (y,i:is) | (y,is) <- genMappings x ]
+        | (i,x) <- zip [1..] xs
+        ]
+genMappings [xMatch| xs  := structural.matrix |]
+    = concat
+        [ [ (y,i:is) | (y,is) <- genMappings x ]
+        | (i,x) <- zip [1..] xs
+        ]
+genMappings x = [(x,[])]
+
+
+intToE :: Integer -> E
+intToE i = [xMake| value.literal := [Prim (I i)] |]
+
 
