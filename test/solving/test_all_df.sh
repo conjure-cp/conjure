de3dbb02fcc91a107d51c8cf3db46d96dad13bff
Özgür Akgün <ozgurakgun@gmail.com>
2013-02-23 21:01:22 +0000

    add a "YVALUE=" to count files, jenkins plotting blah seems to want this.

--- test/solving/test_all_df.sh
+++ test/solving/test_all_df.sh
@@ -37,17 +37,20 @@ export -f perDirectory
 
 find "$SCRIPT_DIR" -name "*.essence" | parallel perDirectory {//}
 
-cat "$FAIL_FILE" | wc -l > "$FAIL_COUNT_FILE"
-cat "$PASS_FILE" | wc -l > "$PASS_COUNT_FILE"
-cat "$ALL_FILE"  | wc -l > "$ALL_COUNT_FILE"
+echo -n "YVALUE=" > "$FAIL_COUNT_FILE"
+echo -n "YVALUE=" > "$PASS_COUNT_FILE"
+echo -n "YVALUE=" > "$ALL_COUNT_FILE"
+cat "$FAIL_FILE" | wc -l >> "$FAIL_COUNT_FILE"
+cat "$PASS_FILE" | wc -l >> "$PASS_COUNT_FILE"
+cat "$ALL_FILE"  | wc -l >> "$ALL_COUNT_FILE"
 
 echo -n "(df) Number of failing tests: "
-cat "$FAIL_COUNT_FILE"
+cat "$FAIL_FILE" | wc -l
 cat "$FAIL_FILE"
 
 echo -n "(df) Number of passing tests: "
-cat "$PASS_FILE"
+cat "$PASS_FILE" | wc -l
 
 echo -n "(df) Number of all tests: "
-cat "$ALL_FILE"
+cat "$ALL_FILE"  | wc -l
 
