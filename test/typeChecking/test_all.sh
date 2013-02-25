c88577053f28859b40f2b0c4e67fe96b23c51402
Özgür Akgün <ozgurakgun@gmail.com>
2013-02-25 14:51:36 +0000

    make it so this script can be run from other directories

--- test/typeChecking/test_all.sh
+++ test/typeChecking/test_all.sh
@@ -3,16 +3,16 @@
 set -o nounset
 set -o errexit
 
-export ROOT_DIR="$(pwd)"
-export FAIL_FILE="$ROOT_DIR/fail.txt"
-export PASS_FILE="$ROOT_DIR/pass.txt"
+export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo $SCRIPT_DIR
+export FAIL_FILE="$SCRIPT_DIR/fail.txt"
+export PASS_FILE="$SCRIPT_DIR/pass.txt"
 
-export FAIL_COUNT_FILE="$ROOT_DIR/countFail.txt"
-export PASS_COUNT_FILE="$ROOT_DIR/countPass.txt"
-export ALL_COUNT_FILE="$ROOT_DIR/countAll.txt"
+export FAIL_COUNT_FILE="$SCRIPT_DIR/countFail.txt"
+export PASS_COUNT_FILE="$SCRIPT_DIR/countPass.txt"
+export ALL_COUNT_FILE="$SCRIPT_DIR/countAll.txt"
 
 function perEssence {
-    WD="$(pwd)"
     SPEC=$1
 
     FLAG1=0
@@ -38,21 +38,21 @@ function perEssence {
     rm -f "$SPEC.pretty"
 
     if (( $FLAG1 != 0 )) ; then
-        echo "[flag1] $PWD/$SPEC.essence" >> "$FAIL_FILE"
+        echo "[flag1] $SPEC.essence" >> "$FAIL_FILE"
     else
-        echo "[flag1] $PWD/$SPEC.essence" >> "$PASS_FILE"
+        echo "[flag1] $SPEC.essence" >> "$PASS_FILE"
     fi
 
     if (( $FLAG2 != 0 )) ; then
-        echo "[flag2] $PWD/$PSEC.essence" >> "$FAIL_FILE"
+        echo "[flag2] $SPEC.essence" >> "$FAIL_FILE"
     else
-        echo "[flag2] $PWD/$PSEC.essence" >> "$PASS_FILE"
+        echo "[flag2] $SPEC.essence" >> "$PASS_FILE"
     fi
 
     if (( $FLAG3 != 0 )) ; then
-        echo "[flag3] $PWD/$PSEC.essence" >> "$FAIL_FILE"
+        echo "[flag3] $SPEC.essence" >> "$FAIL_FILE"
     else
-        echo "[flag3] $PWD/$PSEC.essence" >> "$PASS_FILE"
+        echo "[flag3] $SPEC.essence" >> "$PASS_FILE"
     fi
 }
 
@@ -61,7 +61,7 @@ export -f perEssence;
 rm -f "$FAIL_FILE" "$PASS_FILE"
 touch "$FAIL_FILE" "$PASS_FILE"
 
-parallel perEssence {1.} ::: $(find . -name "*.essence")
+parallel perEssence {1.} ::: $(find "$SCRIPT_DIR" -name "*.essence")
 
 FAIL_COUNT=$(cat "$FAIL_FILE" | wc -l | tr -d ' ')
 PASS_COUNT=$(cat "$PASS_FILE" | wc -l | tr -d ' ')
