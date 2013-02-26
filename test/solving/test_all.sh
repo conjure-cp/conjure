799ba22e4cfb1c5dcbdd0ab9bd3aa65f24175386
Özgür Akgün <ozgurakgun@gmail.com>
2013-02-26 20:33:28 +0000

    separate solving tests into two: fast&slow

--- test/solving/test_all.sh
+++ test/solving/test_all.sh
@@ -3,13 +3,17 @@
 set -o nounset
 set -o errexit
 
-if (( $# == 0 )); then
-    echo "ERROR: Give a list of arguments, each a mode to be used by Conjure."
-    echo "       Options: {df, best, random}"
+
+if (( $# < 2 )); then
+    echo "ERROR:"
+    echo "    - Give a directory path."
+    echo "    - and a list of arguments, each a mode to be used by Conjure."
+    echo "        Modes: {df, best, random}"
     exit 1
 fi
 
-export MODES="$@"
+export WD="$1"
+export MODES="${*:2}"
 
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export SCRIPT_TEST_SINGLE="$SCRIPT_DIR/test_single.sh"
@@ -19,9 +23,9 @@ function perDirectory {
     MODE="$1"
     DIR="$2"
 
-    FAIL_FILE="$SCRIPT_DIR/${MODE}_fail.txt"
-    PASS_FILE="$SCRIPT_DIR/${MODE}_pass.txt"
-    ALL_FILE="$SCRIPT_DIR/${MODE}_all.txt"
+    FAIL_FILE="$WD/${MODE}_fail.txt"
+    PASS_FILE="$WD/${MODE}_pass.txt"
+    ALL_FILE="$WD/${MODE}_all.txt"
 
     cd "$DIR"
     rm -f fail.txt pass.txt
@@ -37,27 +41,27 @@ export -f perDirectory
 
 
 for MODE in $MODES ; do
-    FAIL_FILE="$SCRIPT_DIR/${MODE}_fail.txt"
-    PASS_FILE="$SCRIPT_DIR/${MODE}_pass.txt"
-    ALL_FILE="$SCRIPT_DIR/${MODE}_all.txt"
+    FAIL_FILE="$WD/${MODE}_fail.txt"
+    PASS_FILE="$WD/${MODE}_pass.txt"
+    ALL_FILE="$WD/${MODE}_all.txt"
 
-    FAIL_COUNT_FILE="$SCRIPT_DIR/${MODE}_countFail.txt"
-    PASS_COUNT_FILE="$SCRIPT_DIR/${MODE}_countPass.txt"
-    ALL_COUNT_FILE="$SCRIPT_DIR/${MODE}_countAll.txt"
+    FAIL_COUNT_FILE="$WD/${MODE}_countFail.txt"
+    PASS_COUNT_FILE="$WD/${MODE}_countPass.txt"
+    ALL_COUNT_FILE="$WD/${MODE}_countAll.txt"
 
     rm -f "$FAIL_FILE" "$PASS_FILE" "$ALL_FILE"
 done
 
-parallel --tag perDirectory {1} {2//} ::: $MODES ::: $(find "$SCRIPT_DIR" -name "*.essence")
+parallel --tag perDirectory {1} {2//} ::: $MODES ::: $(find "$WD" -name "*.essence")
 
 for MODE in $MODES ; do
-    FAIL_FILE="$SCRIPT_DIR/${MODE}_fail.txt"
-    PASS_FILE="$SCRIPT_DIR/${MODE}_pass.txt"
-    ALL_FILE="$SCRIPT_DIR/${MODE}_all.txt"
+    FAIL_FILE="$WD/${MODE}_fail.txt"
+    PASS_FILE="$WD/${MODE}_pass.txt"
+    ALL_FILE="$WD/${MODE}_all.txt"
 
-    FAIL_COUNT_FILE="$SCRIPT_DIR/${MODE}_countFail.txt"
-    PASS_COUNT_FILE="$SCRIPT_DIR/${MODE}_countPass.txt"
-    ALL_COUNT_FILE="$SCRIPT_DIR/${MODE}_countAll.txt"
+    FAIL_COUNT_FILE="$WD/${MODE}_countFail.txt"
+    PASS_COUNT_FILE="$WD/${MODE}_countPass.txt"
+    ALL_COUNT_FILE="$WD/${MODE}_countAll.txt"
 
     FAIL_COUNT=$(cat "$FAIL_FILE" | wc -l | tr -d ' ')
     PASS_COUNT=$(cat "$PASS_FILE" | wc -l | tr -d ' ')
