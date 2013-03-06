4380fcbcd6dcd28bb8f95bfe3c8c2cc6c446b341
Özgür Akgün <ozgurakgun@gmail.com>
2013-03-06 17:22:56 +0000

    include repository version in the bug report

--- src/Language/E/Imports.hs
+++ src/Language/E/Imports.hs
@@ -77,6 +77,8 @@ import System.Directory ( getDirectoryContents )
 import System.FilePath ( (</>) )
 import System.CPUTime ( getCPUTime )
 
+import RepositoryVersion ( repositoryVersion )
+
 
 #ifdef TRACELOGS
 import qualified Debug.Trace ( trace )
@@ -208,6 +210,7 @@ bug _message = error $ unlines
     , "Please report a bug."
     , "Conjure is actively maintained, we will get back to you as soon as possible."
     , "You can help us by providing a minimal failing example."
+    , "Also include repository version for this build: " ++ repositoryVersion
     , ""
     , "Issue tracker: http://bitbucket.org/stacs_cp/conjure-public/issues"
 #ifdef TRACELOGS
