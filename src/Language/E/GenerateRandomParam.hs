d705c370a587dbbc1cf06f633fa4b21ca2c5f792
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-03-27 22:52:32 +0000

    This is getting long so split it

--- src/Language/E/GenerateRandomParam.hs
+++ src/Language/E/GenerateRandomParam.hs
@@ -5,11 +5,13 @@
 
 module Language.E.GenerateRandomParam ( generateRandomParam ) where
 
-import Language.E hiding(trace)
+import Language.E
 import Language.E.DomainOf(domainOf)
 import Language.E.Up.Debug(upBug)
 import Language.E.Up.IO(getSpec)
 import Language.E.Up.ReduceSpec(reduceSpec,removeNegatives)
+
+import Language.E.GenerateRandomParam.Data
 
 import Control.Arrow(arr)
 
@@ -19,75 +21,12 @@ import Data.Set (Set)
 
 import qualified Data.Map as M
 import qualified Data.Set as Set
-import qualified Text.PrettyPrint as Pr
 
 import Text.Groom(groom)
 
 
 type Essence      = Spec
 type EssenceParam = Spec
-type Size         = Range
-
--- Data type representing choices
-data Choice =
-     CBool
-   | CInt     Integer [Range]
-   | CTuple  [Choice]
-   | CMatrix [Range]   Choice
-   | CSet     Size     Choice
-   | CRel     Size    [Choice]
-   | CFunc    Size     FAttrs   Choice   Choice
-     deriving (Show,Eq)
-
---TODO MSet partition funcion and where
-
-data FAttrs = FAttrs
-    {fTotal      :: Bool
-    ,fInjective  :: Bool
-    ,fSurjective :: Bool
-    } deriving(Show,Eq)
-
-data PAttrs = PAttrs
-    {pRegular  :: Bool
-    ,pNumParts :: Integer
-    ,pPartSize :: Integer
-    }deriving(Show,Eq)
-
-data Range  =
-    RSingle Integer
-  | RRange  Integer Integer
-    deriving (Show, Eq)
-
--- This assumes no overlapping ranges
-instance Ord Range where
-  (RSingle a ) <= (RSingle b ) = a <= b
-  (RRange a _) <= (RRange c _) = a <= c
-  (RRange _ b) <= (RSingle c ) = b <= c
-  (RSingle a)  <= (RRange b _) = a <= b
-
-instance Pretty Range  where pretty = pretty . show
-instance Pretty Choice where
-    pretty (CBool)           = "CBool"
-    pretty (CInt i rs )      = "CInt{" <> pretty i <> "}"    <+> sep (map pretty rs)
-    pretty (CTuple vs )      = "CTuple" <+> "(" <+>
-                               sep (map (\a -> pretty a <+> " ") vs) <> ")"
-    pretty (CMatrix rs cs)   = "CMatrix" <+> sep (map pretty rs) <+> "[" <+> pretty cs <+> "]"
-    pretty (CSet rs dom)     = "CSet" <+> pretty rs <+> "OF" <+> pretty dom
-    pretty (CRel rs vs)      = "CRel" <+> pretty rs <+> "⟪" <+>
-                               sep (map (\a -> pretty a <+> " ") vs) <> "⟫"
-
-    pretty (CFunc rs attrs from to) = Pr.hang
-                                      ("CFunc" <> pretty attrs  <+> pretty rs)
-                                      8
-                                      (pretty from   <+> "-->" <+> pretty to)
-
-instance Pretty FAttrs where
-    pretty (FAttrs{fTotal=t,fInjective=i,fSurjective=s}) =
-          "{" <> (Pr.hsep . map func) [(t,"Total"),(i,"Injective"),(s,"Surjective")]  <> "}"
-        where
-        func (True, str) = str
-        func (False, _) = Pr.empty
-
 
 _c :: Choice
 _c = CInt 51  [RRange 0 49, RSingle 50 ]
@@ -242,13 +181,12 @@ pickIth index (RRange a b:xs) = pickIth (index - (b - a) - 1 ) xs
 allChoices :: Choice -> [E]
 allChoices (CInt _ rs) = concatMap rangeToE rs
 allChoices (CBool)     = [ [eMake| false |], [eMake| true |] ] 
+
 allChoices (CMatrix rs choice) = 
     map (\p -> [xMake| value.matrix.values := p |] ) perms    
-
     where size    = countRanges rs
           choices = allChoices choice
           perms   = permutationsN size choices
-
 
 rangeToE :: Range -> [E] 
 rangeToE (RSingle i) = [[xMake| value.literal := [Prim (I i)] |]]
