c83c4b50507696500444038c21bb0a6de7d8226c
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-04-07 01:19:17 +0100

    Be a bit smarter, by not rerunning conjure compact  if the generated essence file has NOT been changed (ref #150)

--- src/Language/E/GenerateRandomParam2.hs
+++ src/Language/E/GenerateRandomParam2.hs
@@ -23,41 +23,49 @@ import qualified Data.Text.Lazy    as LT
 import qualified System.Directory  as FP
 import qualified System.FilePath   as FP
 
-
 type EssenceParam = Spec
 type Essence = Spec
 
+
+getSpecMaybe :: FP.FilePath -> IO (Maybe Spec)
+getSpecMaybe filepath  = getSpecMaybe' filepath =<<  FP.doesFileExist filepath
+    where
+    getSpecMaybe' fp True = liftM Just (readSpecFromFile fp)
+    getSpecMaybe' _ False = return Nothing
+
+
 generateParam  :: RulesDB -> Essence -> FP.FilePath -> IO EssenceParam
 generateParam (ruleReprs,ruleRefns) essence intermediateDir  = do
-    let basename        = FP.takeBaseName intermediateDir
-        param_gen       = intermediateDir FP.</> (basename ++ ".essence")
-        param_eprime    = intermediateDir FP.</> (basename ++ ".eprime")
-        param_minion    = intermediateDir FP.</> (basename ++ ".eprime.minion")
-        param_esolution = intermediateDir FP.</> (basename ++ ".eprime.solution")
 
-    putStrLn $ "Creating the intermediate directory: " ++ intermediateDir 
+    paramEssenceOld <- getSpecMaybe param_gen
+
+    putStrLn $ "Creating the intermediate directory(if needed): " ++ intermediateDir
     FP.createDirectoryIfMissing True intermediateDir
 
-    putStrLn $ "Creating Essence specification of the param"
+    putStrLn  "Creating Essence specification of the param"
     driverConjureSingle False
         param_gen
         $ runCompE "generateParamSolve" (prepareParamSpecification essence)
 
     paramEssence <- readSpecFromFile param_gen
-    seed <- getStdGen
 
-    putStrLn "Running Conjure compact on created specification\n"
-    driverConjureSingle True
-        param_eprime
-        (conjureWithMode
-            S.empty seed Nothing (ModeSingleOutput ModeCompact param_gen param_eprime)
-            ruleReprs ruleRefns paramEssence)
+    let runConjure (Just oldSpec) | oldSpec == paramEssence  = 
+            putStrLn "NOT Running Conjure compact on created specification\n" 
 
+        runConjure _ = do
+            seed <- getStdGen
+            putStrLn "Running Conjure compact on created specification\n"
+            driverConjureSingle True
+                param_eprime
+                (conjureWithMode
+                    S.empty seed Nothing (ModeSingleOutput ModeCompact param_gen param_eprime)
+                    ruleReprs ruleRefns paramEssence)
+
+    _ <- runConjure paramEssenceOld
 
     _ <- shelly $ verbosely $ do
         echo "Running Savilerow"
         _ <- savilerow (LT.pack param_eprime) (LT.pack param_minion) (LT.pack param_esolution) Nothing
-
         return ()
 
     putStrLn "\nRunning translateSolution"
@@ -67,6 +75,14 @@ generateParam (ruleReprs,ruleRefns) essence intermediateDir  = do
         param_eprime
         Nothing
         param_esolution
+
+    where
+    basename        = FP.takeBaseName intermediateDir
+    param_gen       = intermediateDir FP.</> (basename ++ ".essence")
+    param_eprime    = intermediateDir FP.</> (basename ++ ".eprime")
+    param_minion    = intermediateDir FP.</> (basename ++ ".eprime.minion")
+    param_esolution = intermediateDir FP.</> (basename ++ ".eprime.solution")
+
 
 savilerow :: LT.Text -> LT.Text -> LT.Text -> Maybe LT.Text -> Sh LT.Text
 savilerow in_eprime out_minion out_solution in_param= run
@@ -82,8 +98,10 @@ savilerow in_eprime out_minion out_solution in_param= run
     handleParam Nothing = []
     handleParam (Just param) = ["-in-param",param]
 
+
 _test :: IO EssenceParam
 _test = do
     db <- decodeFromFile "/Users/bilalh/.cabal/bin/conjure.rulesdb"
     sp <- readSpecFromFile "/Users/bilalh/CS/conjure/test/generateParams/set-all.essence"
     generateParam db sp "intermediate"
+
