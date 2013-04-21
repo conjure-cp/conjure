25b4c1d8976ef1ebb5ebefed24eb9afa7cdd9642
Özgür Akgün <ozgurakgun@gmail.com>
2013-04-21 22:53:37 +0100

    this new rule application order is quite a bit slower. it produces better models though. don't make it the default yet. it is available via a flag: "--slower".

--- src/Language/E/Pipeline/ApplyRefnSlower.hs
+++ src/Language/E/Pipeline/ApplyRefnSlower.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE QuasiQuotes, ViewPatterns, OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Language.E.Pipeline.ApplyRefn ( applyRefn, applyRefnE ) where
+module Language.E.Pipeline.ApplyRefnSlower ( applyRefn, applyRefnE ) where
 
 import Conjure.Mode
 
 import Language.E
 import Language.E.BuiltIn
-import qualified Language.E.Pipeline.ApplyRefnSlower as Slower ( applyRefn, applyRefnE )
 
-import qualified Data.HashSet as S
 import qualified Text.PrettyPrint as Pr
 
 
@@ -21,25 +20,20 @@ applyRefn
     => RuleRefnDB m
     -> Spec
     -> m Spec
-applyRefn db' spec = do
-    flags <- getsGlobal conjureFlags
-    if "--slower" `S.member` flags
-        then Slower.applyRefn db' spec
-        else withBindingScope' $ do
-            let db = db' ++ builtInRefn
-            (spec', _) <- runWriterT $ onSpec db spec
-            return spec'
+applyRefn db' spec = withBindingScope' $ do
+    let db = db' ++ builtInRefn
+    (spec', _) <- runWriterT $ onSpec db spec
+    return spec'
 
 applyRefnE
     :: MonadConjureList m
     => RuleRefnDB m
     -> E
     -> m E
-applyRefnE db' x = do
-    flags <- getsGlobal conjureFlags
-    if "--slower" `S.member` flags
-        then Slower.applyRefnE db' x
-        else return x
+applyRefnE db' spec = withBindingScope' $ do
+    let db = db' ++ builtInRefn
+    (spec', _) <- runWriterT $ onE db spec
+    return spec'
 
 
 {-# INLINEABLE onSpec #-}
@@ -54,26 +48,41 @@ onSpec db (Spec lang statements) = Spec lang <$> onE db statements
 
 {-# INLINEABLE onE #-}
 onE
-    :: MonadConjureList m
+    :: forall m
+    .  MonadConjureList m
     => RuleRefnDB m
     -> E
     -> WriterT Any m E
 -- onE _ x | trace (show $ "onE" <+> pretty x) False = undefined
-onE = applyToTree
-
+onE db i = do
+    (j, flag) <- lift $ go 0 dbLevels i
+    tell (Any flag)
+    return j
+    where
+        dbLevels = map return db
+        go :: Int -> [RuleRefnDB m] -> E -> m (E, Bool)
+        go _ [] x = return (x, False)
+        go levelInt (dbLevel:rest) x = do
+            (x', Any flag) <- runWriterT $ applyToTree levelInt dbLevel x
+            if flag
+                then do
+                    (y, _) <- go 0 dbLevels x'
+                    return (y, True)
+                else go (levelInt + 1) rest x'
 
 
 {-# INLINEABLE applyIdempotent #-}
 applyIdempotent
     :: MonadConjureList m
-    => RuleRefnDB m
+    => Int
+    -> RuleRefnDB m
     -> E
     -> WriterT Any m E
--- applyIdempotent _  x | trace (show $ "applyIdempotent" <+> pretty x) False = undefined
-applyIdempotent db x = do
+-- applyIdempotent _ _ x | trace (show $ "applyIdempotent" <+> pretty x) False = undefined
+applyIdempotent level db x = do
     (y, Any flag) <- listen $ apply db x
     if flag
-        then applyToTree db y
+        then applyToTree level db y
         else return x
 
 
@@ -81,11 +90,15 @@ applyIdempotent db x = do
 {-# INLINEABLE applyToTree #-}
 applyToTree
     :: MonadConjureList m
-    => RuleRefnDB m
+    => Int
+    -> RuleRefnDB m
     -> E
     -> WriterT Any m E
--- applyToTree _  x | trace (show $ "applyToTree" <+> pretty x) False = undefined
-applyToTree db = bottomUpERefn 0 (applyIdempotent db)
+-- applyToTree _ _ x | trace (show $ "applyToTree" <+> pretty x) False = undefined
+applyToTree level db i = do
+    (j, Any flag) <- lift $ runWriterT $ bottomUpERefn level (applyIdempotent level db) i
+    tell (Any flag)
+    return j
 
 
 
@@ -155,4 +168,5 @@ tryApply db mode x = do
                                  ]
                     mkLog "applied" msg
                     return (map snd ys', True)
+
 
