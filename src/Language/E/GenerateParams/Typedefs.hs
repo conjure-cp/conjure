d6d0a6135eeb74a5a076e16e0db477bafb3763b8
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-06-16 00:05:07 +0100

    keep the typedefs in one place

---
+++ src/Language/E/GenerateParams/Typedefs.hs
@@ -0,0 +1,18 @@
+module Language.E.GenerateParams.Typedefs where
+
+import Language.E
+
+type Eprime          = Spec
+type EprimeParam     = Spec
+type EprimeSolution  = Spec
+type Essence         = Spec
+type EssenceParam    = Spec
+type EssenceSolution = Spec
+
+
+type EprimeFP          = FilePath
+type EprimeParamFP     = FilePath
+type EprimeSolutionFP  = FilePath
+type EssenceFP         = FilePath
+type EssenceParamFP    = FilePath
+type EssenceSolutionFP = FilePath
