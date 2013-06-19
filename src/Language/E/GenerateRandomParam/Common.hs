b239315e848bf23e5595e5b2c036e1a81f25dcde
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-06-19 03:57:49 +0100

    printing

--- src/Language/E/GenerateRandomParam/Common.hs
+++ src/Language/E/GenerateRandomParam/Common.hs
@@ -27,8 +27,8 @@ countRange (RSingle _ ) = 1
 countRange (RRange a b) =  b - a + 1
 
 printPretty :: Pretty a => String ->  a -> IO ()
-printPretty s p = putStrLn $ s ++ '\n' : (show . pretty) p
+printPretty s p = putStrLn $ 'ː' :s ++ 'ː' : '\n' : (renderNormal) p
 
 printPrettym :: Pretty a => String -> [a] -> IO ()
-printPrettym s arr= putStrLn  $ s ++ '\n' :  (show . pretty . vcat . map pretty) arr
+printPrettym s arr= putStrLn  $'ː' : s ++  'ː' : '\n' :  (renderNormal . vcat . map pretty) arr
 
