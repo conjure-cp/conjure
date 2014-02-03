366b9b018a3c0aa74f2e2e2473dd66b865455ff4
Özgür Akgün <ozgurakgun@gmail.com>
2014-02-03 14:14:30 +0000

    a separate jenkins install script, for multi-config builds

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -3,109 +3,23 @@
 # This script will install ghc + cabal + happy + conjure.
 # It will only install these tools if they aren't already installed.
 #
-# Everything is installed locally. Except ~/.ghc and ~/.cabal.
-# These two directories are used by ghc and cabal for storing some metadata
-# and data about installed packages.
-#
+# It uses a cabal-sanbox, so dependencies are installed locally.
+# GHC will be installed under ~/.tools/ghc/${GHC_VERSION}
+# cabal-install'ed executables will be under ~/.cabal/bin as usual.
+# 
 # To clean an installation:
-#     rm -rf dist ~/.ghc ~/.cabal
+#     rm -rf dist ~/.cabal-sandbox cabal.sandbox.config
 
 
 set -o errexit
 set -o nounset
 
-OS=$(uname)
+export JOBS="$1"
+export GHC_VERSION="7.6.3"
+export OPTIMISED="optimised"
+export LLVM="llvm-off"
 
-if [ "$OS" == "Darwin" ]; then
-    PLATFORM="apple-darwin"
-elif [ "$OS" == "Linux" ]; then
-    PLATFORM="unknown-linux"
-else
-    echo "Cannot determine your OS, uname reports: ${OS}"
-    exit 1
-fi
+# where is this script?
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-JOBS="0"
-if [ $# -eq 1 ]; then
-    JOBS="$1"
-fi
-
-if [ $JOBS -eq 0 ]; then
-    CORES=$( (grep -c ^processor /proc/cpuinfo 2> /dev/null) || (sysctl hw.logicalcpu | awk '{print $2}' 2> /dev/null) || 0 )
-    if [ $CORES -eq 0 ]; then
-        echo "Cannot tell how many cores the machine has. Will use only 1."
-    else
-        echo "This machine seems to have $CORES cores. Will use all."
-        JOBS=$CORES
-    fi
-fi
-
-WD="$(pwd)"
-mkdir -p dist/tools
-cd dist/tools
-
-export PATH="$WD/dist/tools/ghc-7.6.3-build/bin":$PATH
-export PATH="$HOME/.cabal/bin":$PATH
-
-HAS_LLVM="$(opt -version 2> /dev/null | grep -i llvm | wc -l | tr -d ' ')"
-
-
-# installing ghc
-if [ "$(ghc --version)" != "The Glorious Glasgow Haskell Compilation System, version 7.6.3" ]; then
-    echo "Installing GHC"
-    wget -c "http://www.haskell.org/ghc/dist/7.6.3/ghc-7.6.3-x86_64-${PLATFORM}.tar.bz2"
-    tar xvjf "ghc-7.6.3-x86_64-${PLATFORM}.tar.bz2"
-    cd ghc-7.6.3
-    ./configure --prefix="$WD/dist/tools/ghc-7.6.3-build"
-    make install
-    cd ..
-    rm -rf "ghc-7.6.3-x86_64-${PLATFORM}.tar.bz2" ghc-7.6.3
-fi
-
-
-# installing cabal-install
-if [ "$(cabal --version | head -n 1)" != "cabal-install version 1.18.0.2" ]; then
-    echo "Installing cabal-install"
-    wget -c http://hackage.haskell.org/packages/archive/cabal-install/1.18.0.2/cabal-install-1.18.0.2.tar.gz
-    tar -zxvf cabal-install-1.18.0.2.tar.gz
-    cd cabal-install-1.18.0.2
-    bash bootstrap.sh
-    cd ..
-    rm -rf cabal-install-1.18.0.2.tar.gz cabal-install-1.18.0.2
-fi
-
-cd "$WD"
-cabal update
-
-# installing happy
-HAS_HAPPY="$(which happy 2> /dev/null > /dev/null ; echo $?)" 
-if [ "$HAS_HAPPY" != 0 ] ; then
-    echo "Installing happy"
-    mkdir -p dist/tools
-    pushd dist/tools
-    cabal install happy -O2 \
-        --force-reinstalls \
-        --disable-documentation \
-        --disable-library-profiling \
-        --disable-executable-profiling
-    popd
-fi
-
-ghc --version
-cabal --version
-happy --version
-
-VERSION=$(hg id -i | head -n 1)
-echo "module RepositoryVersion where"       >  src/RepositoryVersion.hs
-echo "repositoryVersion :: String"          >> src/RepositoryVersion.hs
-echo "repositoryVersion = \"${VERSION}\""   >> src/RepositoryVersion.hs
-
-# install conjure, finally
-if (( "$HAS_LLVM" > 0 )) && [ "$OS" = "Linux" ] ; then
-    echo "Using LLVM"
-    scripts/build/make -Ol "-j${JOBS}"
-else
-    echo "Not using LLVM"
-    scripts/build/make -O  "-j${JOBS}"
-fi
-
+bash "${SCRIPT_DIR}/install_jenkins.sh"
---
+++ scripts/build/install_jenkins.sh
@@ -0,0 +1,125 @@
+#/bin/bash
+
+# Use environment variables to configure this install script.
+# JOBS:         number of parallel jobs to use while compiling
+# GHC_VERSION:  version of ghc to use. the script will download and install ghc under ~/.tools/ghc if not installed.
+# OPTIMISED:    set to "optimised" if you want an optimised build
+#               set to "non-optimised" if you don't
+# LLVM:         "llvm-on" / "llvm-off"
+
+set -o errexit
+set -o nounset
+
+OS=$(uname)
+
+if [ "$OS" == "Darwin" ]; then
+    PLATFORM="apple-darwin"
+elif [ "$OS" == "Linux" ]; then
+    PLATFORM="unknown-linux"
+else
+    echo "Cannot determine your OS, uname reports: ${OS}"
+    exit 1
+fi
+
+JOBS=${JOBS:-0}
+
+if [ $JOBS -le 0 ]; then
+    echo "You didn't set JOBS to anything. Will try to use all CORES this machine has."
+    CORES=$( (grep -c ^processor /proc/cpuinfo 2> /dev/null) || (sysctl hw.logicalcpu | awk '{print $2}' 2> /dev/null) || 0 )
+    if [ $CORES -eq 0 ]; then
+        echo "Cannot tell how many cores the machine has. Will use only 1."
+    else
+        echo "This machine seems to have $CORES cores. Will use all."
+        JOBS=$CORES
+    fi
+else
+    CORES="${JOBS}"
+    echo "Using ${CORES} cores."
+fi
+
+CABAL_VERSION="1.18.0.2"
+
+
+echo "JOBS          : ${JOBS}"
+echo "CORES         : ${CORES}"
+echo "GHC_VERSION   : ${GHC_VERSION}"
+echo "OPTIMISED     : ${OPTIMISED}"
+echo "LLVM          : ${LLVM}"
+echo "CABAL_VERSION : ${CABAL_VERSION}"
+
+
+export PATH="${HOME}/.tools/ghc/${GHC_VERSION}/bin":$PATH
+export PATH="${HOME}/.cabal/bin":$PATH
+
+
+# installing ghc
+if [ "$(ghc --version | grep $GHC_VERSION)" ]; then
+    echo "GHC version ${GHC_VERSION} found."
+    which ghc
+    ghc --version
+else
+    echo "GHC version ${GHC_VERSION} not found. Installing."
+    wget -c "http://www.haskell.org/ghc/dist/${GHC_VERSION}/ghc-${GHC_VERSION}-x86_64-${PLATFORM}.tar.bz2"
+    tar xvjf "ghc-${GHC_VERSION}-x86_64-${PLATFORM}.tar.bz2"
+    pushd "ghc-${GHC_VERSION}"
+    mkdir -p "${HOME}/.tools/ghc"
+    ./configure --prefix="${HOME}/.tools/ghc/${GHC_VERSION}"
+    make install
+    popd ..
+    rm -rf "ghc-${GHC_VERSION}-x86_64-${PLATFORM}.tar.bz2" "ghc-${GHC_VERSION}"
+fi
+
+
+# installing cabal-install
+if [ "$(cabal --version | head -n 1 | grep ${CABAL_VERSION})" ]; then
+    echo "cabal install version ${CABAL_VERSION} found."
+else
+    echo "cabal install version ${GHC_VERSION} not found. Installing."
+    wget -c "http://hackage.haskell.org/packages/archive/cabal-install/${CABAL_VERSION}/cabal-install-${CABAL_VERSION}.tar.gz"
+    tar -zxvf "cabal-install-${CABAL_VERSION}.tar.gz"
+    pushd "cabal-install-${CABAL_VERSION}"
+    bash bootstrap.sh
+    popd ..
+    rm -rf "cabal-install-${CABAL_VERSION}.tar.gz" "cabal-install-${CABAL_VERSION}"
+fi
+
+cabal update
+
+# installing happy
+HAS_HAPPY="$(which happy 2> /dev/null > /dev/null ; echo $?)" 
+if [ "$HAS_HAPPY" != 0 ] ; then
+    echo "Installing happy"
+    mkdir -p dist/tools
+    pushd dist/tools
+    cabal install happy -O2 \
+        --force-reinstalls \
+        --disable-documentation \
+        --disable-library-profiling \
+        --disable-executable-profiling
+    popd
+fi
+
+ghc   --version
+cabal --version
+happy --version
+
+VERSION=$(hg id -i | head -n 1)
+echo "module RepositoryVersion where"       >  src/RepositoryVersion.hs
+echo "repositoryVersion :: String"          >> src/RepositoryVersion.hs
+echo "repositoryVersion = \"${VERSION}\""   >> src/RepositoryVersion.hs
+
+# install conjure, finally
+if   [ $LLVM = "llvm-on"  ] && [ $OPTIMISED = "optimised"     ] ; then
+    scripts/build/make -Ol "-j${JOBS}"
+elif [ $LLVM = "llvm-on"  ] && [ $OPTIMISED = "non-optimised" ] ; then
+    scripts/build/make -l "-j${JOBS}"
+elif [ $LLVM = "llvm-off" ] && [ $OPTIMISED = "optimised"     ] ; then
+    scripts/build/make -O "-j${JOBS}"
+elif [ $LLVM = "llvm-off" ] && [ $OPTIMISED = "non-optimised" ] ; then
+    scripts/build/make "-j${JOBS}"
+else
+    echo "Something is wrong here."
+    echo "LLVM: ${LLVM}"
+    echo "OPTIMISED: ${OPTIMISED}"
+fi
+
