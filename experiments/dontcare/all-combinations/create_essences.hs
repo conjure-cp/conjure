a1127989f32e4757081a82377d68ef06cde1820b
Özgür Akgün <ozgurakgun@gmail.com>
2014-03-05 15:27:40 +0000

    ability to create all combinations of abstract domains for a given number of nesting levels

--- experiments/dontcare/all-combinations/create_essences.hs
+++ experiments/dontcare/all-combinations/create_essences.hs
@@ -1,7 +1,10 @@
+module Main where
 
+import Control.Monad ( replicateM )
 import Data.List ( intercalate )
 import System.Directory ( createDirectoryIfMissing )
 import System.Environment ( getArgs )
+
 
 intDomain :: Int -> String
 intDomain maxSize = "int(1.." ++ show maxSize ++ ")"
@@ -17,19 +20,26 @@ domains maxSize =
 
 main :: IO ()
 main = do
-    (n:_) <- getArgs
-    let maxSize = read n
-    sequence_
-        [ do
-            createDirectoryIfMissing True dirname
-            writeFile filename essence
-            putStrLn $ "Created " ++ filename
-        | (n1,d1) <- domains maxSize
-        , (n2,d2) <- domains maxSize
-        , let dirname  = n1 ++ "-" ++ n2
-        , let filename = dirname ++ "/" ++ dirname ++ ".essence"
-        , let essence  = unlines [ "language Essence 1.3"
-                                 , "find x : " ++ d1 (d2 (intDomain maxSize))
-                                 ]
-        ]
+    (level:maxSize:_) <- getArgs
+    create (read level) (read maxSize)
+
+create :: Int -- level of nesting (0 for int, at least 1 for abstract domains)
+       -> Int -- maxSize
+       -> IO ()
+create level maxSize = sequence_
+    [ do createDirectoryIfMissing True dirname
+         writeFile filename essence
+         putStrLn $ "Created " ++ filename
+    | (ns, ds) <- map unzip $ replicateM level $ domains maxSize
+    , let dirname  = intercalate "-" (("nesting" ++ show level) : ns)
+    , let filename = dirname ++ "/" ++ dirname ++ ".essence"
+    , let essence  = unlines
+            [ "language Essence 1.3"
+            , "find x : " ++ chainapp ds (intDomain maxSize)
+            ]
+    ]
+
+chainapp :: [a -> a] -> a -> a
+chainapp [] a = a
+chainapp (f:fs) a = f (chainapp fs a)
 
