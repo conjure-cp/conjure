32e195e6385beb94de07ffbdceb91f20804aa8fb
Özgür Akgün <ozgurakgun@gmail.com>
2014-03-19 18:57:05 +0000

    a recompute function

--- experiments/type-strengthening/recompute.sh
+++ experiments/type-strengthening/recompute.sh
@@ -8,7 +8,7 @@ export -f list_all_essence
 function do_ts1() {
     ESSENCE="$1"
     conjure --mode typeStrengthening --in "${ESSENCE}" > "${ESSENCE}.typeStrengthening"
-    conjure --mode diff "${ESSENCE}.typeStrengthening" "${ESSENCE}.expected"
+    conjure --mode diff "${ESSENCE}.typeStrengthening" "${ESSENCE}.expected" 2> /dev/null
     if [ $? -eq 0 ] ; then
         echo "pass ${ESSENCE}"
     else
@@ -18,12 +18,18 @@ function do_ts1() {
 export -f do_ts1
 
 function do_ts() {
-    parallel --no-notice do_ts1 ::: $(list_all_essence)
+    parallel --no-notice -k do_ts1 ::: $(list_all_essence)
 }
 export -f do_ts
 
 function rm_output_files() {
-    find . -name "*.typeStrengthening"
     find . -name "*.typeStrengthening" -delete
 }
 export -f do_ts
+
+function recompute() {
+    do_ts
+    rm_output_files
+}
+export -f recompute
+
