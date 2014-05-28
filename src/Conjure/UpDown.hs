946f81e7220b7f8133cd9ef6f89d5b0ac4d86b29
Özgür Akgün <ozgurakgun@gmail.com>
2014-05-28 16:48:31 +0200

    updown can sometimes produces list of things now. especially for tuples.

--- src/Conjure/UpDown.hs
+++ src/Conjure/UpDown.hs
@@ -9,7 +9,7 @@ import Language.E.Pretty
 
 import Data.Generics.Uniplate.Data ( transformBiM )
 import Data.List ( findIndex )
-import Safe ( atMay )
+import Safe ( atMay, headNote )
 
 
 data UpDownError
@@ -27,22 +27,26 @@ instance Eq UpDownError where
     _ == _ = False
 
 
-downDomain :: MonadError UpDownError m => Representation -> Domain -> m Domain
+downDomain :: MonadError UpDownError m => Representation -> Domain -> m [Domain]
 downDomain representation domain = do (gen,_,_) <- upDown representation domain ; gen
 
-downValue :: MonadError UpDownError m => Representation -> Domain -> Value -> m Value
+downValue :: MonadError UpDownError m => Representation -> Domain -> Value -> m [Value]
 downValue representation domain value = do (_,gen,_) <- upDown representation domain ; gen value
 
-upValue :: MonadError UpDownError m => Representation -> Domain -> Value -> m Value
-upValue representation domain value = do (_,_,gen) <- upDown representation domain ; gen value
+upValue :: MonadError UpDownError m => Representation -> Domain -> [Value] -> m Value
+upValue representation domain values = do (_,_,gen) <- upDown representation domain ; gen values
 
+-- | This is about one level.
+--   Describes how, for a representation, we can translate a given high level domain into a low level domain.
+--   And how, for that representation and a domain, we can translate a given value of the high level domain into a value of the low level domain.
+--   And how, for that representation and a domain, we can translate a given value of the low level domain into a value of the high level domain.
 upDown
     :: MonadError UpDownError m
     => Representation
     -> Domain
-    -> m ( m Domain                 -- the low level domain
-         , Value -> m Value         -- value down
-         , Value -> m Value         -- value up
+    -> m ( m [Domain]                 -- the low level domain
+         , Value -> m [Value]         -- value down
+         , [Value] -> m Value         -- value up
          )
 upDown NoRepresentation d@(DomainBool   {}) = upDownNoOp d
 upDown NoRepresentation d@(DomainInt    {}) = upDownNoOp d
@@ -63,11 +67,19 @@ upDown representation domain =
                                                 , pretty representation
                                                 ]
 
-upDownNoOp :: MonadError UpDownError m => Domain -> m (m Domain, Value -> m Value, Value -> m Value)
-upDownNoOp d = return (return d, \ v -> return v, \ v -> return v)
+upDownNoOp :: MonadError UpDownError m => Domain -> m (m [Domain], Value -> m [Value], [Value] -> m Value)
+upDownNoOp d =
+    return ( return [d]
+           , return . singletonList
+           , return . headNote "Conjure.UpDown.upDownNoOp"
+           )
 
-upDownEnum :: MonadError UpDownError m => Domain -> m (m Domain, Value -> m Value, Value -> m Value)
-upDownEnum (DomainEnum (DomainDefnEnum _name values) ranges) = return (domainOut, down, up)
+upDownEnum :: MonadError UpDownError m => Domain -> m (m [Domain], Value -> m [Value], [Value] -> m Value)
+upDownEnum (DomainEnum (DomainDefnEnum _name values) ranges) =
+    return ( liftM singletonList domainOut
+           , (liftM . liftM) singletonList down
+           , up . headNote "Conjure.UpDown.upDownEnum"
+           )
 
     where
 
@@ -75,7 +87,7 @@ upDownEnum (DomainEnum (DomainDefnEnum _name values) ranges) = return (domainOut
         domainOut =
             if null ranges
                 then return $ DomainInt [RangeBounded [xMake| value.literal := [Prim (I 1)] |]
-                                                      [xMake| value.literal := [Prim (I nbValues)] |]]
+                                                                      [xMake| value.literal := [Prim (I nbValues)] |]]
                 else DomainInt `liftM` transformBiM down ranges
 
         down v =
@@ -95,4 +107,7 @@ upDownEnum (DomainEnum (DomainDefnEnum _name values) ranges) = return (domainOut
                 _ -> throwError $ ValueUpError $ "upDownEnum.up:" <+> pretty v
 
 upDownEnum d = throwError $ RepresentationDoesntMatch $ "upDownEnum only works on enum domains. this is not one:" <+> pretty d
+
+singletonList :: a -> [a]
+singletonList = return
 
