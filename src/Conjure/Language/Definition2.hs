ba5dd168d781cf32a1a230a361afe5e5ce032434
Özgür Akgün <ozgurakgun@gmail.com>
2014-09-10 11:58:57 +0200

    operators parameterised by inp and output kinds

--- src/Conjure/Language/Definition2.hs
+++ src/Conjure/Language/Definition2.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE EmptyDataDecls, TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DataKinds #-}
 
 module Conjure.Language.Definition2 where
-    
+
+import Data.Proxy
 
 class Kind a where
     data Type a
     data Domain a
     data Constant a
-    typeOfDomain :: a -> Type a
+    typeOfDomain :: Proxy a -> Type a
     allValues :: Domain a -> [Constant a]
     numValues :: Domain a -> Int
 
@@ -21,7 +23,7 @@ instance Kind KindBool where
     data Type     KindBool = TypeBool                       deriving Show
     data Domain   KindBool = DomainBool                     deriving Show
     data Constant KindBool = EFalse | ETrue                 deriving Show
-    typeOfDomain _ = TypeBool
+    typeOfDomain Proxy = TypeBool
     allValues DomainBool = [EFalse, ETrue]
     numValues DomainBool = 2
 
@@ -31,7 +33,7 @@ instance Kind KindInt where
     data Type     KindInt = TypeInt                         deriving Show
     data Domain   KindInt = DomainInt Int Int               deriving Show
     data Constant KindInt = ConstantInt Int                 deriving Show
-    typeOfDomain _ = TypeInt
+    typeOfDomain Proxy = TypeInt
     allValues (DomainInt l u) = map ConstantInt [l .. u]
     numValues (DomainInt l u) = u - l + 1
 
@@ -42,21 +44,54 @@ data Assoc = AssocL | AssocN | AssocR
     deriving (Eq, Ord, Show)
 
 
-class OpC a k where
-    data Operands a
-    fixity :: a -> Fixity
-    typeOfOp :: Operands a -> Type k
+class OpC a x (kInps :: [*]) kOut where
+    data Op a x kInps kOut
+    fixity :: Proxy a -> Fixity
+    typeOfOp :: Op a x kInps kOut -> Type kOut
 
 
+-- | OpPlus
+--
+-- >>> typeOfOp (OpPlus (MkXConstant $ ConstantInt 1) (MkXConstant $ ConstantInt 3))
+-- TypeInt
+--
 data OpPlus
-instance OpC OpPlus KindInt where
-    data Operands OpPlus = forall x k .
-                            ( ExpressionClass x k
-                            , k ~ KindInt
-                            ) => OpPlusOperands (Expression x) (Expression x)
-        -- deriving Show
-    fixity _ = Infix AssocL 600
-    typeOfOp (OpPlusOperands _ _) = TypeInt
+instance ( ExpressionClass x KindInt
+         , kInps ~ [KindInt,KindInt]
+         , kOut  ~ KindInt
+         ) => OpC OpPlus x kInps kOut where
+    data Op OpPlus x kInps kOut = OpPlus (Expression x) (Expression x)
+    fixity Proxy = Infix AssocL 600
+    typeOfOp (OpPlus a b) =
+        case (typeOfExpression a, typeOfExpression b) of
+            (TypeInt, TypeInt) -> TypeInt
+
+
+-- | OpTimes
+--
+-- >>> typeOfOp (OpTimes (MkXConstant $ ConstantInt 1) (MkXConstant $ ConstantInt 3))
+-- TypeInt
+--
+data OpTimes
+instance ( kInps ~ [KindInt,KindInt]
+         , kOut  ~ KindInt
+         , ExpressionClass x kOut
+         ) => OpC OpTimes x kInps kOut where
+    data Op OpTimes x kInps kOut = OpTimes (Expression x) (Expression x)
+    fixity Proxy = Infix AssocL 600
+    typeOfOp (OpTimes a b) =
+        case (typeOfExpression a, typeOfExpression b) of
+            (TypeInt, TypeInt) -> TypeInt
+
+
+
+-- data OpTimes
+-- instance OpC OpTimes x where
+--     data Op OpTimes x = OpTimes (Expression x) (Expression x)
+--     fixity Proxy = Infix AssocL 700
+--     typeOfOp (OpTimes a b) =
+--         case (typeOfExpression a, typeOfExpression b) of
+--             (TypeInt, TypeInt) -> TypeInt
 
 
 class ExpressionClass a k where
@@ -65,31 +100,35 @@ class ExpressionClass a k where
 
 
 -- | data type for injecting constants into expressions
--- 
--- >>> typeOfExpression (MkExpressionConstant EFalse)
+--
+-- >>> typeOfExpression (MkXConstant EFalse)
 -- TypeBool
--- 
--- >>> typeOfExpression (MkExpressionConstant (ConstantInt 1))
+--
+-- >>> typeOfExpression (MkXConstant (ConstantInt 1))
 -- TypeInt
--- 
+--
 data ExpressionConstant k
 instance (Kind k, Kind k2, k ~ k2) => ExpressionClass (ExpressionConstant k) k2 where
-    data Expression (ExpressionConstant k) = MkExpressionConstant (Constant k)
+    data Expression (ExpressionConstant k) = MkXConstant (Constant k)
         -- deriving Show
-    typeOfExpression (MkExpressionConstant _) = typeOfDomain undefined
+    typeOfExpression (MkXConstant _) = typeOfDomain Proxy
 
 -- | OpPlus is an ExpressionClass
--- 
--- >>> typeOfExpression (MkOpPlus (OpPlusOperands (MkExpressionConstant $ ConstantInt 1) (MkExpressionConstant $ ConstantInt 3)))
+--
+-- >>> typeOfExpression (MkXOp (OpPlus (MkXConstant $ ConstantInt 1) (MkXConstant $ ConstantInt 3))) :: Type KindInt
 -- TypeInt
--- 
-instance (k ~ KindInt) => ExpressionClass OpPlus k where
-    data Expression OpPlus = MkOpPlus (Operands OpPlus)
+--
+-- >>> typeOfExpression (MkXOp (OpPlus (MkXConstant $ ConstantInt 1) (MkXConstant $ ConstantInt 3)))
+-- TypeInt
+--
+data ExpressionOp op x (kInps :: [*]) kOut
+instance ( OpC op x kInps kOut
+         , ExpressionClass x kOut
+         , kOut ~ kOut2
+         ) => ExpressionClass (ExpressionOp op x kInps kOut) kOut2 where
+    data Expression (ExpressionOp op x kInps kOut) = MkXOp (Op op x kInps kOut)
         -- deriving Show
-    typeOfExpression (MkOpPlus (OpPlusOperands a b)) =
-        case (typeOfExpression a, typeOfExpression b) of
-            (TypeInt, TypeInt) -> TypeInt
-
+    typeOfExpression (MkXOp op) = typeOfOp op
 
 
 
