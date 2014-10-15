66ae85d030b9cd6d6e26a355a6ede5f433c0f3fe
Özgür Akgün <ozgurakgun@gmail.com>
2014-10-15 17:47:22 +0200

    helper functions in Conjure.Language.Type

--- src/Conjure/Language/Type.hs
+++ src/Conjure/Language/Type.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE DeriveFunctor, DeriveGeneric, DeriveDataTypeable #-}
 
-module Conjure.Language.Type where
+module Conjure.Language.Type
+    ( Type(..)
+    , typeUnify
+    , typesUnify
+    , mostDefined
+    ) where
 
 -- conjure
 import Conjure.Prelude
@@ -30,6 +35,7 @@ instance Hashable  Type
 instance ToJSON    Type where toJSON = JSON.genericToJSON jsonOptions
 instance FromJSON  Type where parseJSON = JSON.genericParseJSON jsonOptions
 
+-- | Check whether two types unify or not.
 typeUnify :: Type -> Type -> Bool
 typeUnify TypeAny _ = True
 typeUnify _ TypeAny = True
@@ -45,3 +51,30 @@ typeUnify (TypeFunction a1 a2) (TypeFunction b1 b2) = and (zipWith typeUnify [a1
 typeUnify (TypeRelation as) (TypeRelation bs) = and (zipWith typeUnify as bs)
 typeUnify (TypePartition a) (TypePartition b) = typeUnify a b
 typeUnify _ _ = False
+
+-- | Check whether a given list of types unify with each other or not.
+typesUnify :: [Type] -> Bool
+typesUnify ts = and [ typeUnify i j | i <- ts, j <- ts ]
+
+-- | Given a list of types return "the most defined" one in this list.
+--   This is to get rid of TypeAny's if there are any.
+--   Precondition: `typesUnify`
+mostDefined :: [Type] -> Type
+mostDefined = foldr f TypeAny
+    where
+        f :: Type -> Type -> Type
+        f TypeAny x = x
+        f x TypeAny = x
+        f _ x@TypeBool{} = x
+        f _ x@TypeInt{} = x
+        f _ x@TypeEnum{} = x
+        f _ x@TypeUnnamed{} = x
+        f (TypeTuple as) (TypeTuple bs) = TypeTuple (zipWith f as bs)
+        f (TypeMatrix a1 a2) (TypeMatrix b1 b2) = TypeMatrix (f a1 b1) (f a2 b2)
+        f (TypeSet a) (TypeSet b) = TypeSet (f a b)
+        f (TypeMSet a) (TypeMSet b) = TypeMSet (f a b)
+        f (TypeFunction a1 a2) (TypeFunction b1 b2) = TypeFunction (f a1 b1) (f a2 b2)
+        f (TypeRelation as) (TypeRelation bs) = TypeRelation (zipWith f as bs)
+        f (TypePartition a) (TypePartition b) = TypePartition (f a b)
+        f _ _ = TypeAny
+
