4726611cdb805b1cab0c82c6fb98253e738d19bc
Özgür Akgün <ozgurakgun@gmail.com>
2014-11-24 11:37:13 +0000

    rules for function !=, <, <=

--- src/Conjure/UI/Model.hs
+++ src/Conjure/UI/Model.hs
@@ -589,6 +589,9 @@ allRules config =
         , rule_Set_Card_ExplicitVarSizeWithMarker
 
         , rule_Function_Eq
+        , rule_Function_Neq
+        , rule_Function_Lt
+        , rule_Function_Leq
 
         , rule_Function_Image_Function1D
         , rule_Function_Image_Function1DPartial
@@ -1458,6 +1461,47 @@ rule_Function_Eq = "function-eq" `namedRule` theRule where
                                 /\
                             (forAll &iPat in &y . &x(&i[1]) = &i[2])
                         |]
+               )
+
+
+rule_Function_Neq :: Rule
+rule_Function_Neq = "function-neq" `namedRule` theRule where
+    theRule [essence| &x != &y |] = do
+        TypeFunction{} <- typeOf x
+        TypeFunction{} <- typeOf y
+        return ( "Horizontal rule for function dis-equality"
+               , const [essence| !(&x = &y) |]
+               )
+    theRule _ = fail "No match."
+
+
+rule_Function_Lt :: Rule
+rule_Function_Lt = "function-lt" `namedRule` theRule where
+    theRule p = do
+        (a,b) <- match opLt p
+        TypeFunction{} <- typeOf a
+        TypeFunction{} <- typeOf b
+        hasRepresentation a
+        hasRepresentation b
+        ma <- AbstractLiteral . AbsLitTuple <$> downX1 a
+        mb <- AbstractLiteral . AbsLitTuple <$> downX1 b
+        return ( "Horizontal rule for function <" <+> pretty (make opLt ma mb)
+               , const $ make opLt ma mb
+               )
+
+
+rule_Function_Leq :: Rule
+rule_Function_Leq = "function-leq" `namedRule` theRule where
+    theRule p = do
+        (a,b) <- match opLeq p
+        TypeFunction{} <- typeOf a
+        TypeFunction{} <- typeOf b
+        hasRepresentation a
+        hasRepresentation b
+        ma <- AbstractLiteral . AbsLitTuple <$> downX1 a
+        mb <- AbstractLiteral . AbsLitTuple <$> downX1 b
+        return ( "Horizontal rule for function <=" <+> pretty (make opLeq ma mb)
+               , const $ make opLeq ma mb
                )
 
 
