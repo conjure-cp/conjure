17ae6a97873f34f87d4da301821f6c4efdfdc076
Özgür Akgün <ozgurakgun@gmail.com>
2015-01-22 17:05:40 +0000

    lens for any quantifier

--- src/Conjure/Language/Lenses.hs
+++ src/Conjure/Language/Lenses.hs
@@ -855,6 +855,30 @@ opSum _ =
     )
 
 
+opQuantifier
+    :: ( OperatorContainer x
+       , Pretty x
+       , MonadFail m
+       )
+    => Proxy (m :: * -> *)
+    -> ( (x -> x, x) -> x
+       , x -> m (x -> x, x)
+       )
+opQuantifier _ =
+    ( \ (mk, x) -> mk x
+    , \ p -> do
+            op <- projectOp p
+            case op of
+                MkOpAnd   (OpAnd   [x]) -> return (injectOp . MkOpAnd   . OpAnd   . return , x)
+                MkOpOr    (OpOr    [x]) -> return (injectOp . MkOpOr    . OpOr    . return , x)
+                MkOpPlus  (OpPlus  [x]) -> return (injectOp . MkOpPlus  . OpPlus  . return , x)
+                MkOpTimes (OpTimes [x]) -> return (injectOp . MkOpTimes . OpTimes . return , x)
+                MkOpMax   (OpMax   [x]) -> return (injectOp . MkOpMax   . OpMax   . return , x)
+                MkOpMin   (OpMin   [x]) -> return (injectOp . MkOpMin   . OpMin   . return , x)
+                _ -> na ("Lenses.opSum:" <++> pretty p)
+    )
+
+
 opAllDiff
     :: ( OperatorContainer x
        , Pretty x
