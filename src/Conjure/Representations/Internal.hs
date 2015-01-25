3c46fce2aaa77bdd4152ceb04fefea7bb05a6a10
Özgür Akgün <ozgurakgun@gmail.com>
2015-01-25 15:18:41 +0000

    adding a few type synonyms, so we can get better error messages while defining a repr rule

--- src/Conjure/Representations/Internal.hs
+++ src/Conjure/Representations/Internal.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE KindSignatures #-}
 
 module Conjure.Representations.Internal
     ( Representation(..)
+    , TypeOf_ReprCheck, TypeOf_DownD, TypeOf_Structural, TypeOf_DownC, TypeOf_Up
     , DomainX, DomainC
     , rDownToX
     ) where
@@ -26,32 +28,42 @@ type DomainC = Domain HasRepresentation Constant
 --   It takes in a instance level domain for the high level object.
 -- * rCheck is for calculating all representation options.
 --   It take a function to be used as a "checker" for inner domains, if any.
-data Representation m = Representation
-    { rCheck
-        :: forall x r . (Pretty x, ExpressionLike x)
-        => (Domain r x -> [DomainX x])                  -- other checkers for inner domains
-        -> Domain r x                                   -- this domain
-        -> [DomainX x]                                  -- with all repr options
-    , rDownD
-        :: (Name, DomainX Expression)
-        -> m (Maybe [(Name, DomainX Expression)])
-    , rStructural
-        :: (DomainX Expression -> m ([Name] -> Expression -> m [Expression]))
-                                                        -- other structural constraints for inner domains
-        -> (Expression -> m [Expression])               -- general downX1
-        -> DomainX Expression                           -- this domain
-        -> m (     [Name]                               -- a source of fresh names
-              ->    Expression                          -- the original variable, before refinement
-              -> m [Expression]                         -- structural constraints
-             )
-    , rDownC
-        :: (Name, DomainC, Constant)                    -- the input name, domain and constant
-        -> m (Maybe [(Name, DomainC, Constant)])        -- the outputs names, domains, and constants
-    , rUp
-        :: [(Name, Constant)]                           -- all known constants, representing a solution at the low level
-        -> (Name, DomainC)                              -- the name and domain we are working on
-        -> m (Name, Constant)                           -- the output constant, at the high level
+data Representation (m :: * -> *) = Representation
+    { rCheck      :: TypeOf_ReprCheck  m
+    , rDownD      :: TypeOf_DownD      m
+    , rStructural :: TypeOf_Structural m
+    , rDownC      :: TypeOf_DownC      m
+    , rUp         :: TypeOf_Up         m
     }
+
+type TypeOf_ReprCheck (m :: * -> *) =
+       forall x r . (Pretty r, Pretty x, ExpressionLike x)
+    => (Domain r x -> [DomainX x])                  -- other checkers for inner domains
+    -> Domain r x                                   -- this domain
+    -> [DomainX x]                                  -- with all repr options
+
+type TypeOf_DownD (m :: * -> *) =
+                 (Name, DomainX Expression)
+    -> m (Maybe [(Name, DomainX Expression)])
+
+type TypeOf_Structural (m :: * -> *) =
+       (DomainX Expression -> m ([Name] -> Expression -> m [Expression]))
+                                                    -- other structural constraints for inner domains
+    -> (Expression -> m [Expression])               -- general downX1
+    -> DomainX Expression                           -- this domain
+    -> m (     [Name]                               -- a source of fresh names
+          ->    Expression                          -- the original variable, before refinement
+          -> m [Expression]                         -- structural constraints
+         )
+
+type TypeOf_DownC (m :: * -> *) =
+                 (Name, DomainC, Constant)          -- the input name, domain and constant
+    -> m (Maybe [(Name, DomainC, Constant)])        -- the outputs names, domains, and constants
+
+type TypeOf_Up (m :: * -> *) =
+       [(Name, Constant)]                           -- all known constants, representing a solution at the low level
+    -> (Name, DomainC)                              -- the name and domain we are working on
+    -> m (Name, Constant)                           -- the output constant, at the high level
 
 
 rDownToX
