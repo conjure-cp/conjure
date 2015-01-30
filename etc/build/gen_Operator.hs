bdac5634221193b8844e2d3de249fcb3c034755c
Özgür Akgün <ozgurakgun@gmail.com>
2015-01-30 16:15:11 +0000

    do not regenerate if the file exists and its contents are the same

--- etc/build/gen_Operator.hs
+++ etc/build/gen_Operator.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Applicative
-import Data.Maybe
-import Data.List
-import System.Directory
-import System.Environment
+import Control.Applicative ( (<$>) )
+import Data.Maybe ( Maybe(..) )
+import Data.List ( sort, isSuffixOf )
+import System.Directory ( getDirectoryContents )
+import Control.Exception ( catch, IOException )
 
 
 main :: IO ()
@@ -18,61 +19,70 @@ main = do
     let consModifier m = "MkOp" ++ m ++ " (Op" ++ m ++ " x)"
     let patModifier m = "MkOp" ++ m ++ " x"
 
-    writeFile (modulesDir ++ "/Generated.hs") $ unlines $ concat
-        [ [ "{-# LANGUAGE DeriveGeneric, DeriveDataTypeable, DeriveFunctor, DeriveTraversable, DeriveFoldable #-}"
-          , "{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}"
-          , ""
-          , "module Conjure.Language.Ops.Generated" ]
-        , [ "    ( Ops(..), valuesInIntDomain, evaluateOp, Fixity(..)"
-          , "    , functionals, operators" ]
-        , [ "    , Op" ++ m ++ "(..)" | m <- modules ]
-        , [ "    ) where"
-          , ""
-          , "-- conjure"
-          , "import Conjure.Prelude"
-          , "import Conjure.Language.Ops.Common"
-          , ""
-          ]
+    let outText = unlines $ concat
+            [ [ "{-# LANGUAGE DeriveGeneric, DeriveDataTypeable, DeriveFunctor, DeriveTraversable, DeriveFoldable #-}"
+              , "{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}"
+              , ""
+              , "module Conjure.Language.Ops.Generated" ]
+            , [ "    ( Ops(..), valuesInIntDomain, evaluateOp, Fixity(..)"
+              , "    , functionals, operators" ]
+            , [ "    , Op" ++ m ++ "(..)" | m <- modules ]
+            , [ "    ) where"
+              , ""
+              , "-- conjure"
+              , "import Conjure.Prelude"
+              , "import Conjure.Language.Ops.Common"
+              , ""
+              ]
 
-        , [ "import Conjure.Language.Ops." ++ m
-          | m <- modules
-          ]
+            , [ "import Conjure.Language.Ops." ++ m
+              | m <- modules
+              ]
 
-        , [ ""
-          , "data Ops x"
-          ]
-        , [ "    = " ++ consModifier (head modules)        ]
-        , [ "    | " ++ consModifier m | m <- tail modules ]
-        , [ "    deriving (Eq, Ord, Show, Data, Functor, Traversable, Foldable, Typeable, Generic)"
-          , ""
-          , "instance Serialize x => Serialize (Ops x)"
-          , "instance Hashable  x => Hashable  (Ops x)"
-          , "instance ToJSON    x => ToJSON    (Ops x) where toJSON = genericToJSON jsonOptions"
-          , "instance FromJSON  x => FromJSON  (Ops x) where parseJSON = genericParseJSON jsonOptions"
-          ]
+            , [ ""
+              , "data Ops x"
+              ]
+            , [ "    = " ++ consModifier (head modules)        ]
+            , [ "    | " ++ consModifier m | m <- tail modules ]
+            , [ "    deriving (Eq, Ord, Show, Data, Functor, Traversable, Foldable, Typeable, Generic)"
+              , ""
+              , "instance Serialize x => Serialize (Ops x)"
+              , "instance Hashable  x => Hashable  (Ops x)"
+              , "instance ToJSON    x => ToJSON    (Ops x) where toJSON = genericToJSON jsonOptions"
+              , "instance FromJSON  x => FromJSON  (Ops x) where parseJSON = genericParseJSON jsonOptions"
+              ]
 
-        , [ ""
-          , "instance (TypeOf x, Show x, Pretty x, ExpressionLike x) => TypeOf (Ops x) where"
-          ]
-        , [ "    typeOf (" ++ patModifier m ++ ") = typeOf x"
-          | m <- modules
-          ]
+            , [ ""
+              , "instance (TypeOf x, Show x, Pretty x, ExpressionLike x) => TypeOf (Ops x) where"
+              ]
+            , [ "    typeOf (" ++ patModifier m ++ ") = typeOf x"
+              | m <- modules
+              ]
 
-        , [ ""
-          , "instance EvaluateOp Ops where"
-          ]
-        , [ "    evaluateOp (" ++ patModifier m ++ ") = evaluateOp x"
-          | m <- modules
-          ]
+            , [ ""
+              , "instance EvaluateOp Ops where"
+              ]
+            , [ "    evaluateOp (" ++ patModifier m ++ ") = evaluateOp x"
+              | m <- modules
+              ]
 
-        , [ ""
-          , "instance Pretty x => Pretty (Ops x) where"
-          ]
-        , [ "    prettyPrec prec (" ++ patModifier m ++ ") = prettyPrec prec x"
-          | m <- modules
-          ]
+            , [ ""
+              , "instance Pretty x => Pretty (Ops x) where"
+              ]
+            , [ "    prettyPrec prec (" ++ patModifier m ++ ") = prettyPrec prec x"
+              | m <- modules
+              ]
 
-        ]
+            ]
+
+    outText' <- catch (Just <$> readFile (modulesDir ++ "/Generated.hs"))
+                      (\ (e :: IOException) -> return Nothing )
+    if Just outText /= outText'
+        then do
+            putStrLn $ "Generating " ++ modulesDir ++ "/Generated.hs"
+            writeFile (modulesDir ++ "/Generated.hs") outText
+        else
+            putStrLn $ "Skipping " ++ modulesDir ++ "/Generated.hs"
 
 splitOn :: Char -> String -> [String]
 splitOn _ [] = []
