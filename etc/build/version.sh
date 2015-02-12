fcb511165c6f8357c00aef8891a19b3059d3af35
Özgür Akgün <ozgurakgun@gmail.com>
2015-02-12 15:37:47 +0000

    using the variable DEVELOPMENT_MODE to skip updating the repo version. this is to avoid recompilation.

--- etc/build/version.sh
+++ etc/build/version.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-if ! [ -d .hg ] && [ -f src/Conjure/RepositoryVersion.hs ] ; then
-    echo "This is not a mercurial repository, but it contains repository information. Reusing."
-    cat src/Conjure/RepositoryVersion.hs | grep "repositoryVersion ="
+export DEVELOPMENT_MODE=${DEVELOPMENT_MODE:-"no"}
+
+if [ $DEVELOPMENT_MODE = "yes" ]; then
+    echo "Not touching the version file: DEVELEOPMENT_MODE = yes"
 else
-    VERSION="unknown"
-    if [ -d .hg ] ; then
-        VERSION=$(hg parent --template "{node|short} ({date|isodate})")
-    fi
-    if ( grep "repositoryVersion = \"${VERSION}\"" src/Conjure/RepositoryVersion.hs 2> /dev/null > /dev/null ) ; then
-        echo "Reusing src/Conjure/RepositoryVersion.hs with version ${VERSION}."
+    if ! [ -d .hg ] && [ -f src/Conjure/RepositoryVersion.hs ] ; then
+        echo "This is not a mercurial repository, but it contains repository information. Reusing."
+        cat src/Conjure/RepositoryVersion.hs | grep "repositoryVersion ="
     else
-        echo "Generating src/Conjure/RepositoryVersion.hs with version ${VERSION}."
-        echo "module Conjure.RepositoryVersion where"   >  src/Conjure/RepositoryVersion.hs
-        echo "import Prelude"                           >> src/Conjure/RepositoryVersion.hs
-        echo "repositoryVersion :: String"              >> src/Conjure/RepositoryVersion.hs
-        echo "repositoryVersion = \"${VERSION}\""       >> src/Conjure/RepositoryVersion.hs
+        VERSION="unknown"
+        if [ -d .hg ] ; then
+            VERSION=$(hg parent --template "{node|short} ({date|isodate})")
+        fi
+        if ( grep "repositoryVersion = \"${VERSION}\"" src/Conjure/RepositoryVersion.hs 2> /dev/null > /dev/null ) ; then
+            echo "Reusing src/Conjure/RepositoryVersion.hs with version ${VERSION}."
+        else
+            echo "Generating src/Conjure/RepositoryVersion.hs with version ${VERSION}."
+            echo "module Conjure.RepositoryVersion where"   >  src/Conjure/RepositoryVersion.hs
+            echo "import Prelude"                           >> src/Conjure/RepositoryVersion.hs
+            echo "repositoryVersion :: String"              >> src/Conjure/RepositoryVersion.hs
+            echo "repositoryVersion = \"${VERSION}\""       >> src/Conjure/RepositoryVersion.hs
+        fi
     fi
 fi
 
