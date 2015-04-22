2659ab505cdea56847fab6f1c269b80c087aad0f
Özgür Akgün <ozgurakgun@gmail.com>
2015-04-22 17:40:37 +0100

    use the cardinality in the domain when refining card!

--- src/Conjure/Rules/Horizontal/Set.hs
+++ src/Conjure/Rules/Horizontal/Set.hs
@@ -303,9 +303,11 @@ rule_Card = "set-card" `namedRule` theRule where
         TypeSet{} <- typeOf s
         return
             ( "Horizontal rule for set cardinality."
-            , do
-                 (iPat, _) <- quantifiedVar
-                 return [essence| sum &iPat in &s . 1 |]
+            , case domainOf s of
+                Just (DomainSet _ (SetAttr (SizeAttr_Size n)) _) -> return n
+                _ -> do
+                    (iPat, _) <- quantifiedVar
+                    return [essence| sum &iPat in &s . 1 |]
             )
 
 
