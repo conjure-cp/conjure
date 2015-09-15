3a2724e4c65a9f57cff911b2a1c17008a10b64a3
Özgür Akgün <ozgurakgun@gmail.com>
2015-09-15 11:22:25 +0100

    undoing the domainOf change: this may not be the best way of handling it

--- src/Conjure/Compute/DomainOf.hs
+++ src/Conjure/Compute/DomainOf.hs
@@ -22,12 +22,12 @@ class DomainOf a where
 
 instance DomainOf ReferenceTo where
     domainOf (Alias x) = domainOf x
-    domainOf (InComprehension (GenDomainNoRepr Single{} dom)) = return dom
-    domainOf (InComprehension (GenDomainHasRepr _ dom)) = return (forgetRepr dom)
-    domainOf (InComprehension (GenInExpr Single{} x)) = do
-        domX <- domainOf x
-        innX <- innerDomainOf domX
-        return innX
+    -- domainOf (InComprehension (GenDomainNoRepr Single{} dom)) = return dom
+    -- domainOf (InComprehension (GenDomainHasRepr _ dom)) = return (forgetRepr dom)
+    -- domainOf (InComprehension (GenInExpr Single{} x)) = do
+    --     domX <- domainOf x
+    --     innX <- innerDomainOf domX
+    --     return innX
     domainOf x@InComprehension{} = fail $ vcat [ "domainOf-ReferenceTo-InComprehension", pretty x, pretty (show x) ]
     domainOf (DeclNoRepr  _ _ dom) = return dom
     domainOf (DeclHasRepr _ _ dom) = return (forgetRepr dom)
