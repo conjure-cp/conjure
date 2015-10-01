85161c43215ddf8b63045d782c5f3f37fb2e5b1e
Özgür Akgün <ozgurakgun@gmail.com>
2015-10-02 00:29:22 +0100

    allow nested directories under tests/exhaustive

--- tests/exhaustive/acceptOutput.sh
+++ tests/exhaustive/acceptOutput.sh
@@ -6,12 +6,15 @@ set -o nounset
 TESTCASE="$1"
 
 if [ -d "${TESTCASE}" ]; then
-    echo "Accepting the output of ${TESTCASE}"
-    rm -f "${TESTCASE}"/expected/*
-    mkdir -p "${TESTCASE}"/expected
-    cp "${TESTCASE}"/outputs/*.eprime \
-       "${TESTCASE}"/outputs/*.solution \
-       "${TESTCASE}"/outputs/*.eprime-param \
-       "${TESTCASE}"/expected/ 2> /dev/null || :
-    parallel "[ -f {} ] && (cat {} | grep -v '\\$' > {}.temp ; mv {}.temp {})" ::: "${TESTCASE}"/expected/*.eprime
+    NUM=$(ls "${TESTCASE}/"*.essence | grep -v disabled.essence 2> /dev/null | wc -l)
+    if [ "$NUM" -eq "1" ]; then
+        echo "Accepting the output of ${TESTCASE}"
+        rm -f "${TESTCASE}"/expected/*
+        mkdir -p "${TESTCASE}"/expected
+        cp "${TESTCASE}"/outputs/*.eprime \
+           "${TESTCASE}"/outputs/*.solution \
+           "${TESTCASE}"/outputs/*.eprime-param \
+           "${TESTCASE}"/expected/ 2> /dev/null || :
+        parallel "[ -f {} ] && (cat {} | grep -v '\\$' > {}.temp ; mv {}.temp {})" ::: "${TESTCASE}"/expected/*.eprime
+    fi
 fi
--- tests/parse_print/acceptOutput.sh
+++ tests/parse_print/acceptOutput.sh
@@ -6,7 +6,7 @@ set -o nounset
 TESTCASE="$1"
 
 if [ -d "${TESTCASE}" ]; then
-    NUM=$(ls "${TESTCASE}/"*.essence 2> /dev/null | wc -l)
+    NUM=$(ls "${TESTCASE}/"*.essence | grep -v disabled.essence 2> /dev/null | wc -l)
     if [ "$NUM" -eq "1" ]; then
         echo "Accepting the output of ${TESTCASE}"
         touch "${TESTCASE}"/stdout "${TESTCASE}"/stderr
