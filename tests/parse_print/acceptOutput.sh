20ddbbb5d335fa0b42bbbb0b90a06bc141b297bb
Özgür Akgün <ozgurakgun@gmail.com>
2015-10-02 21:00:01 +0100

    except "disabled.essence"

--- tests/parse_print/acceptOutput.sh
+++ tests/parse_print/acceptOutput.sh
@@ -6,12 +6,12 @@ set -o nounset
 TESTCASE="$1"
 
 if [ -d "${TESTCASE}" ]; then
-    NUM=$(ls "${TESTCASE}/"*.essence | grep -v disabled.essence 2> /dev/null | wc -l)
+    NUM=$(ls "${TESTCASE}/"*.essence 2> /dev/null | grep -v disabled.essence | wc -l | tr -d ' ')
     if [ "$NUM" -eq "1" ]; then
         echo "Accepting the output of ${TESTCASE}"
         touch "${TESTCASE}"/stdout "${TESTCASE}"/stderr
         cp "${TESTCASE}"/stdout "${TESTCASE}"/stdout.expected
         cp "${TESTCASE}"/stderr "${TESTCASE}"/stderr.expected
-        find "${TESTCASE}" -size 0 -delete
+        find "${TESTCASE}" -depth 1 -size 0 -delete
     fi
 fi
