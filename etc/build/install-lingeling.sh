35bca88f112cfa966791dc5fc3c3cad5b528dbf6
Özgür Akgün <ozgurakgun@gmail.com>
2016-05-05 13:19:38 +0100

    mkdir ~/bin if it doesn't exist

--- etc/build/install-lingeling.sh
+++ etc/build/install-lingeling.sh
@@ -10,7 +10,10 @@ pushd ~/tmp-install-lingeling
 wget -c http://fmv.jku.at/lingeling/lingeling-ayv-86bf266-140429.zip
 unzip lingeling-ayv-86bf266-140429.zip
 ./build.sh
+mkdir -p ~/bin
 cp binary/lingeling ~/bin/lingeling
+echo "lingeling executable is at ~/bin/lingeling"
+ls -l ~/bin/lingeling
 popd
 rm -rf ~/tmp-install-lingeling
 
