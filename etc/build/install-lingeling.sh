5a824f98b457bb5fa42fa7bf4b7f0ef4258fc09a
Özgür Akgün <ozgurakgun@gmail.com>
2016-05-09 11:28:53 +0100

    installing lingeling to ~/cabal/bin (by default) as well

--- etc/build/install-lingeling.sh
+++ etc/build/install-lingeling.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o nounset
 
+export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
 
 rm -rf ~/tmp-install-lingeling
 mkdir ~/tmp-install-lingeling
@@ -10,10 +11,10 @@ pushd ~/tmp-install-lingeling
 wget -c http://fmv.jku.at/lingeling/lingeling-ayv-86bf266-140429.zip
 unzip lingeling-ayv-86bf266-140429.zip
 ./build.sh
-mkdir -p ~/bin
-cp binary/lingeling ~/bin/lingeling
-echo "lingeling executable is at ~/bin/lingeling"
-ls -l ~/bin/lingeling
+mkdir -p ${BIN_DIR}
+cp binary/lingeling ${BIN_DIR}/lingeling
+echo "lingeling executable is at ${BIN_DIR}/lingeling"
+ls -l ${BIN_DIR}/lingeling
 popd
 rm -rf ~/tmp-install-lingeling
 
