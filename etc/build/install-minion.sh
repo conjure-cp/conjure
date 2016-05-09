5e051538a9f7d70830c769857043ade3c7f2b135
Özgür Akgün <ozgurakgun@gmail.com>
2016-05-09 14:45:56 +0100

    installing minion and other travis tweaks
    - installing minion
    - printing versions of tools
    - set BIN_DIR once

---
+++ etc/build/install-minion.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -o errexit
+set -o nounset
+
+export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
+
+rm -rf ~/tmp-install-minion
+mkdir ~/tmp-install-minion
+pushd ~/tmp-install-minion
+hg clone ssh://hg@bitbucket.org/stacs_cp/minion
+mkdir -p minion/build
+(cd minion/build && ../build.py && make minion -j4)
+cp minion/build/minion ${BIN_DIR}/minion
+echo "minion executable is at ${BIN_DIR}/minion"
+ls -l ${BIN_DIR}/minion
+popd
+rm -rf ~/tmp-install-minion
+
