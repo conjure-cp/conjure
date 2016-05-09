569b6102d4b123fb4717d145eeefb0ec7054e0ae
Özgür Akgün <ozgurakgun@gmail.com>
2016-05-09 14:30:29 +0100

    actually use the BIN_DIR and not hardcode it to "bin"

--- etc/build/install-savilerow.sh
+++ etc/build/install-savilerow.sh
@@ -7,10 +7,10 @@ export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
 
 wget -c http://savilerow.cs.st-andrews.ac.uk/savilerow-1.6.4-linux.tgz
 tar zxvf savilerow-1.6.4-linux.tgz
-cp savilerow-1.6.4-linux/savilerow.jar bin/savilerow.jar
-mkdir -p bin/lib
-cp savilerow-1.6.4-linux/lib/trove.jar bin/lib/trove.jar
-pushd bin
+cp savilerow-1.6.4-linux/savilerow.jar ${BIN_DIR}/savilerow.jar
+mkdir -p ${BIN_DIR}/lib
+cp savilerow-1.6.4-linux/lib/trove.jar ${BIN_DIR}/lib/trove.jar
+pushd ${BIN_DIR}
 echo '#!/bin/bash'                                                               >  savilerow
 echo 'DIR="$( cd "$( dirname "$0" )" && pwd )"'                                  >> savilerow
 echo 'java -ea -XX:ParallelGCThreads=1 -Xmx8G -jar "$DIR/savilerow.jar" "$@"'    >> savilerow
