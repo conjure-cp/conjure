622452bcb58162b27c5c89c13588f6f2d9e6cc96
Özgür Akgün <ozgurakgun@gmail.com>
2016-05-13 17:05:11 +0100

    install-minion script doesn't hardcode the compiler version

--- etc/build/install-minion.sh
+++ etc/build/install-minion.sh
@@ -4,17 +4,25 @@ set -o errexit
 set -o nounset
 
 export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
+export COMPILER=${COMPILER:-""}
 
 rm -rf ~/tmp-install-minion
 mkdir ~/tmp-install-minion
 pushd ~/tmp-install-minion
 hg clone https://bitbucket.org/stacs_cp/minion
 mkdir -p minion/build
-COMPILER="g++-4.9"
-if which ccache; then
-    COMPILER="ccache ${COMPILER}"
-fi
-(cd minion/build && ../build.py --compiler "${COMPILER}" && make minion -j2)
+(
+    cd minion/build
+    if [ -z "${COMPILER}" ]; then
+        ../build.py
+    else
+        if which ccache; then
+            COMPILER="ccache ${COMPILER}"
+        fi
+        ../build.py --compiler "${COMPILER}"
+    fi
+    make minion -j2
+)
 cp minion/build/minion ${BIN_DIR}/minion
 echo "minion executable is at ${BIN_DIR}/minion"
 ls -l ${BIN_DIR}/minion
