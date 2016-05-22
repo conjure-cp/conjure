8c3a9e2b73fd4f32b939ceac61219121ed20bba2
Özgür Akgün <ozgurakgun@gmail.com>
2016-05-22 15:27:28 +0100

    keep other cabal.config files

--- etc/build/freeze-deps.sh
+++ etc/build/freeze-deps.sh
@@ -8,6 +8,7 @@ set -o nounset
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source ${SCRIPT_DIR}/default_envvars.sh
 echo "Creating a cabal-freeze file for GHC ${GHC_VERSION}"
+git checkout -- cabal.config-*
 cabal freeze --enable-tests
 cat cabal.config | grep -v '             base ==' > cabal.config-${GHC_VERSION}
 rm -f cabal.config
