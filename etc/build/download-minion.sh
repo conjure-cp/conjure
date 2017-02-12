1f7afbce4fd63dd42e7bab33b69f98e1cfef2697
Özgür Akgün <ozgurakgun@gmail.com>
2017-02-12 22:34:30 +0000

    Download minion, depending on TRAVIS_OS_NAME

--- etc/build/download-minion.sh
+++ etc/build/download-minion.sh
@@ -5,7 +5,18 @@ set -o nounset
 
 export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
 
-wget -c https://dl.dropboxusercontent.com/u/14272760/SavileRow/minion-1.8-linux.tar_1.gz
-tar -xvzf minion-1.8-linux.tar_1.gz
-mv minion-1.8/bin/minion ${BIN_DIR}/minion
+OS=$(uname)
+
+if [ "$OS" == "Darwin" ]; then
+    wget -c https://dl.dropboxusercontent.com/u/14272760/SavileRow/minion-1.8-mac.tar_.gz
+    tar -xvzf minion-1.8-mac.tar_.gz
+    mv minion-1.8/bin/minion ${BIN_DIR}/minion
+elif [ "$OS" == "Linux" ]; then
+    wget -c https://dl.dropboxusercontent.com/u/14272760/SavileRow/minion-1.8-linux.tar_1.gz
+    tar -xvzf minion-1.8-linux.tar_1.gz
+    mv minion-1.8/bin/minion ${BIN_DIR}/minion
+else
+    echo "Cannot determine your OS, uname reports: ${OS}"
+    exit 1
+fi
 
