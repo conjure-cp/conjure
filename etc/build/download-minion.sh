0f6e533545c3877caf53dbfb04bc5d6216380f04
Özgür Akgün <ozgurakgun@gmail.com>
2017-04-28 19:46:11 +0100

    Use wget --no-check-certificate when downloading Minion

--- etc/build/download-minion.sh
+++ etc/build/download-minion.sh
@@ -8,11 +8,11 @@ export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
 OS=$(uname)
 
 if [ "$OS" == "Darwin" ]; then
-    wget -c https://ozgur.host.cs.st-andrews.ac.uk/Minions/minion-1.8-mac.tar_.gz
+    wget --no-check-certificate -c https://ozgur.host.cs.st-andrews.ac.uk/Minions/minion-1.8-mac.tar_.gz
     tar -xvzf minion-1.8-mac.tar_.gz
     mv minion-1.8/bin/minion ${BIN_DIR}/minion
 elif [ "$OS" == "Linux" ]; then
-    wget -c https://ozgur.host.cs.st-andrews.ac.uk/Minions/minion-1.8-linux.tar_1.gz
+    wget --no-check-certificate -c https://ozgur.host.cs.st-andrews.ac.uk/Minions/minion-1.8-linux.tar_1.gz
     tar -xvzf minion-1.8-linux.tar_1.gz
     mv minion-1.8/bin/minion ${BIN_DIR}/minion
 else
