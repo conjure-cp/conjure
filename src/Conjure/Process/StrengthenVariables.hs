8293e35b3413abeff1629a8d964e7f62dbaf0310
Billy Brown <wb33@st-andrews.ac.uk>
2017-06-08 16:48:20 +0100

    StrengthenVariables now monadic with MonadFail
    All functions in StrengthenVariables are now monadic, in the sense
    that they return a `m a` where m is at least a MonadFail.

--- src/Conjure/Process/StrengthenVariables.hs
+++ src/Conjure/Process/StrengthenVariables.hs
@@ -28,33 +28,42 @@ strengthenVariables = runNameGen . resolveNames >=> core
           if model == model'
              then return model'
              else core   model'
+        -- Apply the function to every statement and fold it into the model
         folded m@Model { mStatements = stmts } f
-          = return m { mStatements = map (`f` m) stmts }
+          = foldM (\m' s -> do
+                    s' <- f m s
+                    -- This is map, but unwraps the value from the monad
+                    return m' { mStatements = s' : mStatements m' })
+                  m { mStatements = [] }
+                  (reverse stmts)
 
 -- | Make the attributes of function variables as constrictive as possible.
-functionAttributes :: Statement -- ^ Statement to constrain.
-                   -> Model     -- ^ Model as context.
-                   -> Statement -- ^ Possibly updated statement.
-functionAttributes (Declaration (FindOrGiven forg n d@DomainFunction{})) m
-  = Declaration (FindOrGiven forg n (constrainFunctionDomain d m))
-functionAttributes (Declaration (Letting n (Domain d@DomainFunction{}))) m
-  = Declaration (Letting n (Domain (constrainFunctionDomain d m)))
-functionAttributes s _ = s
+functionAttributes :: (MonadFail m)
+                   => Model       -- ^ Model as context.
+                   -> Statement   -- ^ Statement to constrain.
+                   -> m Statement -- ^ Possibly updated statement.
+functionAttributes m (Declaration (FindOrGiven forg n d@DomainFunction{})) = do
+  d' <- constrainFunctionDomain d m
+  return $ Declaration (FindOrGiven forg n d')
+functionAttributes m (Declaration (Letting n (Domain d@DomainFunction{}))) = do
+  d' <- constrainFunctionDomain d m
+  return $ Declaration (Letting n (Domain d'))
+functionAttributes _ s = return s
 
 -- | Constrain the domain of a function given the context of a model.
-constrainFunctionDomain :: (Eq r, Pretty r)
-                        => Domain r Expression  -- ^ Current domain of the function.
-                        -> Model                -- ^ Context of the model.
-                        -> Domain r Expression  -- ^ Possibly modified domain.
+constrainFunctionDomain :: (MonadFail m, Eq r, Pretty r)
+                        => Domain r Expression      -- ^ Current domain of the function.
+                        -> Model                    -- ^ Context of the model.
+                        -> m (Domain r Expression)  -- ^ Possibly modified domain.
 constrainFunctionDomain d@(DomainFunction r attrs from to) _
   = case attrs of
          -- If a function is surjective and its domain and codomain are equal, then it is bijective
          FunctionAttr s p JectivityAttr_Surjective ->
            if from == to
-              then DomainFunction r (FunctionAttr s p JectivityAttr_Bijective) from to
-              else d
+              then return $ DomainFunction r (FunctionAttr s p JectivityAttr_Bijective) from to
+              else return d
          -- If a function is bijective, then it is total
          FunctionAttr s PartialityAttr_Partial j@JectivityAttr_Bijective ->
-           DomainFunction r (FunctionAttr s PartialityAttr_Total j) from to
-         _ -> d
-constrainFunctionDomain d _ = d
+           return $ DomainFunction r (FunctionAttr s PartialityAttr_Total j) from to
+         _ -> return d
+constrainFunctionDomain d _ = return d
