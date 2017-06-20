5a4e6739ca0a5393bf9714106facbf5350bb1992
Billy Brown <wb33@st-andrews.ac.uk>
2017-06-20 17:08:36 +0100

    Started work on detecting total functions
    Started work on detecting total functions by looking at the
    constraints. It currently applies to forAll and only if there is
    a reference to the function in its body and, the values are all
    generated from the function's domain and there is no conditional.

--- src/Conjure/Process/StrengthenVariables.hs
+++ src/Conjure/Process/StrengthenVariables.hs
@@ -12,6 +12,8 @@ module Conjure.Process.StrengthenVariables
     strengthenVariables
   ) where
 
+import Data.List ( union )
+
 import Conjure.Prelude
 import Conjure.Language
 import Conjure.Language.NameResolution ( resolveNames )
@@ -43,27 +45,35 @@ functionAttributes :: (MonadFail m, MonadLog m)
                    -> Statement   -- ^ Statement to constrain.
                    -> m Statement -- ^ Possibly updated statement.
 functionAttributes m (Declaration (FindOrGiven forg n d@DomainFunction{})) = do
-  d' <- constrainFunctionDomain d m
+  d' <- constrainFunctionDomain n d m
   return $ Declaration (FindOrGiven forg n d')
 functionAttributes m (Declaration (Letting n (Domain d@DomainFunction{}))) = do
-  d' <- constrainFunctionDomain d m
+  d' <- constrainFunctionDomain n d m
   return $ Declaration (Letting n (Domain d'))
 functionAttributes _ s = return s
 
 -- | Constrain the domain of a function given the context of a model.
 constrainFunctionDomain :: (MonadFail m, MonadLog m)
-                        => Domain () Expression     -- ^ Current domain of the function.
+                        => Name                     -- ^ Name of the function.
+                        -> Domain () Expression     -- ^ Current domain of the function.
                         -> Model                    -- ^ Context of the model.
                         -> m (Domain () Expression) -- ^ Possibly modified domain.
-constrainFunctionDomain d@DomainFunction{} _
-  = surjectiveIsTotalBijective d
-constrainFunctionDomain d _ = return d
+constrainFunctionDomain n d@DomainFunction{} m
+  = surjectiveIsTotalBijective d >>=
+    definedForAllIsTotal n (suchThat m)
+constrainFunctionDomain _ d _ = return d
+
+-- | Extract the such that expressions from a model.
+suchThat :: Model -> [Expression]
+suchThat = foldr fromSuchThat [] . mStatements
+  where fromSuchThat (SuchThat es) a = a `union` es
+        fromSuchThat _             a = a
 
 -- | If a function is surjective or bijective and its domain and codomain
 --   are of equal size, then it is total and bijective.
 surjectiveIsTotalBijective :: (MonadFail m, MonadLog m)
-                           => Domain () Expression      -- Domain of the function.
-                           -> m (Domain () Expression)  -- Possibly modified domain.
+                           => Domain () Expression      -- ^ Domain of the function.
+                           -> m (Domain () Expression)  -- ^ Possibly modified domain.
 surjectiveIsTotalBijective d@(DomainFunction r (FunctionAttr s PartialityAttr_Partial j) from to)
   | j == JectivityAttr_Surjective || j == JectivityAttr_Bijective = do
     (fromSize, toSize) <- functionDomainSizes from to
@@ -74,10 +84,42 @@ surjectiveIsTotalBijective d = return d
 
 -- | Calculate the sizes of the domain and codomain of a function.
 functionDomainSizes :: (MonadFail m)
-                    => Domain () Expression       -- The function's domain.
-                    -> Domain () Expression       -- The function's codomain.
-                    -> m (Expression, Expression) -- The sizes of the two.
+                    => Domain () Expression       -- ^ The function's domain.
+                    -> Domain () Expression       -- ^ The function's codomain.
+                    -> m (Expression, Expression) -- ^ The sizes of the two.
 functionDomainSizes from to = do
   fromSize <- domainSizeOf from
   toSize   <- domainSizeOf to
   return (fromSize, toSize)
+
+-- | If a function is defined for all values in its domain, then it is total.
+definedForAllIsTotal :: (MonadFail m, MonadLog m)
+                     => Name                      -- ^ Name of the function.
+                     -> [Expression]              -- ^ Such that constraints.
+                     -> Domain () Expression      -- ^ Domain of the function.
+                     -> m (Domain () Expression)  -- ^ Possibly modified domain.
+definedForAllIsTotal n cs (DomainFunction r (FunctionAttr s PartialityAttr_Partial j) from to)
+  | any definedForAll cs
+    = return $ DomainFunction r (FunctionAttr s PartialityAttr_Total j) from to
+  where
+        definedForAll (Op (MkOpAnd (OpAnd (Comprehension e gorcs))))
+          = e `containsReference` n &&
+            gorcs `containsGenerator` from &&
+            hasNoConditions gorcs
+        definedForAll _ = False
+        hasNoConditions = not . any isCondition
+        isCondition Condition{} = True
+        isCondition _           = False
+definedForAllIsTotal _ _ d = return d
+
+-- | Determine whether an expression contains a reference to a variable.
+containsReference :: Expression -> Name -> Bool
+containsReference e n = any nameEqual $ universe e
+  where nameEqual (Reference n' _) = n == n'
+        nameEqual _                = False
+
+-- | Determine whether a list of generators or conditions contains a generator for a given domain.
+containsGenerator :: [GeneratorOrCondition] -> Domain () Expression -> Bool
+containsGenerator gorcs d = any containsGenerator' gorcs
+  where containsGenerator' (Generator (GenDomainNoRepr  _ d')) = d == d'
+        containsGenerator' _ = False
