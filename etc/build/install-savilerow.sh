481623f9ef11263af70a9b9a389cd589259929ea
Özgür Akgün <ozgurakgun@gmail.com>
2017-07-07 00:07:56 +0100

    Update Savile Row version

--- etc/build/install-savilerow.sh
+++ etc/build/install-savilerow.sh
@@ -5,12 +5,12 @@ set -o nounset
 
 export BIN_DIR=${BIN_DIR:-${HOME}/.cabal/bin}
 
-wget -c http://savilerow.cs.st-andrews.ac.uk/savilerow-1.6.4-linux.tgz
-tar zxvf savilerow-1.6.4-linux.tgz
-# cp savilerow-1.6.4-linux/savilerow.jar ${BIN_DIR}/savilerow.jar
-mkdir -p ${BIN_DIR}/lib
-cp savilerow-1.6.4-linux/lib/trove.jar ${BIN_DIR}/lib/trove.jar
-rm -rf savilerow-1.6.4-linux.tgz savilerow-1.6.4-linux
+# wget -c http://savilerow.cs.st-andrews.ac.uk/savilerow-1.6.4-linux.tgz
+# tar zxvf savilerow-1.6.4-linux.tgz
+# # cp savilerow-1.6.4-linux/savilerow.jar ${BIN_DIR}/savilerow.jar
+# mkdir -p ${BIN_DIR}/lib
+# cp savilerow-1.6.4-linux/lib/trove.jar ${BIN_DIR}/lib/trove.jar
+# rm -rf savilerow-1.6.4-linux.tgz savilerow-1.6.4-linux
 
 pushd ${BIN_DIR}
 echo '#!/bin/bash'                                                               >  savilerow
@@ -20,5 +20,5 @@ chmod +x savilerow
 popd
 
 # using an unreleased version of savilerow
-wget --no-check-certificate -c https://ozgur.host.cs.st-andrews.ac.uk/SavileRows/2017-04-23--85dce3ca35e1/savilerow.jar
+wget --no-check-certificate -c https://ozgur.host.cs.st-andrews.ac.uk/SavileRows/2017-07-03--ad9229da27ca/savilerow.jar
 cp savilerow.jar ${BIN_DIR}/savilerow.jar
