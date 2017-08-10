200fc0ae1076384f86130fd1a2d23c50ca025ec7
Özgür Akgün <ozgurakgun@gmail.com>
2017-08-10 22:20:50 +0100

    pass --no-notice to parallel

--- tests/acceptAllOutputs.sh
+++ tests/acceptAllOutputs.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o nounset
 
-parallel tests/custom/acceptOutput.sh ::: $(find tests/custom -type d)
-parallel tests/exhaustive/acceptOutput.sh ::: $(find tests/exhaustive -type d)
-parallel tests/parse_print/acceptOutput.sh ::: $(find tests/parse_print -type d)
+parallel --no-notice tests/custom/acceptOutput.sh ::: $(find tests/custom -type d)
+parallel --no-notice tests/exhaustive/acceptOutput.sh ::: $(find tests/exhaustive -type d)
+parallel --no-notice tests/parse_print/acceptOutput.sh ::: $(find tests/parse_print -type d)
