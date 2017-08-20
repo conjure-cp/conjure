0de95fda595ad898675b416e449c96e475a04e18
Özgür Akgün <ozgurakgun@gmail.com>
2017-08-20 16:21:24 +0100

    frameUpdate_01 test passes

--- tests/custom/basic/frameUpdate_01/run.sh
+++ tests/custom/basic/frameUpdate_01/run.sh
@@ -1,3 +1,31 @@
 
-conjure solve -aai --channelling=no frameUpdate_01.essence --number-of-solutions=all
+
+rm -rf Occurrence Explicit
+
+
+# Occurrence
+conjure solve frameUpdate_01.essence \
+    -aai \
+    --channelling=no \
+    --copy-solutions=no \
+    --number-of-solutions=all \
+    --responses 1,1 \
+    -o Occurrence
+
+cat Occurrence/*.solution
+
+
+# Explicit
+conjure solve frameUpdate_01.essence \
+    -aai \
+    --channelling=no \
+    --copy-solutions=no \
+    --number-of-solutions=all \
+    --responses 1,1 \
+    -o Explicit
+
+cat Explicit/*.solution
+
+
+rm -rf Occurrence Explicit
 
