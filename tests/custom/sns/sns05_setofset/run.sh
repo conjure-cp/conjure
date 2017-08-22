ad959e41c67752b7c7411a275dc6b7b24342f7e8
Özgür Akgün <ozgurakgun@gmail.com>
2017-08-22 14:28:35 +0100

    Add a test case

---
+++ tests/custom/sns/sns05_setofset/run.sh
@@ -0,0 +1,13 @@
+
+conjure -ac --generate-neighbourhoods sns.essence
+
+# remove comments and move file
+cat conjure-output/model000001.eprime | grep -v '\$' > sns-compact.eprime.new
+
+
+# check output, save new output
+diff sns-compact.eprime.new sns-compact.eprime
+mv sns-compact.eprime.new sns-compact.eprime
+
+rm -rf conjure-output
+
