bad52e016cf521eb4c6363ff284b51681343c628
Özgür Akgün <ozgurakgun@gmail.com>
2017-09-06 11:16:28 +0100

    Add frameUpdate/mset tests

---
+++ tests/custom/frameUpdate/mset/01/run.sh
@@ -0,0 +1,31 @@
+
+
+rm -rf WithFlags WithRepetition
+
+
+# WithFlags
+conjure solve *.essence \
+    -aai \
+    --channelling=no \
+    --copy-solutions=no \
+    --number-of-solutions=all \
+    --responses 1,1 \
+    -o WithFlags
+
+grep letting WithFlags/*.solution
+
+
+# WithRepetition
+conjure solve *.essence \
+    -aai \
+    --channelling=no \
+    --copy-solutions=no \
+    --number-of-solutions=all \
+    --responses 2,2 \
+    -o WithRepetition
+
+grep letting WithRepetition/*.solution
+
+
+rm -rf WithFlags WithRepetition
+
