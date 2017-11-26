5d6f8aaf034a9cf54c86dd141a2344fba91b8f77
Özgür Akgün <ozgurakgun@gmail.com>
2017-11-26 19:51:04 +0000

    Remove bool cases for #387 -- fixes #387

--- tests/custom/issues/387/bool04/run.sh
+++
@@ -1,5 +0,0 @@
-rm -rf conjure-output && \
-conjure solve -ax --smart-filenames --channelling=no *.essence --validate-solutions && \
-cat conjure-output/*.solution && \
-rm -rf conjure-output *.solution && \
-rm -rf conjure-output
