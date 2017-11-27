0cf286b88ff07ac3642275c3d7cf6ca587fd29ac
Özgür Akgün <ozgurakgun@gmail.com>
2017-11-27 00:08:41 +0000

    Remove redundant import

--- src/Conjure/Language/Definition.hs
+++ src/Conjure/Language/Definition.hs
@@ -55,7 +55,6 @@ import Conjure.Language.Expression
 -- aeson
 import Data.Aeson ( (.=), (.:) )
 import qualified Data.Aeson as JSON
-import qualified Data.Aeson.Types as JSON
 import qualified Data.HashMap.Strict as M       -- unordered-containers
 import qualified Data.Vector as V               -- vector
 
