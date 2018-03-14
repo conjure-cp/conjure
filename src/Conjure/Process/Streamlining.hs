277abab499bf5685377b79df79ed15b3287640fa
Özgür Akgün <ozgurakgun@gmail.com>
2018-03-14 19:47:45 +0000

    reorder and group streamliners by type

--- src/Conjure/Process/Streamlining.hs
+++ src/Conjure/Process/Streamlining.hs
@@ -35,74 +35,45 @@ type StreamlinerGen m = Expression -> m Streamliner
 streamlinersForSingleVariable :: (MonadFail m, NameGen m) => StreamlinerGen m
 streamlinersForSingleVariable x = concatMapM ($ x)
     [ intOdd
-     , intEven
-     , setAll streamlinersForSingleVariable
-     , setHalf streamlinersForSingleVariable
-     , setMost  streamlinersForSingleVariable
-     , approxHalf streamlinersForSingleVariable
-     , quasiRegular
-     , commutative
-     , nonCommutative
-     , associative
-     , onRange streamlinersForSingleVariable
-     , onDefined streamlinersForSingleVariable
-     --, prefix streamlinersForSingleVariable
-     --, postfix streamlinersForSingleVariable
-     , parts streamlinersForSingleVariable
-     , smallestFirst
-     , largestFirst
-     , intLowerHalf
-     , intUpperHalf
-     , monotonicallyIncreasing
-     , monotonicallyDecreasing
+    , intEven
+
+    , setAll streamlinersForSingleVariable
+    , setHalf streamlinersForSingleVariable
+    , setMost  streamlinersForSingleVariable
+    , approxHalf streamlinersForSingleVariable
+
+    , commutative
+    , nonCommutative
+    , associative
+    , onRange streamlinersForSingleVariable
+    , onDefined streamlinersForSingleVariable
+    --, prefix streamlinersForSingleVariable
+    --, postfix streamlinersForSingleVariable
+    , smallestFirst
+    , largestFirst
+    , intLowerHalf
+    , intUpperHalf
+    , monotonicallyIncreasing
+    , monotonicallyDecreasing
+
+    , parts streamlinersForSingleVariable
+    , quasiRegular
     ]
 
 
---given an integer expression (which can be a reference to a decision variable),
---generate a constraint forcing it to be odd
+------------------------------------------------------------------------------
+-- Integers
+------------------------------------------------------------------------------
+
+
+-- given an integer expression (which can be a reference to a decision variable),
+-- generate a constraint forcing it to be odd
 intOdd :: (MonadFail m, NameGen m) => StreamlinerGen m
 intOdd x = do
     ty <- typeOf x
     if typeUnify ty TypeInt
         then return $ return [essence| &x % 2 = 1 |]
         else return []
-
-
-
-
-quasiRegular :: (MonadFail m, NameGen m) => StreamlinerGen m
-quasiRegular x = do
-    dom <- domainOf x
-    case dom of
-            DomainPartition _ _ partitionDomain -> do
-                return $ return [essence| minPartSize(&x, |participants(&x)| / |parts(&x)| - 1) /\ maxPartSize(&x, |participants(&x)|/ |parts(&x)| + 1) |]
-            _ -> return []
---
-intLowerHalf :: (MonadFail m, NameGen m) => StreamlinerGen m
-intLowerHalf x = do
-    ty <- typeOf x
-    dom <- domainOf x
-
-    case dom of
-        DomainInt [RangeBounded lower upper] -> do
-            traceM $ show $ "DomainInt " <+> pretty (lower, upper)
-            if typeUnify ty TypeInt
-                    then return $ return [essence| &x < 1 + (&upper -1) /2 |]
-                    else return []
-        _ -> return []
-
-intUpperHalf :: (MonadFail m, NameGen m) => StreamlinerGen m
-intUpperHalf x = do
-    ty <- typeOf x
-    dom <- domainOf x
-
-    case dom of
-        DomainInt [RangeBounded lower upper] -> do
-            traceM $ show $ "DomainInt " <+> pretty (lower, upper)
-            if typeUnify ty TypeInt
-                    then return $ return [essence| &x > 1 + (&upper -1) /2 |]
-                    else return []
-        _ -> return []
 
 
 intEven :: MonadFail m => StreamlinerGen m
@@ -112,6 +83,100 @@ intEven x = do
         then return $ return [essence| &x % 2 = 0 |]
         else return []
 
+
+intLowerHalf :: (MonadFail m, NameGen m) => StreamlinerGen m
+intLowerHalf x = do
+    ty <- typeOf x
+    dom <- domainOf x
+    case dom of
+        DomainInt [RangeBounded lower upper] -> do
+            -- traceM $ show $ "DomainInt " <+> pretty (lower, upper)
+            if typeUnify ty TypeInt
+                then return $ return [essence| &x < 1 + (&upper -1) /2 |]
+                else return []
+        _ -> return []
+
+
+intUpperHalf :: (MonadFail m, NameGen m) => StreamlinerGen m
+intUpperHalf x = do
+    ty <- typeOf x
+    dom <- domainOf x
+    case dom of
+        DomainInt [RangeBounded lower upper] -> do
+            -- traceM $ show $ "DomainInt " <+> pretty (lower, upper)
+            if typeUnify ty TypeInt
+                then return $ return [essence| &x > 1 + (&upper -1) /2 |]
+                else return []
+        _ -> return []
+
+
+------------------------------------------------------------------------------
+-- Sets
+------------------------------------------------------------------------------
+
+
+setAll :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
+setAll innerStreamliner x = do
+    dom <- domainOf x
+    case dom of
+        DomainSet _ _ innerDom -> do
+            nm <- nextName "q"
+            traceM $ show $ "setAll nm" <+> pretty nm
+            let pat = Single nm
+                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
+            innerConstraints <- innerStreamliner ref
+            --traceM $ show $ "maybeInnerConstraint" <+> vcat (map pretty innerConstraints)
+            forM innerConstraints $ \ innerConstraint ->
+                    return [essence| forAll &pat in &x . &innerConstraint |]
+        _ -> return []
+
+
+setMost :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
+setMost innerStreamliner x = do
+    dom <- domainOf x
+    case dom of
+        DomainSet _ (SetAttr (SizeAttr_Size size)) innerDom -> do
+            nm <- nextName "q"
+            let pat = Single nm
+                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
+            innerConstraints <- innerStreamliner ref
+            forM innerConstraints $ \ innerConstraint ->
+                return [essence| 1 >= sum &pat in &x . toInt(&innerConstraint) |]
+        _ -> return []
+
+
+setHalf ::(MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
+setHalf innerStreamliner x = do
+    dom <- domainOf x
+    case dom of
+        DomainSet _ (SetAttr (SizeAttr_Size size)) innerDom -> do
+            nm <- nextName "q"
+            let pat = Single nm
+                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
+            innerConstraints <- innerStreamliner ref
+            forM innerConstraints $ \ innerConstraint ->
+                    return [essence| &size / 2 = (sum &pat in &x . toInt(&innerConstraint)) |]
+        _ -> return []
+
+
+approxHalf:: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
+approxHalf innerStreamliner x = do
+    dom <- domainOf x
+    case dom of
+        DomainSet _ (SetAttr (SizeAttr_Size size)) innerDom -> do
+            nm <- nextName "q"
+            let pat = Single nm
+                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
+            innerConstraints <- innerStreamliner ref
+            forM innerConstraints $ \ innerConstraint ->
+                return [essence| (&size/2) + 1 >= (sum &pat in &x . toInt(&innerConstraint)) /\ (&size/2 -1) <= (sum &pat in &x . toInt(&innerConstraint)) |]
+
+        _ -> return []
+
+
+------------------------------------------------------------------------------
+-- Functions
+------------------------------------------------------------------------------
 
 
 monotonicallyIncreasing :: (MonadFail m, NameGen m) => StreamlinerGen m
@@ -131,7 +196,7 @@ monotonicallyIncreasing x = do
                                      &i < &j -> &x(&i) <= &x(&j)|]
 
         _ -> return []
---
+
 monotonicallyDecreasing :: (MonadFail m, NameGen m) => StreamlinerGen m
 monotonicallyDecreasing x = do
     dom <- domainOf x
@@ -150,7 +215,7 @@ monotonicallyDecreasing x = do
                                      &i < &j -> &x(&i) >= &x(&j)|]
 
         _ -> return []
---
+
 smallestFirst :: (MonadFail m, NameGen m) => StreamlinerGen m
 smallestFirst x = do
     dom <- domainOf x
@@ -164,7 +229,7 @@ smallestFirst x = do
             return $ return [essence| forAll &ipat in defined(&x).
                                 &x(min(defined(&x))) <= &x(&i)|]
 
---
+
 largestFirst :: (MonadFail m, NameGen m) => StreamlinerGen m
 largestFirst x = do
     dom <- domainOf x
@@ -172,57 +237,11 @@ largestFirst x = do
          DomainFunction _ attrs (DomainInt _) (DomainInt _) -> do
 
             traceM $ show $ "Largest First"
-            i <- nextName "q"
             (ipat, i) <- quantifiedVar
 
 
             return $ return [essence| forAll &ipat in defined(&x).
                                 &x(max(defined(&x))) >= &x(&i)|]
-
---
-setHalf ::(MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
-setHalf innerStreamliner x = do
-    dom <- domainOf x
-    case dom of
-        DomainSet _ (SetAttr (SizeAttr_Size size)) innerDom -> do
-            nm <- nextName "q"
-            let pat = Single nm
-                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
-            innerConstraints <- innerStreamliner ref
-            forM innerConstraints $ \ innerConstraint ->
-                    return [essence| &size / 2 = (sum &pat in &x . toInt(&innerConstraint)) |]
-        _ -> return []
-
-
-
-
-setMost :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
-setMost innerStreamliner x = do
-    dom <- domainOf x
-    case dom of
-        DomainSet _ (SetAttr (SizeAttr_Size size)) innerDom -> do
-            nm <- nextName "q"
-            let pat = Single nm
-                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
-            innerConstraints <- innerStreamliner ref
-            forM innerConstraints $ \ innerConstraint ->
-                return [essence| 1 >= sum &pat in &x . toInt(&innerConstraint) |]
-        _ -> return []
-
-
-approxHalf:: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
-approxHalf innerStreamliner x = do
-    dom <- domainOf x
-    case dom of
-        DomainSet _ (SetAttr (SizeAttr_Size size)) innerDom -> do
-            nm <- nextName "q"
-            let pat = Single nm
-                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
-            innerConstraints <- innerStreamliner ref
-            forM innerConstraints $ \ innerConstraint ->
-                return [essence| (&size/2) + 1 >= (sum &pat in &x . toInt(&innerConstraint)) /\ (&size/2 -1) <= (sum &pat in &x . toInt(&innerConstraint)) |]
-
-        _ -> return []
 
 
 commutative :: (MonadFail m, NameGen m) => StreamlinerGen m
@@ -241,6 +260,7 @@ commutative x = do
                     return $ return [essence| forAll(&ipat,&jpat) in defined(&x). &x((&i,&j)) = &x((&j,&i)) |]
                 False -> return []
         _ -> return []
+
 
 nonCommutative:: (MonadFail m, NameGen m) => StreamlinerGen m
 nonCommutative x = do
@@ -278,23 +298,6 @@ associative x = do
         _ -> return []
 
 
-
-setAll :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
-setAll innerStreamliner x = do
-    dom <- domainOf x
-    case dom of
-        DomainSet _ _ innerDom -> do
-            nm <- nextName "q"
-            traceM $ show $ "setAll nm" <+> pretty nm
-            let pat = Single nm
-                ref = Reference nm (Just (DeclNoRepr Find nm innerDom NoRegion))
-            innerConstraints <- innerStreamliner ref
-            --traceM $ show $ "maybeInnerConstraint" <+> vcat (map pretty innerConstraints)
-            forM innerConstraints $ \ innerConstraint ->
-                    return [essence| forAll &pat in &x . &innerConstraint |]
-        _ -> return []
-
---
 onRange :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
 onRange innerStreamliner x = do
     traceM $ show $ "onRange" <+> pretty x
@@ -320,8 +323,8 @@ onRange innerStreamliner x = do
 
         _ -> return []
 
---
---
+
+
 -- diagonal :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
 -- diagonal innerStreamliner x = do
 --     traceM $ show $ "diagnoal" <+> pretty x
@@ -333,10 +336,10 @@ onRange innerStreamliner x = do
 --                     nm <- nextname "fx"
 --                     let auxfunction = domainfunction () def a domto
 --                         ref =  reference nm (just (declnorepr find nm auxfunction noregion))
---
+
 --                     i <- nextname "q"
 --                     (ipat, i) <- quantifiedvar
---
+
 --                     innerconstraints <- innerstreamliner ref
 --                     return $ return [essence| find &ref: function &a --> &domto such that forall &ipat: &a. (&i, &i) in defined(&x) -> &ref(&i) = &x((&i,&i)),
 --                                     forall &ipat: &a . &i in defined(&ref) -> &ref(&i) = &x((&i,&i)) |]
@@ -344,7 +347,53 @@ onRange innerStreamliner x = do
 --                     return []
 --         _ -> return []
 
+-- 
+-- prefix :: (MonadFail m, NameGen m) => StreamlinerGen m ->  StreamlinerGen m
+-- prefix innerStreamliner x = do
+--     traceM $ show $ "prefix"
+--     dom <- domainOf x
+--     case dom of
+--         DomainFunction () _ (DomainInt [RangeBounded lb up]) innerDomTo -> do
+--             case x of
+--                 Reference nm (Just (DeclNoRepr Find _ domain NoRegion)) -> do
 
+--                     innerConstraints <- innerStreamliner x
+
+--                     let
+--                         replaceWithRangeOfX (Reference n _) | n == nm = [essence| restrict(&x,`int(&lb..(&up-1))`) |]
+--                         replaceWithRangeOfX p = p
+
+--                 --  traceM $ show $ "innerConstraints 1" <+> vcat (map pretty innerConstraints)
+--                 -- traceM $ show $ "innerConstraints 2" <+> vcat (map pretty (transformBi replaceWithRangeOfX innerConstraints))
+
+--                     return (transformBi replaceWithRangeOfX innerConstraints)
+--                 _ -> return []
+
+--         _ -> return []
+-- 
+
+-- postfix :: (MonadFail m, NameGen m) => StreamlinerGen m ->  StreamlinerGen m
+-- postfix innerStreamliner x = do
+--     traceM $ show $ "postfix"
+--     dom <- domainOf x
+--     case dom of
+--         DomainFunction () _ (DomainInt [RangeBounded lb up]) innerDomTo -> do
+--             case x of
+--                 Reference nm (Just (DeclNoRepr Find _ domain NoRegion)) -> do
+
+--                     innerConstraints <- innerStreamliner x
+
+--                     let
+--                         replaceWithRangeOfX (Reference n _) | n == nm = [essence| restrict(&x,`int((&lb+1)..&up)`) |]
+--                         replaceWithRangeOfX p = p
+
+--                 --  traceM $ show $ "innerConstraints 1" <+> vcat (map pretty innerConstraints)
+--                 -- traceM $ show $ "innerConstraints 2" <+> vcat (map pretty (transformBi replaceWithRangeOfX innerConstraints))
+
+--                     return (transformBi replaceWithRangeOfX innerConstraints)
+--                 _ -> return []
+
+--         _ -> return []
 
 
 
@@ -376,6 +425,10 @@ onDefined innerStreamliner x = do
         _ -> return []
 
 
+------------------------------------------------------------------------------
+-- Partitions
+------------------------------------------------------------------------------
+
 
 parts :: (MonadFail m, NameGen m) => StreamlinerGen m -> StreamlinerGen m
 parts innerStreamliner x = do
@@ -398,50 +451,14 @@ parts innerStreamliner x = do
 
         _ -> return []
 
--- --
--- prefix :: (MonadFail m, NameGen m) => StreamlinerGen m ->  StreamlinerGen m
--- prefix innerStreamliner x = do
---     traceM $ show $ "prefix"
---     dom <- domainOf x
---     case dom of
---         DomainFunction () _ (DomainInt [RangeBounded lb up]) innerDomTo -> do
---             case x of
---                 Reference nm (Just (DeclNoRepr Find _ domain NoRegion)) -> do
---
---                     innerConstraints <- innerStreamliner x
---
---                     let
---                         replaceWithRangeOfX (Reference n _) | n == nm = [essence| restrict(&x,`int(&lb..(&up-1))`) |]
---                         replaceWithRangeOfX p = p
---
---                 --  traceM $ show $ "innerConstraints 1" <+> vcat (map pretty innerConstraints)
---                 -- traceM $ show $ "innerConstraints 2" <+> vcat (map pretty (transformBi replaceWithRangeOfX innerConstraints))
---
---                     return (transformBi replaceWithRangeOfX innerConstraints)
---                 _ -> return []
---
---         _ -> return []
--- --
---
--- postfix :: (MonadFail m, NameGen m) => StreamlinerGen m ->  StreamlinerGen m
--- postfix innerStreamliner x = do
---     traceM $ show $ "postfix"
---     dom <- domainOf x
---     case dom of
---         DomainFunction () _ (DomainInt [RangeBounded lb up]) innerDomTo -> do
---             case x of
---                 Reference nm (Just (DeclNoRepr Find _ domain NoRegion)) -> do
---
---                     innerConstraints <- innerStreamliner x
---
---                     let
---                         replaceWithRangeOfX (Reference n _) | n == nm = [essence| restrict(&x,`int((&lb+1)..&up)`) |]
---                         replaceWithRangeOfX p = p
---
---                 --  traceM $ show $ "innerConstraints 1" <+> vcat (map pretty innerConstraints)
---                 -- traceM $ show $ "innerConstraints 2" <+> vcat (map pretty (transformBi replaceWithRangeOfX innerConstraints))
---
---                     return (transformBi replaceWithRangeOfX innerConstraints)
---                 _ -> return []
---
---         _ -> return []
+
+quasiRegular :: (MonadFail m, NameGen m) => StreamlinerGen m
+quasiRegular x = do
+    dom <- domainOf x
+    case dom of
+        DomainPartition _ _ partitionDomain -> do
+            return $ return [essence|
+                minPartSize(&x, |participants(&x)| / |parts(&x)| - 1) /\
+                maxPartSize(&x, |participants(&x)|/ |parts(&x)| + 1)
+            |]
+        _ -> return []
