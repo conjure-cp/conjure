6561f64f63c98459376d6c03686b4fd44f75bdfd
Özgür Akgün <ozgurakgun@gmail.com>
2018-04-25 22:35:06 +0100

    Install stack to ~/.local/bin so we don't require sudo

--- etc/build/install-stack.sh
+++ etc/build/install-stack.sh
@@ -6,12 +6,12 @@ set -o nounset
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source ${SCRIPT_DIR}/default_envvars.sh
 
-if ! which stack ; then
-    echo "Installing Haskell build tool stack"
-    if which curl; then
-        curl -sSL https://get.haskellstack.org/ | sh
-    elif which wget; then
-        wget -qO- https://get.haskellstack.org/ | sh
+if ! which stack 2> /dev/null > /dev/null; then
+    echo "Installing Haskell build tool stack to ~/.local/bin"
+    if which curl 2> /dev/null > /dev/null; then
+        curl -sSL https://get.haskellstack.org/ | sh -s - -d ~/.local/bin
+    elif which wget 2> /dev/null > /dev/null; then
+        wget -qO- https://get.haskellstack.org/ | sh -s - -d ~/.local/bin
     else
         echo "You seem to have neither curl nor wget on this computer."
         echo "Cannot install stack without one of them."
