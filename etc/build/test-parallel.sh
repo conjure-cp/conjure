da9db9a1fe11f516e361d9ceb30232b837c97c91
Özgür Akgün <ozgurakgun@gmail.com>
2018-05-06 10:52:23 +0100

    Use the conjure-testing executable built by stack

--- etc/build/test-parallel.sh
+++ etc/build/test-parallel.sh
@@ -9,13 +9,17 @@ echo "conjure/domainSize"       >> test_commands
 echo "conjure/representations"  >> test_commands
 echo "conjure/type-checking"    >> test_commands
 
-dist/build/conjure-testing/conjure-testing --limit-time ${LIMIT_TIME} -l > all-tests-list
+echo "Using conjure-testing executable from the following location."
+ls .stack-work/dist/*/*/build/conjure-testing/conjure-testing
+cp .stack-work/dist/*/*/build/conjure-testing/conjure-testing .
+
+./conjure-testing --limit-time ${LIMIT_TIME} -l > all-tests-list
 cat all-tests-list | grep 'conjure/custom'     >> test_commands
 cat all-tests-list | grep 'conjure/exhaustive' >> test_commands
 rm all-tests-list
 
 function t {
-    dist/build/conjure-testing/conjure-testing --limit-time 0 -p "$1"
+    ./conjure-testing --limit-time 0 -p "$1"
 }
 export -f t
 
