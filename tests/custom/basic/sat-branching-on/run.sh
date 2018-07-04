79afb1861c7d7a936dbf57b30a5e2dc724093acb
Özgür Akgün <ozgurakgun@gmail.com>
2018-07-04 21:38:57 +0100

    Consistent sorting between operating systems

--- tests/custom/basic/sat-branching-on/run.sh
+++ tests/custom/basic/sat-branching-on/run.sh
@@ -3,10 +3,10 @@ rm -rf conjure-output
 
 conjure solve sbo.essence --number-of-solutions=all --copy-solutions=no --solver minion
 
-grep 'letting a' conjure-output/*.solution | cut -d ':' -f 2 | sort
+grep 'letting a' conjure-output/*.solution | cut -d ':' -f 2 | LC_ALL=C sort
 
 conjure solve sbo.essence --number-of-solutions=all --copy-solutions=no --solver lingeling
 
-grep 'letting a' conjure-output/*.solution | cut -d ':' -f 2 | sort
+grep 'letting a' conjure-output/*.solution | cut -d ':' -f 2 | LC_ALL=C sort
 
 rm -rf conjure-output
