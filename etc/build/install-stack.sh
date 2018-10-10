82d7017ba5e55d4ac50eaacc2378e7b68c16616c
Özgür Akgün <ozgurakgun@gmail.com>
2018-10-10 17:58:28 +0100

    Attempt downloading stack first, only if that fails try building it from source

--- etc/build/install-stack.sh
+++ etc/build/install-stack.sh
@@ -9,24 +9,34 @@ source ${SCRIPT_DIR}/default_envvars.sh
 export BIN_DIR=${BIN_DIR:-${HOME}/.local/bin}
 export CI=${CI:-false}
 
+function dlStack {
+    if [ `uname` = "Darwin" ] ; then
+        curl --insecure -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ${BIN_DIR}
+    else
+        curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ${BIN_DIR} '*/stack'
+    fi
+}
+export -f dlStack
+
 
 if ! which stack 2> /dev/null > /dev/null; then
     echo "Installing Haskell build tool stack to ${BIN_DIR}"
     if $CI; then
-        if [ `uname` = "Darwin" ] ; then
-            curl --insecure -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
-        else
-            curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
-        fi
+        dlStack
     else
-        if which curl 2> /dev/null > /dev/null; then
-            curl -sSL https://get.haskellstack.org/ | sh -s - -d ${BIN_DIR}
-        elif which wget 2> /dev/null > /dev/null; then
-            wget -qO- https://get.haskellstack.org/ | sh -s - -d ${BIN_DIR}
+        if dlStack ; then
+            echo "Downloaded stack."
         else
-            echo "You seem to have neither curl nor wget on this computer."
-            echo "Cannot install stack without one of them."
-            exit 1
+            echo "Couldn't downloaded stack, attempting build from source."
+            if which curl 2> /dev/null > /dev/null; then
+                curl -sSL https://get.haskellstack.org/ | sh -s - -d ${BIN_DIR}
+            elif which wget 2> /dev/null > /dev/null; then
+                wget -qO- https://get.haskellstack.org/ | sh -s - -d ${BIN_DIR}
+            else
+                echo "You seem to have neither curl nor wget on this computer."
+                echo "Cannot install stack without one of them."
+                exit 1
+            fi
         fi
     fi
 fi
