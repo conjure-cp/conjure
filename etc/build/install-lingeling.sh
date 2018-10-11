85af98abff5da363329853c27fd5f310c41d65e1
András Salamon <Andras.Salamon@st-andrews.ac.uk>
2018-10-11 20:22:11 +0100

    update lingeling from 2014 to 2016

--- etc/build/install-lingeling.sh
+++ etc/build/install-lingeling.sh
@@ -8,11 +8,13 @@ export BIN_DIR=${BIN_DIR:-${HOME}/.local/bin}
 rm -rf ${BIN_DIR}/tmp-install-lingeling
 mkdir ${BIN_DIR}/tmp-install-lingeling
 pushd ${BIN_DIR}/tmp-install-lingeling
-wget --no-check-certificate -c http://fmv.jku.at/lingeling/lingeling-ayv-86bf266-140429.zip
-unzip lingeling-ayv-86bf266-140429.zip
-./build.sh
+wget --no-check-certificate -c http://fmv.jku.at/lingeling/lingeling-bbc-9230380-160707.tar.gz
+tar xzf lingeling-bbc-9230380-160707.tar.gz
+cd lingeling-bbc-9230380-160707
+./configure.sh
+make
 mkdir -p ${BIN_DIR}
-cp binary/lingeling ${BIN_DIR}/lingeling
+cp lingeling ${BIN_DIR}/lingeling
 echo "lingeling executable is at ${BIN_DIR}/lingeling"
 ls -l ${BIN_DIR}/lingeling
 popd
