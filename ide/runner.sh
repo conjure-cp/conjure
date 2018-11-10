1512aae3c314a64dba84046ad52ca05e5c67e439
Tom Oliver <to26@st-andrews.ac.uk>
2018-11-10 17:01:47 +0000

    modified runner

--- ide/runner.sh
+++ ide/runner.sh
@@ -1,12 +1,15 @@
-for d in ~/EssenceCatalog/problems/*; do
+for d in /scratch/EssenceCatalog/problems/*; do
   if [ -d "$d" ]; then
 
     model=$(ls $d/*.essence | head -1)
     # echo $model
     params=$(find $d -type f -name "*.param")
 
+    count=0
+
     for p in $params; do
-        echo $model
+	((count++))
+       # echo $model
         echo $p
         newName=$(dirname $model)/$(basename $model .essence)~$(basename $p .param)
         conjureOutput=$newName
@@ -14,7 +17,19 @@ for d in ~/EssenceCatalog/problems/*; do
         # echo $conjureOutput
         treeName=$conjureOutput/"out.json"
         # echo $treeName
-        conjure solve $model $p -o $conjureOutput --copy-solutions=off --savilerow-options='-S0 -O0 -active-cse -reduce-domains -aggregate' --solver-options "-dumptreejson "$treeName
+
+	#pwd &
+        conjure solve $model $p -o $conjureOutput --copy-solutions=off --savilerow-options='-S0 -O3' --solver-options "-dumptreejson "$treeName &
+	
+	#echo $count
+
+	if (($count > 20));	then
+		echo "waiting!!!!!!!!!!!!!!!!!!!!!!!!"
+		wait
+		count=0
+	fi
+	
     done
   fi
 done
+
