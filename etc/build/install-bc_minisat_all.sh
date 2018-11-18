140a621d6309900a289f744226d9d8e4b219335e
Özgür Akgün <ozgurakgun@gmail.com>
2018-11-18 21:40:44 +0000

    make solvers -- use -j when building the solvers

--- etc/build/install-bc_minisat_all.sh
+++ etc/build/install-bc_minisat_all.sh
@@ -11,7 +11,7 @@ pushd ${BIN_DIR}/tmp-install-bc_minisat_all
 wget --no-check-certificate -c http://www.sd.is.uec.ac.jp/toda/code/bc_minisat_all-1.1.2.tar.gz
 tar zxf bc_minisat_all-1.1.2.tar.gz
 cd bc_minisat_all-1.1.2/
-make bc_minisat_all_release
+make -j bc_minisat_all_release
 mv bc_minisat_all_release ${BIN_DIR}/bc_minisat_all_release
 echo "bc_minisat_all executable is at ${BIN_DIR}/bc_minisat_all_release"
 ls -l ${BIN_DIR}/bc_minisat_all_release
