e75acd89cef3b62699b87e19b6edfd9c57ff17d7
Fraser Dunlop <frjd2@8afbf4e9.st-andrews.ac.uk>
2018-11-19 10:26:25 +0000

    Some unnamed tests

--- src/Conjure/UI/Model.hs
+++ src/Conjure/UI/Model.hs
@@ -1077,17 +1077,7 @@ paramRules =
 
 verticalRules :: [Rule]
 verticalRules =
-    [ 
-      Vertical.Permutation.AsFunction.rule_Permute_Comprehension_Tuples
-    , Vertical.Permutation.AsFunction.rule_Relation_Permute
-    , Vertical.Permutation.AsFunction.rule_Relation_Permute_Comprehension
-    , Vertical.Permutation.AsFunction.rule_Set_Permute
-    , Vertical.Permutation.AsFunction.rule_Tuple_Permute
-    , Vertical.Permutation.AsFunction.rule_Tuple_Permute_Comprehension
-    , Vertical.Permutation.AsFunction.rule_Matrix_Permute
-    , Vertical.Permutation.AsFunction.rule_Matrix_Permute_Comprehension
-
-    , Vertical.Tuple.rule_Tuple_Eq
+    [ Vertical.Tuple.rule_Tuple_Eq
     , Vertical.Tuple.rule_Tuple_Neq
     , Vertical.Tuple.rule_Tuple_Leq
     , Vertical.Tuple.rule_Tuple_Lt
@@ -1191,6 +1181,18 @@ verticalRules =
 
     , Vertical.Partition.PartitionAsSet.rule_Comprehension
     , Vertical.Partition.Occurrence.rule_Comprehension
+
+
+    , Vertical.Permutation.AsFunction.rule_Permute_Comprehension_Tuples
+    , Vertical.Permutation.AsFunction.rule_Relation_Permute
+    , Vertical.Permutation.AsFunction.rule_Relation_Permute_Comprehension
+    , Vertical.Permutation.AsFunction.rule_Set_Permute
+    , Vertical.Permutation.AsFunction.rule_Tuple_Permute
+    , Vertical.Permutation.AsFunction.rule_Tuple_Permute_Comprehension
+    , Vertical.Permutation.AsFunction.rule_Matrix_Permute
+    , Vertical.Permutation.AsFunction.rule_Matrix_Permute_Comprehension
+
+
 
 
     ]
---
+++ tests/custom/permutations/basic/34/run.sh
@@ -0,0 +1,3 @@
+conjure solve *.essence *.param --number-of-solutions=60
+cat conjure-output/*.solution
+rm -rf conjure-output *.solution
