670869f23d8a89fa3c403da0669d3834fcc4afc1
Özgür Akgün <ozgurakgun@gmail.com>
2019-01-13 14:47:42 +0000

    Change typeOf to take an implicit parameter.
    This parameter will decide the mode of the type checker. Currently there are two modes: StronglyTyped and RelaxedIntegerTags. StronglyTyped is used for user input. RelaxedIntegerTags is for internal use only and it ignores the integer tags during type checking.

--- src/Conjure/Language/TypeOf.hs
+++ src/Conjure/Language/TypeOf.hs
@@ -1,4 +1,7 @@
-module Conjure.Language.TypeOf where
+module Conjure.Language.TypeOf
+    ( TypeOf(..)
+    , TypeCheckerMode(..)
+    ) where
 
 -- conjure
 import Conjure.Prelude
@@ -6,4 +9,14 @@ import Conjure.Language.Type
 
 
 class TypeOf a where
-    typeOf :: MonadFail m => a -> m Type
+    typeOf :: (MonadFail m, ?typeCheckerMode :: TypeCheckerMode) => a -> m Type
+
+
+-- typeOf takes an implicit parameter.
+
+-- This parameter will decide the mode of the type checker.
+-- Currently there are two modes: StronglyTyped and RelaxedIntegerTags.
+-- StronglyTyped is used for user input.
+-- RelaxedIntegerTags is for internal use only and it ignores the integer tags during type checking.
+
+data TypeCheckerMode = StronglyTyped | RelaxedIntegerTags
