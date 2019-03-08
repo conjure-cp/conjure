af78b519492ef6bbf74ee535958358293fa5ac88
Tom <to26@st-andrews.ac.uk>
2019-03-08 11:05:36 +0000

    fixed dbverifier

--- ide/src/nim/src/verifyDB.nim
+++ ide/src/nim/src/verifyDB.nim
@@ -1,9 +1,54 @@
-import  os, db_sqlite, strutils, json
+import os, db_sqlite, strutils, json
+
+proc verifyDomains(obj: JsonNode, db: DbConn) =
+    for row in db.fastRows(sql("select name, lower, upper from Domain where nodeId = ? "), obj["Node"]):
+        let name = row[0]
+        let lower = row[1]
+        let upper = row[2]
+
+        # echo name
+        # echo row
+        # echo upper
+
+        let bounds = obj["Domains"][name].getElems()
+
+        # echo bounds
+
+        var jsonLower: string
+        var jsonUpper: string
+
+
+        if (bounds.len() > 1):
+            var min = high(int)
+            var max = low(int)
+
+            for subArray in bounds:
+                let first = subArray[0].getInt()
+                let second = subArray[1].getInt()
+
+                if first < min:
+                    min = first
+                
+                if second > max:
+                    max = second
+
+            jsonLower = $min
+            jsonUpper = $max
+        else:
+            jsonLower = $bounds[0].getElems()[0].getInt()
+            jsonUpper = $bounds[0].getElems()[1].getInt()
+
+        # echo jsonLower
+
+        assert(lower == jsonLower)
+        assert(upper == jsonUpper)
+
 
 proc walkTree(obj: JsonNode, db: DbConn, wentLeft: bool, pId: string) =
 
     if (obj.hasKey("Node")):
-        echo obj["Node"]
+        echo "-------------------------------------------------Node ", obj["Node"]
+        verifyDomains(obj, db)
 
         let row = db.getRow(sql("select * from Node where nodeId = ? "), obj["Node"])
 
@@ -28,6 +73,7 @@ proc walkTree(obj: JsonNode, db: DbConn, wentLeft: bool, pId: string) =
 
         if (obj.hasKey("isSolution")):
             assert(isSolution == "1")
+
             
     # echo obj
     if (obj.hasKey("left")):
@@ -35,6 +81,8 @@ proc walkTree(obj: JsonNode, db: DbConn, wentLeft: bool, pId: string) =
 
     if (obj.hasKey("right")):
         walkTree(obj["right"], db, false, $obj["Node"].getInt())
+
+
 
 
 if paramCount() != 2:
