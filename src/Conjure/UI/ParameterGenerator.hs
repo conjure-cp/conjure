8906092c720e95c716e05642fff28966db5f1618
Özgür Akgün <ozgurakgun@gmail.com>
2019-03-27 14:21:30 +0000

    paramgen: first go at ints

--- src/Conjure/UI/ParameterGenerator.hs
+++ src/Conjure/UI/ParameterGenerator.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Conjure.UI.ParameterGenerator where
 
 import Conjure.Prelude
 import Conjure.Language
 import Conjure.Language.NameResolution ( resolveNames )
-import Conjure.Language.Expression.DomainSizeOf ( domainSizeOf )
+-- import Conjure.Language.Expression.DomainSizeOf ( domainSizeOf )
 
 
 -- | This doesn't do anything to do with correcting categories at the moment, it should.
@@ -15,21 +17,17 @@ import Conjure.Language.Expression.DomainSizeOf ( domainSizeOf )
 --      find s : set (minSize 1, maxSize 10) of int(1..10)
 --      such that n = |s|
 --   (Just dropping wrong category stuff from attribute list isn't acceptable, because mset.)
-parameterGenerator
-    :: ( MonadLog m
-       , MonadFail m
-       , MonadUserError m
-       , ?typeCheckerMode :: TypeCheckerMode
-       ) => Model -> m Model
+parameterGenerator ::
+    MonadLog m =>
+    MonadFail m =>
+    MonadUserError m =>
+    (?typeCheckerMode :: TypeCheckerMode) =>
+    Model -> m Model
 parameterGenerator model = runNameGen () (resolveNames model) >>= core
     where
         core m = do
-            (outStatements, errs) <- runWriterT $ forM (mStatements m) $ \ st -> case st of
-                Declaration (FindOrGiven Given nm dom) ->
-                    case domainSizeOf dom of
-                        Nothing -> tell [(nm, dom)] >> return []
-                        Just (_ :: Expression)
-                                -> return [Declaration (FindOrGiven Find nm dom)]
+            outStatements <- forM (mStatements m) $ \ st -> case st of
+                Declaration (FindOrGiven Given nm dom) -> pgOnDomain nm dom
                 Declaration (FindOrGiven Find  _  _  ) -> return []
                 Declaration       {}                   -> return [st]
                 SearchOrder       {}                   -> return []
@@ -37,9 +35,79 @@ parameterGenerator model = runNameGen () (resolveNames model) >>= core
                 Where             xs                   -> return [SuchThat xs]
                 Objective         {}                   -> return []
                 SuchThat          {}                   -> return []
-            if null errs
-                then return m { mStatements = concat outStatements }
-                else userErr1 $ vcat $ "Some parameters have infinite domains."
-                                     : [ pretty nm <> ":" <++> pretty dom
-                                       | (nm, dom) <- errs
-                                       ]
+            let
+                usesMININT = not $ null [ () | Reference "MININT" _ <- universeBi outStatements ]
+                usesMAXINT = not $ null [ () | Reference "MAXINT" _ <- universeBi outStatements ]
+                addMinMaxInt stmts =
+                    [ Declaration (FindOrGiven Given "MININT" (DomainInt TagInt [])) | usesMININT ] ++
+                    [ Declaration (FindOrGiven Given "MAXINT" (DomainInt TagInt [])) | usesMAXINT ] ++
+                    stmts
+            return m { mStatements = addMinMaxInt (concat outStatements) }
+
+
+pgOnDomain ::
+    MonadUserError m =>
+    Name ->
+    Domain () Expression ->
+    m [Statement]
+pgOnDomain nm dom =
+    case dom of
+        DomainInt t _ -> do
+            lb <- minOfIntDomain dom
+            ub <- maxOfIntDomain dom
+            let nmMiddle = nm `mappend` "_middle"
+            let nmDelta  = nm `mappend` "_delta"
+            let x = Reference nm Nothing
+            let middle = Reference nmMiddle Nothing
+            let delta = Reference nmDelta Nothing
+            return
+                [ Declaration (FindOrGiven Given nmMiddle (DomainInt t [RangeBounded lb ub]))
+                , Declaration (FindOrGiven Given nmDelta  (DomainInt t [RangeBounded lb ub]))
+                , Declaration (FindOrGiven Find  nm       (DomainInt t [RangeBounded lb ub]))
+                , SuchThat [ [essence| &x >= &middle - &delta |]
+                           , [essence| &x <= &middle + &delta |]
+                           ]
+                ]
+        _ -> userErr1 $ "Unhandled domain:" <++> vcat [ pretty dom
+                                                      , pretty (show dom)
+                                                      ]
+
+
+-- helper functions
+
+minInt :: Expression
+minInt = Reference "MININT" Nothing
+
+maxInt :: Expression
+maxInt = Reference "MAXINT" Nothing
+
+
+minOfIntDomain :: MonadUserError m => Domain () Expression -> m Expression
+minOfIntDomain (DomainInt _ []) = return minInt
+minOfIntDomain (DomainInt _ [r]) = minOfIntRange r
+minOfIntDomain (DomainInt _ rs) = do
+    xs <- mapM minOfIntRange rs
+    return $ make opMax $ fromList xs
+minOfIntDomain d = userErr1 $ "Expected integer domain, but got:" <+> pretty d
+
+minOfIntRange :: Monad m => Range Expression -> m Expression
+minOfIntRange (RangeSingle lb) = return lb
+minOfIntRange (RangeLowerBounded lb) = return lb
+minOfIntRange (RangeBounded lb _) = return lb
+minOfIntRange _ = return minInt
+
+
+maxOfIntDomain :: MonadUserError m => Domain () Expression -> m Expression
+maxOfIntDomain (DomainInt _ []) = return maxInt
+maxOfIntDomain (DomainInt _ [r]) = maxOfIntRange r
+maxOfIntDomain (DomainInt _ rs) = do
+    xs <- mapM maxOfIntRange rs
+    return $ make opMin $ fromList xs
+maxOfIntDomain d = userErr1 $ "Expected integer domain, but got:" <+> pretty d
+
+maxOfIntRange :: Monad m => Range Expression -> m Expression
+maxOfIntRange (RangeSingle ub) = return ub
+maxOfIntRange (RangeUpperBounded ub) = return ub
+maxOfIntRange (RangeBounded _ ub) = return ub
+maxOfIntRange _ = return maxInt
+
---
+++ tests/custom/paramgen/int01/run.sh
@@ -0,0 +1,3 @@
+conjure parameter-generator int.essence --essence-out int-gen.essence
+cat int-gen.essence
+rm -f int-gen.essence
