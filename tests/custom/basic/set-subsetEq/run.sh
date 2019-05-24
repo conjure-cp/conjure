73ea845298716df08b30c17e10c3e3a83c672d7a
Özgür Akgün <ozgurakgun@gmail.com>
2019-05-24 11:46:43 +0100

    Run SR -O0 as well

--- tests/custom/basic/set-subsetEq/run.sh
+++ tests/custom/basic/set-subsetEq/run.sh
@@ -1,4 +1,6 @@
 rm -rf conjure-output
 conjure solve -ax --channelling=no --smart-filenames *.essence *.param
 grep Node conjure-output/*info
+conjure solve -ax --channelling=no --smart-filenames *.essence *.param --savilerow-options -O0
+grep Node conjure-output/*info
 rm -rf conjure-output
