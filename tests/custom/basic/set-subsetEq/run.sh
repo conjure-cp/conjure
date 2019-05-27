5b106b8a4399f236e91df9ad83a05db99a4616e1
Özgür Akgün <ozgurakgun@gmail.com>
2019-05-27 12:23:28 +0100

    Run more SR optimisation levels

--- tests/custom/basic/set-subsetEq/run.sh
+++ tests/custom/basic/set-subsetEq/run.sh
@@ -1,6 +1,23 @@
 rm -rf conjure-output
-conjure solve -ax --channelling=no --smart-filenames *.essence *.param
-grep Node conjure-output/*info
+
 conjure solve -ax --channelling=no --smart-filenames *.essence *.param --savilerow-options -O0
 grep Node conjure-output/*info
+grep -i cse conjure-output/*eprime
+rm conjure-output/*info
+
+conjure solve -ax --channelling=no --smart-filenames *.essence *.param --savilerow-options -O1
+grep Node conjure-output/*info
+grep -i cse conjure-output/*eprime
+rm conjure-output/*info
+
+conjure solve -ax --channelling=no --smart-filenames *.essence *.param --savilerow-options -O2
+grep Node conjure-output/*info
+grep -i cse conjure-output/*eprime
+rm conjure-output/*info
+
+conjure solve -ax --channelling=no --smart-filenames *.essence *.param --savilerow-options -O3
+grep Node conjure-output/*info
+grep -i cse conjure-output/*eprime
+rm conjure-output/*info
+
 rm -rf conjure-output
