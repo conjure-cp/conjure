119e8761497ce0b7eb9b0821807319071f64779d
Özgür Akgün <ozgurakgun@gmail.com>
2019-06-06 16:37:49 +0100

    Update test files

--- tests/custom/basic/sat-branching-on/run.sh
+++ tests/custom/basic/sat-branching-on/run.sh
@@ -4,6 +4,7 @@ rm -rf conjure-output
 conjure solve sbo.essence --number-of-solutions=all --copy-solutions=no --solver minion
 
 grep 'letting a' conjure-output/*.solution | cut -d ':' -f 2 | LC_ALL=C sort
+rm -rf conjure-output/*.solution
 
 conjure solve sbo.essence --number-of-solutions=all --copy-solutions=no --solver glucose
 
