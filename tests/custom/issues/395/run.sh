d8d81ee63659561abb161c69e97b32f42ef84dc1
Özgür Akgün <ozgurakgun@gmail.com>
2019-06-29 10:27:39 +0300

    Fix copy/paste error in the regression test for #395

--- tests/custom/issues/395/run.sh
+++ tests/custom/issues/395/run.sh
@@ -4,7 +4,7 @@ conjure modelling 1.essence ; head -n30 conjure-output/model000001.eprime
 conjure modelling 2.essence ; head -n30 conjure-output/model000001.eprime
 conjure modelling 3.essence ; head -n30 conjure-output/model000001.eprime
 
-conjure solve 1.essence m_is_3.param ; cat *.solution ; rm -f *.solution
+# conjure solve 1.essence m_is_3.param ; cat *.solution ; rm -f *.solution
 # conjure solve 2.essence m_is_3.param ; cat *.solution ; rm -f *.solution
 conjure solve 3.essence m_is_3.param ; cat *.solution ; rm -f *.solution
 
