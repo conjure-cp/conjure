39a33ba857af3e3d823614e8ea88cbddec0b17dc
Tom Oliver <to26@st-andrews.ac.uk>
2019-07-30 14:53:39 +0100

    fixed movementHelperTest

--- ide/src/webclient/__tests__/MovementHelperTest.ts
+++ ide/src/webclient/__tests__/MovementHelperTest.ts
@@ -1,16 +1,20 @@
 import { WhichTree } from "../src/modules/Node"
 import {
   coreOf3 as coreOf3Normal,
+  coreOf7 as coreOf7Normal,
   coreOf17 as coreOf17Normal,
+  coreOf21 as coreOf21Normal,
   coreOf27 as coreOf27Normal,
   descendantsOf28 as descendantsOf28Normal,
-  core as coreNormal
+  core as coreNormal8
 } from "./resources/normal-8"
 import {
   coreOf3 as coreOf3Sacbounds,
+  coreOf4 as coreOf4Sacbounds,
   coreOf6 as coreOf6Sacbounds,
+  coreOf7 as coreOf7Sacbounds,
   coreOf9 as coreOf9Sacbounds,
-  core as coreSacbounds
+  core as coreSacbounds8
 } from "./resources/sacbounds-8"
 import { loadAllDiffs } from "../src/modules/ForestHelper"
 import { bigToSmall } from "./resources/normalVSSacbounds-8"
@@ -26,16 +30,21 @@ describe("test goleftboyo", () => {
     fetchMock.resetMocks()
     fetchMock
       .once(JSON.stringify(coreOf3Normal))
-      .once(JSON.stringify(coreOf17Normal))
-      .once(JSON.stringify(coreOf27Normal))
       .once(JSON.stringify(coreOf3Sacbounds))
+      .once(JSON.stringify(coreOf7Normal))
+      .once(JSON.stringify(coreOf4Sacbounds))
+      .once(JSON.stringify(coreOf17Normal))
       .once(JSON.stringify(coreOf6Sacbounds))
+      .once(JSON.stringify(coreOf21Normal))
+      .once(JSON.stringify(coreOf7Sacbounds))
+      .once(JSON.stringify(coreOf27Normal))
       .once(JSON.stringify(coreOf9Sacbounds))
+
       .once(JSON.stringify(descendantsOf28Normal))
 
     let res = await loadAllDiffs(
       ["", "s"],
-      [coreNormal, coreSacbounds],
+      [coreNormal8, coreSacbounds8],
       bigToSmall,
       5000
     )
