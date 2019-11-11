6bcf3355db375db5ff8943c04db060888be7d76d
Özgür Akgün <ozgurakgun@gmail.com>
2019-11-11 20:45:42 +0000

    Remove debugging statements

--- etc/build/install.sh
+++ etc/build/install.sh
@@ -9,26 +9,21 @@ export BUILD_TESTS=${BUILD_TESTS:-false}
 export COVERAGE=${COVERAGE:-false}
 
 COMMAND="stack install --local-bin-path ${BIN_DIR}"
-echo $COMMAND
 
 if  [ ${GHC_VERSION} == "head" ]; then
     COMMAND="${COMMAND} --resolver nightly"
-    echo $COMMAND
 fi
 
 if ${BUILD_TESTS}; then
     COMMAND="${COMMAND} --test --no-run-tests"
-    echo $COMMAND
 fi
 
 if ${CI}; then
     COMMAND="${COMMAND} --no-terminal"
-    echo $COMMAND
 fi
 
 if ${COVERAGE}; then
     COMMAND="${COMMAND} --coverage"
-    echo $COMMAND
 fi
 
 echo "Running: ${COMMAND}"
