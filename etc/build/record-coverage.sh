a9a2c5f6839834e16cfb002cab292ad46c5a3deb
Özgür Akgün <ozgurakgun@gmail.com>
2019-11-12 21:01:51 +0000

    Remove the commit message bit

--- etc/build/record-coverage.sh
+++ etc/build/record-coverage.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
 
 if ${COVERAGE}; then
     # this is from https://cloudblogs.microsoft.com/opensource/2019/04/05/publishing-github-pages-from-azure-pipelines
@@ -14,7 +18,7 @@ if ${COVERAGE}; then
     git config --local user.name "Özgür Akgün"
     git config --local user.email "ozgurakgun@gmail.com"
     git add .
-    git commit . -m "Conjure commit: https://github.com/conjure-cp/conjure/commit/${SOURCE_VERSION} \n\n Commit message: \n\n ${SOURCE_VERSION_MSG}"
+    git commit . -m "Conjure commit: https://github.com/conjure-cp/conjure/commit/${SOURCE_VERSION}"
     git push origin master
 else
     echo "Skipping, COVERAGE is set to ${COVERAGE}"
