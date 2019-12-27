86f18fed0388b28948b4f4f00e2ab7bb49352fa3
Tom Oliver <tom.oliver@bizreach.co.jp>
2019-12-27 12:54:40 +0900

    slight modifications, tslin etc

--- ide/preprocessors__typescript-webpack/cypress/integration/spec.ts
+++ ide/preprocessors__typescript-webpack/cypress/integration/spec.ts
@@ -1,76 +1,83 @@
 // import function "add" from another TypeScript file
 
 describe('Homepage', () => {
-	beforeEach('works', () => {
-		cy.server() // enable response stubbing
+    beforeEach('works', () => {
+        cy.server() // enable response stubbing
 
-		const serverBase = 'http://localhost:4000'
+        const serverBase = 'http://localhost:4000'
 
-		cy.route('GET', `${serverBase}/config/files`, 'fixture:files.json')
-		cy.route('GET', `${serverBase}/config/caches`, 'fixture:caches.json')
-	})
+        cy.route('GET', `${serverBase}/config/files`, 'fixture:files.json')
+        cy.route('GET', `${serverBase}/config/caches`, 'fixture:caches.json')
+    })
 
-	describe('Conjure', () => {
-		beforeEach('expand', () => {
-			cy.visit('/')
-			cy.contains('Conjure').click()
-		})
+    describe('Conjure', () => {
+        beforeEach('expand', () => {
+            cy.visit('/')
+            cy.contains('Conjure').click()
+        })
 
-		it('Checks invalid field in conjure', () => {
-			cy.get('input[name="caches[0].config.conjureConfig.conjureTime"]').type('dasas')
-			cy.contains('Leave empty or specify an integer > 0')
-			cy.contains('Fix the errors first!')
-		})
+        it('Checks invalid field in conjure', () => {
+            cy.get('input[name="caches[0].config.conjureConfig.conjureTime"]').type('dasas')
+            cy.contains('Leave empty or specify an integer > 0')
+            cy.contains('Fix the errors first!')
+        })
 
-		it('Checks can select answers', () => {
-			cy.get('input[id="ChooseRepresentation-check"]').click()
-			cy.contains('x')
-			cy.contains('explicit')
-		})
-	})
+        it('Checks can select answers', () => {
+            cy.get('input[id="ChooseRepresentation-check"]').click()
+            cy.contains('x')
+            cy.contains('explicit')
+        })
+    })
 
-	describe('Savile Row', () => {
-		beforeEach('expand', () => {
-			cy.visit('/')
-			cy.contains('Savile Row').click()
-		})
+    describe('Savile Row', () => {
+        beforeEach('expand', () => {
+            cy.visit('/')
+            cy.contains('Savile Row').click()
+        })
 
-		it('Checks time field in SR', () => {
-			cy.get('input[name="caches[0].config.srConfig.srTime"]').type('blah1')
-			cy.contains('Leave empty or specify an integer > 0')
-			cy.contains('Fix the errors first!')
-		})
+        it('Checks time field in SR', () => {
+            cy.get('input[name="caches[0].config.srConfig.srTime"]').type('blah1')
+            cy.contains('Leave empty or specify an integer > 0')
+            cy.contains('Fix the errors first!')
+        })
 
-		it('Checks clause field in SR', () => {
-			cy.get('input[name="caches[0].config.srConfig.cnfLimit"]').type('dasas')
-			cy.contains('Leave empty or specify an integer > 0')
-			cy.contains('Fix the errors first!')
-		})
-	})
+        it('Checks clause field in SR', () => {
+            cy.get('input[name="caches[0].config.srConfig.cnfLimit"]').type('dasas')
+            cy.contains('Leave empty or specify an integer > 0')
+            cy.contains('Fix the errors first!')
+        })
+    })
 
-	describe('Minion', () => {
-		beforeEach('expand', () => {
-			cy.visit('/')
-			// Need to specify a otheriws it will pick up on the minionmappers option in the SR translation select box.
-			cy.contains('a', 'Minion').click()
-		})
+    describe('Minion', () => {
+        beforeEach('expand', () => {
+            cy.visit('/')
+            // Need to specify a otheriws it will pick up on the minionmappers option in the SR translation select box.
+            cy.contains('a', 'Minion').click()
+        })
 
-		it('Checks nodelimit field in Minion', () => {
-			cy.get('input[name="caches[0].config.minionConfig.nodeLimit"]').type('dasas')
-			cy.contains('Leave empty or specify an integer > 0')
-			cy.contains('Fix the errors first!')
-		})
+        it('Checks nodelimit field in Minion', () => {
+            cy.get('input[name="caches[0].config.minionConfig.nodeLimit"]').type('dasas')
+            cy.contains('Leave empty or specify an integer > 0')
+            cy.contains('Fix the errors first!')
+        })
 
-		it('Checks sollimit field in Minion', () => {
-			cy.get('input[name="caches[0].config.minionConfig.solLimit"]').type('dasas')
-			cy.contains('Leave empty or specify an integer > 0')
-			cy.contains('Fix the errors first!')
-		})
+        it('Checks sollimit field in Minion', () => {
+            cy.get('input[name="caches[0].config.minionConfig.solLimit"]').type('dasas')
+            cy.contains('Leave empty or specify an integer > 0')
+            cy.contains('Fix the errors first!')
+        })
 
-		it('Checks minionTimelimit field in Minion', () => {
-			cy.get('input[name="caches[0].config.minionConfig.minionTime"]').type('dasas')
-			cy.contains('Leave empty or specify an integer > 0')
-			cy.contains('Fix the errors first!')
-		})
-	})
+        it('Checks minionTimelimit field in Minion', () => {
+            cy.get('input[name="caches[0].config.minionConfig.minionTime"]').type('dasas')
+            cy.contains('Leave empty or specify an integer > 0')
+            cy.contains('Fix the errors first!')
+        })
+    })
+
+    describe('nothing', () => {
+        it('Checks can select answers', () => {
+            cy.visit('/')
+            cy.contains('poop')
+        })
+    })
 })
--- ide/src/server/server.ts
+++ ide/src/server/server.ts
@@ -1,250 +1,217 @@
-import * as path from "path"
-import * as vscode from "vscode"
-import express from "express";
-import fs = require("fs")
-import { sortBy, isEqual, head } from "lodash"
+import * as path from 'path'
+import * as vscode from 'vscode'
+import express from 'express'
+import fs = require('fs')
+import { sortBy, isEqual, head } from 'lodash'
 
-import {
-  Server,
-  Path,
-  GET,
-  POST,
-  PathParam,
-  QueryParam,
-  Errors
-} from "typescript-rest"
+import { Server, Path, GET, POST, PathParam, QueryParam, Errors } from 'typescript-rest'
 
-import * as init from "./init"
-import cors from "cors";
-const fetch = require("node-fetch")
+import * as init from './init'
+import cors from 'cors'
+const fetch = require('node-fetch')
 
-import ConfigHelper, { hasher } from "../extension/src/configHelper"
-import { Cache, RepMap } from "../extension/src/utils";
-import { execSync } from "child_process"
-import { Core } from "../webclient/src/components/vis/TreeContainer"
+import ConfigHelper, { hasher } from '../extension/src/configHelper'
+import { Cache, RepMap } from '../extension/src/utils'
+import { execSync } from 'child_process'
+import { Core } from '../webclient/src/components/vis/TreeContainer'
 
 const collator = new Intl.Collator(undefined, { numeric: true })
 
 interface FilesResponse {
-  models: string[]
-  params: string[]
-  representations: RepMap
+    models: string[]
+    params: string[]
+    representations: RepMap
 }
 
 export interface InitResponse {
-  trees: { core: Core; info: string; path: string; hash: string }[]
-  nimServerPort: number
-  vscodeServerPort: number
+    trees: { core: Core; info: string; path: string; hash: string }[]
+    nimServerPort: number
+    vscodeServerPort: number
 }
 
 let nimServerPort: number
 let thisServerPort: number
 let context: vscode.ExtensionContext
 
-@Path("/")
+@Path('/')
 class HTMlService {
-  @Path("/")
-  @GET
-  getPage(): string {
-    let html =
-      fs
-        .readFileSync(
-          path.join(context.extensionPath, "src/webclient/src/index.html")
-        )
-        .toString() +
-      `
+    @Path('/')
+    @GET
+    getPage(): string {
+        let html =
+            fs.readFileSync(path.join(context.extensionPath, 'src/webclient/src/index.html')).toString() +
+            `
       <div id="port" vscodeServerPort="${thisServerPort}"></div>
-    <script>${fs
-      .readFileSync(path.join(context.extensionPath, "dist/configBundle.js"))
-      .toString()}</script>
+    <script>${fs.readFileSync(path.join(context.extensionPath, 'dist/configBundle.js')).toString()}</script>
   </body>
 </html>
       `
 
-    console.log(html)
-    return html
-  }
+        console.log(html)
+        return html
+    }
 }
 
-@Path("/config")
+@Path('/config')
 class ConfigService {
-  toRel(uri: vscode.Uri): string {
-    return vscode.workspace.asRelativePath(uri.path)
-  }
-
-  @Path("/invalidateCaches")
-  @GET
-  async invalidateCaches() {
-    await ConfigHelper.invalidateCaches()
-  }
-
-  @Path("/caches")
-  @GET
-  async getCaches(): Promise<Cache[]> {
-    let cachedFiles = await vscode.workspace.findFiles(
-      `**/${ConfigHelper.cacheFileName}`
-    )
-
-
-
-    return sortBy(
-      cachedFiles.map(uri => {
-        const file = JSON.parse(fs.readFileSync(uri.path).toString())
-        return {
-          name: path.basename(path.dirname(uri.path)),
-          config: file.config,
-          essenceFile: file.essenceFile,
-          paramFile: file.paramFile
-        }
-      }),
-      ["name"]
-    ).reverse()
-  }
-
-  @Path("/files")
-  @GET
-  async getFiles(): Promise<FilesResponse> {
-    let models = await vscode.workspace.findFiles("**/*.essence")
-    let params = await vscode.workspace.findFiles("**/*.param")
-
-    let reps = models.map(model => {
-      return {
-        name: this.toRel(model),
-        representations: JSON.parse(
-          execSync(
-            `conjure ide ${model.path} --dump-representations`
-          ).toString()
-        )
-      }
-    })
-
-    let map: any = {}
-
-    reps.forEach(r => {
-      map[r.name] = r.representations
-    })
-
-    return {
-      models: models.map(uri => this.toRel(uri)).sort(collator.compare),
-      params: params.map(uri => this.toRel(uri)).sort(collator.compare),
-      representations: map
+    toRel(uri: vscode.Uri): string {
+        return vscode.workspace.asRelativePath(uri.path)
     }
-  }
 
-  @Path("/solve")
-  @POST
-  async startSearch(list: Cache[]): Promise<InitResponse> {
-    console.log("SOLLLLLLLLLLLLLLLLLLLLVE REQUEST", list)
+    @Path('/invalidateCaches')
+    @GET
+    async invalidateCaches() {
+        await ConfigHelper.invalidateCaches()
+    }
 
-    let configsAreTheSame = isEqual(list[0], list[1])
-    // vscode.window.showErrorMessage("Configs are the same! aborting..")
-    // return
+    @Path('/caches')
+    @GET
+    async getCaches(): Promise<Cache[]> {
+        let cachedFiles = await vscode.workspace.findFiles(`**/${ConfigHelper.cacheFileName}`)
 
-    let jobs = configsAreTheSame ? [list[0]] : list
-
-    const { needToGenerate, loadFromCache } = await ConfigHelper.separateJobs(
-      jobs
-    )
-
-    return vscode.window
-      .withProgress(
-        {
-          cancellable: true,
-          location: vscode.ProgressLocation.Notification,
-          title: "Solving "
-        },
-        (progress, token) => {
-          return ConfigHelper.makePromise(needToGenerate, progress, token)
-        }
-      )
-      .then(() => {
-        return vscode.window.withProgress(
-          {
-            cancellable: false,
-            location: vscode.ProgressLocation.Notification,
-            title: "Processing Tree"
-          },
-          async () => {
-            const inits = await Promise.all(
-              list.map(async tree => {
-                const fullPath = path.join(
-                  ConfigHelper.cacheFolderPath,
-                  tree.name
-                )
-
-                const response = await fetch(
-                  `http://localhost:${nimServerPort}/init/${fullPath}`
-                )
-
-                const json = (await response.json()) as {
-                  core: Core
-                  info: string
-                }
-
-                // json["hash"] = hasher(tree.config)
-
-                // json["path"] = fullPath
-                // console.log(json)
-
+        return sortBy(
+            cachedFiles.map((uri) => {
+                const file = JSON.parse(fs.readFileSync(uri.path).toString())
                 return {
-                  hash: hasher(tree.config),
-                  path: fullPath,
-                  ...json
+                    name: path.basename(path.dirname(uri.path)),
+                    config: file.config,
+                    essenceFile: file.essenceFile,
+                    paramFile: file.paramFile
                 }
-              })
-            )
-
-            const response = {
-              trees: inits,
-              nimServerPort: nimServerPort,
-              vscodeServerPort: thisServerPort
-            }
-
-            // console.log(inits)
-            return response
-          }
-        )
-      })
-  }
-}
-
-@Path("/hello")
-class HelloService {
-  @Path(":name")
-  @GET
-  sayHello(@PathParam("name") name: string): string {
-    return "Hello " + name
-  }
-}
-
-@Path("/init")
-class Init {
-  @GET
-  init(@QueryParam("path") path: string) {
-    try {
-      init.findFiles(path)
-    } catch (e) {
-      vscode.window.showErrorMessage(e.message)
-      return new Errors.BadRequestError("Invalid init path: " + e.message)
+            }),
+            [ 'name' ]
+        ).reverse()
     }
-    return "We got this path " + path
-  }
+
+    @Path('/files')
+    @GET
+    async getFiles(): Promise<FilesResponse> {
+        let models = await vscode.workspace.findFiles('**/*.essence')
+        let params = await vscode.workspace.findFiles('**/*.param')
+
+        let reps = models.map((model) => {
+            return {
+                name: this.toRel(model),
+                representations: JSON.parse(execSync(`conjure ide ${model.path} --dump-representations`).toString())
+            }
+        })
+
+        let map: any = {}
+
+        reps.forEach((r) => {
+            map[r.name] = r.representations
+        })
+
+        return {
+            models: models.map((uri) => this.toRel(uri)).sort(collator.compare),
+            params: params.map((uri) => this.toRel(uri)).sort(collator.compare),
+            representations: map
+        }
+    }
+
+    @Path('/solve')
+    @POST
+    async startSearch(list: Cache[]): Promise<InitResponse> {
+        console.log('SOLLLLLLLLLLLLLLLLLLLLVE REQUEST', list)
+
+        let configsAreTheSame = isEqual(list[0], list[1])
+        // vscode.window.showErrorMessage("Configs are the same! aborting..")
+        // return
+
+        let jobs = configsAreTheSame ? [ list[0] ] : list
+
+        const { needToGenerate, loadFromCache } = await ConfigHelper.separateJobs(jobs)
+
+        return vscode.window
+            .withProgress(
+                {
+                    cancellable: true,
+                    location: vscode.ProgressLocation.Notification,
+                    title: 'Solving '
+                },
+                (progress, token) => {
+                    return ConfigHelper.makePromise(needToGenerate, progress, token)
+                }
+            )
+            .then(() => {
+                return vscode.window.withProgress(
+                    {
+                        cancellable: false,
+                        location: vscode.ProgressLocation.Notification,
+                        title: 'Processing Tree'
+                    },
+                    async () => {
+                        const inits = await Promise.all(
+                            list.map(async (tree) => {
+                                const fullPath = path.join(ConfigHelper.cacheFolderPath, tree.name)
+
+                                const response = await fetch(`http://localhost:${nimServerPort}/init/${fullPath}`)
+
+                                const json = (await response.json()) as {
+                                    core: Core
+                                    info: string
+                                }
+
+                                // json["hash"] = hasher(tree.config)
+
+                                // json["path"] = fullPath
+                                // console.log(json)
+
+                                return {
+                                    hash: hasher(tree.config),
+                                    path: fullPath,
+                                    ...json
+                                }
+                            })
+                        )
+
+                        const response = {
+                            trees: inits,
+                            nimServerPort: nimServerPort,
+                            vscodeServerPort: thisServerPort
+                        }
+
+                        // console.log(inits)
+                        return response
+                    }
+                )
+            })
+    }
 }
 
-export function startServer(
-  nsPort: number,
-  vscodeServerPort: number,
-  ctx: vscode.ExtensionContext
-) {
-  nimServerPort = nsPort
-  thisServerPort = vscodeServerPort
-  context = ctx
+@Path('/hello')
+class HelloService {
+    @Path(':name')
+    @GET
+    sayHello(@PathParam('name') name: string): string {
+        return 'Hello ' + name
+    }
+}
 
-  let app: express.Application = express()
-  app.use(cors())
-  Server.buildServices(app)
+@Path('/init')
+class Init {
+    @GET
+    init(@QueryParam('path') path: string) {
+        try {
+            init.findFiles(path)
+        } catch (e) {
+            vscode.window.showErrorMessage(e.message)
+            return new Errors.BadRequestError('Invalid init path: ' + e.message)
+        }
+        return 'We got this path ' + path
+    }
+}
 
-  app.listen(vscodeServerPort, function() {
-    console.log(`Rest Server listening on port ${vscodeServerPort}!`)
-  })
+export function startServer(nsPort: number, vscodeServerPort: number, ctx: vscode.ExtensionContext) {
+    nimServerPort = nsPort
+    thisServerPort = vscodeServerPort
+    context = ctx
+
+    let app: express.Application = express()
+    app.use(cors())
+    Server.buildServices(app)
+
+    app.listen(vscodeServerPort, function() {
+        console.log(`Rest Server listening on port ${vscodeServerPort}!`)
+    })
 }
