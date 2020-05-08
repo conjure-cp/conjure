f3bdde0f77b6b4761a3fed5f4962642bf10d73b7
Özgür Akgün <ozgurakgun@gmail.com>
2020-05-08 12:29:07 +0100

    importing examples 3 & 4 from #465

--- tests/custom/issues/465/2/run.sh
+++ tests/custom/issues/465/2/run.sh
@@ -1,8 +1,5 @@
+conjure parameter-generator ex.essence --MAXINT=20
+cat ex-instanceGenerator.essence
+cat ex-instanceRepair.essence
 
-conjure parameter-generator ex*.essence --MAXINT=20
-cat ex*-instanceGenerator.essence
-cat ex*-instanceRepair.essence
-
-rm -f ex*-*
-
-
+rm -f ex-*
