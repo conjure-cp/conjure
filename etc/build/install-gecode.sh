c3887074d22a991b67dab16ee378ae920ccfee3b
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-04-16 15:26:39 +0100

    Add PROCESSES variable to control paralleisation when building solvers

--- etc/build/install-gecode.sh
+++ etc/build/install-gecode.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o nounset
 
 export BIN_DIR=${BIN_DIR:-${HOME}/.local/bin}
+export PROCESSES=${PROCESSES:-1}
+
 
 rm -rf tmp-install-gecode
 mkdir tmp-install-gecode
@@ -14,7 +16,7 @@ git checkout release-6.0.1
 mkdir build
 cd build
 ../configure --disable-qt --disable-gist --enable-static
-make -j4
+make -j${PROCESSES}
 cp tools/flatzinc/fzn-gecode ${BIN_DIR}/fzn-gecode
 echo "gecode executable is at ${BIN_DIR}/fzn-gecode"
 ls -l ${BIN_DIR}/fzn-gecode
--- etc/build/install-open-wbo.sh
+++ etc/build/install-open-wbo.sh
@@ -4,13 +4,15 @@ set -o errexit
 set -o nounset
 
 export BIN_DIR=${BIN_DIR:-${HOME}/.local/bin}
+export PROCESSES=${PROCESSES:-1}
+
 
 rm -rf tmp-install-open-wbo
 mkdir -p tmp-install-open-wbo
 pushd tmp-install-open-wbo
 git clone https://github.com/sat-group/open-wbo.git
 cd open-wbo
-make -j r
+make -j${PROCESSES} r
 cp open-wbo_release ${BIN_DIR}/open-wbo
 echo "open-wbo executable is at ${BIN_DIR}/open-wbo"
 ls -l ${BIN_DIR}/open-wbo
