8440d45e85771ba41eb7dbe68f8bd9ccb30c9352
Özgür Akgün <ozgurakgun@gmail.com>
2022-10-20 12:22:43 +0100

    making the test case quicker to run and more consistent (by using minion's static search ordering)

--- tests/custom/variant-motif/run.sh
+++ tests/custom/variant-motif/run.sh
@@ -3,9 +3,7 @@ rm -rf conjure-output
 
 GRAPH="zkc"
 
-conjure solve motif.essence data/$GRAPH.param --solver chuffed --output-format=json --copy-solutions=no
-python3 readConjureSolution.py conjure-output/model000001-$GRAPH-solution000001.solution.json > data/$GRAPH.solution
-cp conjure-output/model000001-$GRAPH.eprime-info data/$GRAPH.info
-cat data/$GRAPH.solution
+conjure solve motif.essence data/$GRAPH.param --solver minion --output-format=json --copy-solutions=no
+python3 readConjureSolution.py conjure-output/model000001-$GRAPH-solution000001.solution.json
 
 rm -rf conjure-output
