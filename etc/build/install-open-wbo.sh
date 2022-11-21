a7382e3d99e27d4d7c369a0b2e50ee4fa827e325
Özgür Akgün <ozgurakgun@gmail.com>
2022-11-21 10:41:03 +0000

    Support for building the solvers on macOS

--- etc/build/install-open-wbo.sh
+++ etc/build/install-open-wbo.sh
@@ -9,6 +9,13 @@ set -o nounset
 export BIN_DIR=${BIN_DIR:-${HOME}/.local/bin}
 export PROCESSES=${PROCESSES:-1}
 
+# we will add these to the path, just in case the user is using a homebrew installed gmp
+export C_INCLUDE_PATH=${C_INCLUDE_PATH:-} # initialise if undefined
+export C_INCLUDE_PATH=/opt/homebrew/include:${C_INCLUDE_PATH} # add the brew dir
+export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH:-}
+export CPLUS_INCLUDE_PATH=/opt/homebrew/include:${CPLUS_INCLUDE_PATH}
+export LIBRARY_PATH=${LIBRARY_PATH:-}
+export LIBRARY_PATH=/opt/homebrew/lib/:${LIBRARY_PATH}
 
 rm -rf tmp-install-open-wbo
 mkdir -p tmp-install-open-wbo
