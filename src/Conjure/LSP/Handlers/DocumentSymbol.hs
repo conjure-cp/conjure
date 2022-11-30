b0313676426cffe08cc64213f2ffcac9fee91351
mfb22 <mfb22@st-andrews.ac.uk>
2022-11-30 17:40:12 +0000

    Update

--- src/Conjure/LSP/Handlers/DocumentSymbol.hs
+++ src/Conjure/LSP/Handlers/DocumentSymbol.hs
@@ -5,7 +5,7 @@ import Control.Lens
 import Conjure.Prelude
 import Data.Text (pack,unpack)
 import Conjure.LSP.Util (getProcessedDoc, getRelevantRegions, withProcessedDoc, ProcessedFile (ProcessedFile), regionToRange)
-import Conjure.Language.Validator (RegionInfo (..), ValidatorState (regionInfo), DeclarationType (..))
+import Conjure.Language.Validator (RegionInfo (..), ValidatorState (regionInfo), DeclarationType (..), Kind (..), Class (..))
 import Language.LSP.Types.Lens (HasParams(..), HasTextDocument (textDocument))
 import Conjure.Language (Type(..))
 import Conjure.Language.Type (IntTag(..))
@@ -16,30 +16,44 @@ docSymbolHandler = requestHandler STextDocumentDocumentSymbol $ \ req res -> do
     let ps = req^.params . textDocument
     let u =  (case ps of { (TextDocumentIdentifier uri) -> uri }) :: Uri
     withProcessedDoc ps $ \(ProcessedFile _ _ (regionInfo->ri)) -> do
-        res $ Right $ InR . T.List $ map (regionInfoToDocumentSymbols u) ri
+        res $ Right $ InR . T.List $ mapMaybe (regionInfoToDocumentSymbols u) ri
 
 
 
 
 
-regionInfoToDocumentSymbols ::Uri -> RegionInfo -> SymbolInformation
+regionInfoToDocumentSymbols ::Uri -> RegionInfo -> Maybe SymbolInformation
 regionInfoToDocumentSymbols uri (RegionInfo {rRegion=reg,rText=n, rType=t, rDeclaration=dec}) = SymbolInformation
     (pack $  (unpack n) ++":" ++ show  (pretty t))  --Name
+    <$>
     sk--Kind
+    <*> pure
     (Just (T.List []))
+    <*> pure
     Nothing
-    (Location uri (regionToRange reg)) 
+    <*> pure
+    (Location uri (regionToRange reg))
+    <*> pure
     Nothing
     where 
         sk = case dec of          
-          Definition -> SkVariable
-          LiteralDecl -> case t of 
-            TypeBool -> SkBoolean
-            TypeInt it -> case it of
-              TagInt -> SkNumber
-              TagEnum txt -> SkEnumMember
-              TagUnnamed txt -> SkNumber
-            TypeEnum na -> SkEnum
-            TypeUnnamed na -> SkEnum
-            _ -> SkConstant
-          Ref _ -> SkVariable
+          Definition -> Just $ case t of 
+                    Kind ValueType _ -> SkVariable
+                    Kind DomainType _ -> SkClass
+                    Kind MemberType _ -> SkEnumMember --DO better
+          LiteralDecl -> do
+            case t of 
+              Kind _ ty -> return $ case ty of 
+                TypeBool -> SkBoolean
+                TypeInt it -> case it of
+                  TagInt -> SkNumber
+                  TagEnum txt -> SkEnumMember
+                  TagUnnamed txt -> SkNumber
+                TypeEnum na -> SkEnum
+                TypeUnnamed na -> SkEnum
+                _ -> SkConstant
+             
+          Ref _ -> Just $ case t of 
+                    Kind ValueType _ -> SkVariable
+                    Kind DomainType _ -> SkClass
+                    Kind MemberType _ -> SkEnumMember --DO better
