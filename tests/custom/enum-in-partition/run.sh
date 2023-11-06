a16783c32ddad57438b2ddae8f4e5deddb8c8cff
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-06 11:38:40 +0000

    update test case to also track the json output

--- tests/custom/enum-in-partition/run.sh
+++ tests/custom/enum-in-partition/run.sh
@@ -1,4 +1,4 @@
-rm -rf conjure-output *.solution
-conjure solve eip.essence
-cat *.solution
-rm -rf conjure-output *.solution
+rm -rf conjure-output *.solution *.solution.json
+conjure solve eip.essence --output-format=json
+cat *.solution *.solution.json
+rm -rf conjure-output *.solution *.solution.json
