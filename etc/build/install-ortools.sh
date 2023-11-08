26575f65bd01ec0dd893b855b2b09f38edf3a5b9
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-08 08:07:42 +0000

    debugging

--- etc/build/install-ortools.sh
+++ etc/build/install-ortools.sh
@@ -20,6 +20,7 @@ cd or-tools-9.7
 cmake -S. -Bbuild -DBUILD_DEPS:BOOL=ONere
 cmake --build build
 cp build/bin/fzn-ortools ${BIN_DIR}/fzn-ortools
+ls -l build/lib
 cp build/lib/libortools_flatzinc.9.dylib build/lib/libortools.9.dylib ${BIN_DIR}
 echo "ortools executable is at ${BIN_DIR}/fzn-ortools"
 ls -l ${BIN_DIR}/fzn-ortools
