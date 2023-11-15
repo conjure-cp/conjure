fe5c11a9af56199f50a2fc9be0db15b025dbecf1
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-15 15:00:08 +0000

    using myst_parser instead for including markdown

--- docs/conf.py
+++ docs/conf.py
@@ -31,11 +31,10 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo',
     'sphinxcontrib.bibtex',
     'sphinxcontrib.jquery',
     'nbsphinx',
-    'sphinx_mdinclude'
+    'myst_parser'
 ]
 bibtex_bibfiles = ['refs.bib']
 
