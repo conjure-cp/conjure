bb03cab2169b47cfe85407cae15ab07969753ac2
Özgür Akgün <ozgurakgun@gmail.com>
2023-12-06 16:11:53 +0000

    WIP

--- tests/custom/json-integration/json-enum-in/run.sh
+++ tests/custom/json-integration/json-enum-in/run.sh
@@ -1,7 +1,11 @@
 rm -rf conjure-output
 
-conjure solve m.essence data.json --output-format=json --line-width=40 --copy-solutions=no --savilerow-options -O0 --number-of-solutions=all --output-format=json --solutions-in-one-file
+conjure solve m.essence data.param --output-format=json --line-width=40 --copy-solutions=no --savilerow-options -O0 --number-of-solutions=all --output-format=json --solutions-in-one-file
+cat conjure-output/*.solutions.json
 
+rm -rf conjure-output/*.solutions.json
+
+conjure solve m.essence data.json --output-format=json --line-width=40 --copy-solutions=no --savilerow-options -O0 --number-of-solutions=all --output-format=json --solutions-in-one-file
 cat conjure-output/*.solutions.json
 
 rm -rf conjure-output
