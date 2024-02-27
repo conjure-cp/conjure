07db46c801a46a7020d5a18213fafdd237e0d111
Özgür Akgün <ozgurakgun@gmail.com>
2024-02-27 15:40:54 +0000

    tidy up

--- src/Conjure/Language/ParserC.hs
+++ src/Conjure/Language/ParserC.hs
@@ -1,20 +1,16 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Conjure.Language.ParserC (   
-    parseModel ) where
+module Conjure.Language.ParserC
+  ( parseModel,
+  )
+where
 
--- conjure
-import Conjure.Prelude
-import Conjure.Language.Definition
-
-import qualified Conjure.Language.Validator as V
-import Conjure.Language.AST.Syntax (ProgramTree)
-import Conjure.Language.Parser (Pipeline)
 import Conjure.Language.AST.ASTParser (parseProgram)
-
-
-
-
+import Conjure.Language.AST.Syntax (ProgramTree)
+import Conjure.Language.Definition
+import Conjure.Language.Parser (Pipeline)
+import Conjure.Language.Validator qualified as V
+import Conjure.Prelude
 
 parseModel :: Pipeline ProgramTree Model
-parseModel = (parseProgram,V.validateModel,False)
+parseModel = (parseProgram, V.validateModel, False)
