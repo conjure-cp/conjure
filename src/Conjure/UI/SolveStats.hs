734889aa0a232669b154deb3f298873179bbe8c8
Özgür Akgün <ozgurakgun@gmail.com>
2024-03-09 16:22:53 +0000

    tidier way of storing sr stdout/stderr

--- src/Conjure/UI/SolveStats.hs
+++ src/Conjure/UI/SolveStats.hs
@@ -8,7 +8,7 @@ import Conjure.Bug
 import Conjure.Prelude
 import Conjure.UI (UI (..), versionLine)
 import Data.HashMap.Strict qualified as M -- unordered-containers
-import Data.Text qualified as T (isInfixOf, unlines) -- text
+import Data.Text qualified as T (isInfixOf, null, unlines) -- text
 import Data.Time (UTCTime, getCurrentTime) -- time
 import Network.HostName (getHostName) -- hostname
 import Shelly (run) -- shelly
@@ -27,7 +27,7 @@ data SolveStats = SolveStats
     timestamp :: UTCTime,
     conjureVersion :: String,
     savileRowVersion :: String,
-    savileRowLogs :: M.HashMap String String
+    savileRowLogs :: SavileRowLogs
   }
   deriving (Eq, Ord, Show, Data, Typeable, Generic)
 
@@ -46,16 +46,29 @@ instance ToJSON SolveStatus where toJSON = genericToJSON jsonOptions
 
 instance FromJSON SolveStatus where parseJSON = genericParseJSON jsonOptions
 
+data SavileRowLogs = SavileRowLogs
+  { exitCode :: Int,
+    stderr :: Maybe [String],
+    stdout :: Maybe [String]
+  }
+  deriving (Eq, Ord, Show, Data, Typeable, Generic)
+
+instance Hashable SavileRowLogs
+
+instance ToJSON SavileRowLogs where toJSON = genericToJSON jsonOptions
+
+instance FromJSON SavileRowLogs where parseJSON = genericParseJSON jsonOptions
+
 mkSolveStats :: UI -> (Int, Text, Text) -> String -> IO SolveStats
 mkSolveStats Solve {..} (exitCodeSR, stdoutSR, stderrSR) rawInfo = do
   let combinedSR = T.unlines [stdoutSR, stderrSR]
   let info = M.fromList [(k, v) | [k, v] <- map (splitOn ":") (lines rawInfo)]
       status
+        | T.isInfixOf "java.lang.OutOfMemoryError" combinedSR = MemOut
+        | T.isInfixOf "Savile Row timed out." combinedSR = TimeOut
         | M.lookup "SavileRowTimeOut" info == Just "1" = TimeOut
         | M.lookup "SavileRowClauseOut" info == Just "1" = TimeOut
         | M.lookup "SolverTimeOut" info == Just "1" = TimeOut
-        | T.isInfixOf "Savile Row timed out." combinedSR = TimeOut
-        | T.isInfixOf "java.lang.OutOfMemoryError" combinedSR = MemOut
         | exitCodeSR /= 0 = Error
         | otherwise = OK
       totalTime
@@ -69,10 +82,10 @@ mkSolveStats Solve {..} (exitCodeSR, stdoutSR, stderrSR) rawInfo = do
   let conjureVersion = versionLine
   savileRowVersion <- head . lines . textToString <$> sh (run "savilerow" ["-help"])
   let savileRowLogs =
-        M.fromList
-          [ ("exitCode", show exitCodeSR),
-            ("stdout", textToString stdoutSR),
-            ("stderr", textToString stderrSR)
-          ]
+        SavileRowLogs
+          { exitCode = exitCodeSR,
+            stdout = if T.null stdoutSR then Nothing else Just (lines (textToString stdoutSR)),
+            stderr = if T.null stderrSR then Nothing else Just (lines (textToString stderrSR))
+          }
   return SolveStats {..}
 mkSolveStats _ _ _ = bug "mkSolveStats"
