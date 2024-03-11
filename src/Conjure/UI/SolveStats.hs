6cb0e9795a8efe4ff387070db0e2859a70753e79
Özgür Akgün <ozgurakgun@gmail.com>
2024-03-11 14:54:46 +0000

    catch MiniZinc errors as well

--- src/Conjure/UI/SolveStats.hs
+++ src/Conjure/UI/SolveStats.hs
@@ -65,6 +65,7 @@ mkSolveStats Solve {..} (exitCodeSR, stdoutSR, stderrSR) rawInfo = do
   let info = M.fromList [(k, v) | [k, v] <- map (splitOn ":") (lines rawInfo)]
       status
         | T.isInfixOf "type error: undefined identifier" combinedSR = Error
+        | T.isInfixOf "MiniZinc error" combinedSR = Error
         | T.isInfixOf "java.lang.OutOfMemoryError" combinedSR = MemOut
         | T.isInfixOf "Out of Memory" combinedSR = MemOut
         | T.isInfixOf "Savile Row timed out." combinedSR = TimeOut
