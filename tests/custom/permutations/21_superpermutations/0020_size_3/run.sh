b68c5329defe9178b78a353e8e00d4e8b9d52ada
Chris Jefferson <caj21@st-andrews.ac.uk>
2024-03-16 12:52:50 +0800

    Disable very slow permutation tests for a while

--- tests/custom/permutations/21_superpermutations/0020_size_3/run.sh
+++ tests/custom/permutations/21_superpermutations/0020_size_3/run.sh
@@ -1,3 +1,4 @@
-conjure solve *.essence *.param 
-cat conjure-output/*.solution
-rm -rf conjure-output *.solution
+#conjure solve *.essence *.param 
+#cat conjure-output/*.solution
+#rm -rf conjure-output *.solution
+echo TODO: Speed up test
