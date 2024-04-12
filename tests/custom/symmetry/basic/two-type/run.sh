94be5ba2a83ad012c8aaa010d183e114e819ff19
Chris Jefferson <caj21@st-andrews.ac.uk>
2024-04-12 17:28:04 +0800

    Add symmetry tests

---
+++ tests/custom/symmetry/basic/two-type/run.sh
@@ -0,0 +1,10 @@
+for sym in Quick Complete; do
+    for amount in Consecutive AllPairs AllPermutations; do
+        for combine in Independently Altogether; do
+            echo $sym-$amount-$combine
+            conjure solve --number-of-solutions=all --solutions-in-one-file --output-format=jsonstream --unnamed-symmetry-breaking=$sym-$amount-$combine *.essence 2>&1
+            cat *.json | LC_ALL=C sort
+            rm -rf conjure-output *solutions*
+        done
+    done
+done
