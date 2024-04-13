2b1eb714b24efc09a4f8b130d6b4907329e5bcc4
Özgür Akgün <ozgurakgun@gmail.com>
2024-04-13 23:10:05 +0100

    rm solution json files too

--- tests/custom/symmetry/basic/two-type/run.sh
+++ tests/custom/symmetry/basic/two-type/run.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-rm -rf conjure-output *solutions
+rm -rf conjure-output *solutions*
 
 for sym in Quick Complete; do
     for amount in Consecutive AllPairs AllPermutations; do
@@ -11,7 +11,7 @@ for sym in Quick Complete; do
             echo $sym-$amount-$combine
             conjure solve --number-of-solutions=all --solutions-in-one-file --output-format=jsonstream --unnamed-symmetry-breaking=$sym-$amount-$combine *.essence 2>&1
             cat *.json | LC_ALL=C sort
-            rm -rf conjure-output *solutions
+            rm -rf conjure-output *solutions*
         done
     done
 done
