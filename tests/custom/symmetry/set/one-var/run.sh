23a93188f1d10b297f13feb4448a5ac62a710175
Chris Jefferson <caj21@st-andrews.ac.uk>
2024-04-14 10:27:50 +0800

    Add set tests

---
+++ tests/custom/symmetry/set/one-var/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+rm -rf conjure-output *solutions*
+
+for sym in Complete; do
+    for amount in Consecutive AllPairs AllPermutations; do
+        for combine in Independently Altogether; do
+            echo $sym-$amount-$combine
+            conjure solve --number-of-solutions=all --solutions-in-one-file --output-format=jsonstream --unnamed-symmetry-breaking=$sym-$amount-$combine *.essence 2>&1
+            cat *.json | LC_ALL=C sort
+            rm -rf conjure-output *solutions*
+        done
+    done
+done
