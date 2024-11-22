cb41d3bcfaef6d0c557f80637398656af4c56869
Özgür Akgün <ozgurakgun@gmail.com>
2024-11-22 11:02:16 +0000

    making sure the representations all work and adding some comments

--- tests/custom/permutations/permInverse/13-sets/run.sh
+++ tests/custom/permutations/permInverse/13-sets/run.sh
@@ -1,7 +1,26 @@
 
 rm -rf conjure-output
+conjure solve 00.essence --output-format=json --solutions-in-one-file --number-of-solutions=all --line-width=50 --copy-solutions=no --channelling=no --responses=1,1
+mv conjure-output/model000001.solutions.json 1.sols
 
-conjure solve 00.essence --output-format=json --solutions-in-one-file --number-of-solutions=all --line-width=50 --copy-solutions=no --channelling=no --responses=1,1 
+rm -rf conjure-output
+conjure solve 00.essence --output-format=json --solutions-in-one-file --number-of-solutions=all --line-width=50 --copy-solutions=no --channelling=no --responses=2,2
+mv conjure-output/model000001.solutions.json 2.sols
+
+rm -rf conjure-output
+conjure solve 00.essence --output-format=json --solutions-in-one-file --number-of-solutions=all --line-width=50 --copy-solutions=no --channelling=no --responses=3,3
+mv conjure-output/model000001.solutions.json 3.sols
+
+rm -rf conjure-output
+conjure solve 00.essence --output-format=json --solutions-in-one-file --number-of-solutions=all --line-width=50 --copy-solutions=no --channelling=no --responses=4,4
+cp conjure-output/model000001.solutions.json 4.sols
 
 cat conjure-output/model000001.solutions.json
-rm -rf conjure-output
+diff 1.sols 2.sols
+diff 1.sols 3.sols
+diff 1.sols 4.sols
+diff 2.sols 3.sols
+diff 2.sols 4.sols
+diff 3.sols 4.sols
+
+rm -rf conjure-output *.sols
