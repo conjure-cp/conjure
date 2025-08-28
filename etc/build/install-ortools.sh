f69d1f1e691319158a25b40bf7ec3c380e8d8fa8
Özgür Akgün <ozgurakgun@gmail.com>
2025-08-28 23:34:43 +0100

    Various fixes to solver installation scripts, specifically or-tools, update the allsolvers test, remove open-wbo from the test,

--- etc/build/install-ortools.sh
+++ etc/build/install-ortools.sh
@@ -10,9 +10,10 @@ set -o errexit
 set -o nounset
 
 export BIN_DIR=${BIN_DIR:-${HOME}/.local/bin}
-export LIB_DIR=${LIB_DIR:-${BIN_DIR}/lib}
+export LIB_DIR=${LIB_DIR:-${HOME}/.local/lib}
 export PROCESSES=${PROCESSES:-1}
 
+mkdir -p ${BIN_DIR}
 mkdir -p ${LIB_DIR}
 
 rm -rf tmp-install-ortools
@@ -25,7 +26,7 @@ cmake -S . -B build -DBUILD_DEPS=ON
 cmake --build build --config Release --target all -j${PROCESSES}
 cp build/bin/fzn-cp-sat ${BIN_DIR}/fzn-cp-sat
 # .dylib or .a depending on OS
-cp build/lib*/libortools* ${LIB_DIR}
+cp build/lib*/lib* ${LIB_DIR}
 echo "ortools executable is at ${BIN_DIR}/fzn-cp-sat"
 ls -l ${BIN_DIR}/fzn-cp-sat
 popd
