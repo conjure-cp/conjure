7baeb76199f99177282f01e0970e796e8df9e031
Özgür Akgün <ozgurakgun@gmail.com>
2025-12-18 23:13:36 +0000

    no silence in the CI

--- etc/build/silent-wrapper.sh
+++ etc/build/silent-wrapper.sh
@@ -16,36 +16,41 @@ else
     echo "Running $1"
 fi
 
-PID=$$
-export STARTTIME=$(date +%s)
-bash $1 > make-solvers-${PID}.stdout 2> make-solvers-${PID}.stderr
-EXITCODE=$?
-export ELAPSED=$(($(date +%s) - ${STARTTIME}))
-
-if [ ${EXITCODE} -eq 0 ] ; then
-    echo "        Done (took ${ELAPSED} seconds)"
-    rm -f make-solvers-${PID}.stdout make-solvers-${PID}.stderr
+if ${CI}; then
+    bash $1
 else
-    echo "        Failed (took ${ELAPSED} seconds)"
-    echo "        Exit code: ${EXITCODE}"
-    echo "        Outputs saved to: make-solvers-${PID}.stdout and make-solvers-${PID}.stderr"
-    echo ""
-    if ${CI}; then
-        echo "stdout was:"
-        cat make-solvers-${PID}.stdout
-        echo ""
-        echo "stderr was:"
-        cat make-solvers-${PID}.stderr
-    else
-        echo "Last 10 lines of the stdout was:"
-        tail -n10 make-solvers-${PID}.stdout
-        echo ""
-        echo "Last 10 lines of the stderr was:"
-        tail -n10 make-solvers-${PID}.stderr
-    fi
-    echo ""
-    echo ""
-    echo ""
-fi
 
-exit ${EXITCODE}
+    PID=$$
+    export STARTTIME=$(date +%s)
+    bash $1 > make-solvers-${PID}.stdout 2> make-solvers-${PID}.stderr
+    EXITCODE=$?
+    export ELAPSED=$(($(date +%s) - ${STARTTIME}))
+
+    if [ ${EXITCODE} -eq 0 ] ; then
+        echo "        Done (took ${ELAPSED} seconds)"
+        rm -f make-solvers-${PID}.stdout make-solvers-${PID}.stderr
+    else
+        echo "        Failed (took ${ELAPSED} seconds)"
+        echo "        Exit code: ${EXITCODE}"
+        echo "        Outputs saved to: make-solvers-${PID}.stdout and make-solvers-${PID}.stderr"
+        echo ""
+        if ${CI}; then
+            echo "stdout was:"
+            cat make-solvers-${PID}.stdout
+            echo ""
+            echo "stderr was:"
+            cat make-solvers-${PID}.stderr
+        else
+            echo "Last 10 lines of the stdout was:"
+            tail -n10 make-solvers-${PID}.stdout
+            echo ""
+            echo "Last 10 lines of the stderr was:"
+            tail -n10 make-solvers-${PID}.stderr
+        fi
+        echo ""
+        echo ""
+        echo ""
+    fi
+
+    exit ${EXITCODE}
+fi
