cba515f7871853b3979fbafa7b60e02af74d845b
Özgür Akgün <ozgurakgun@gmail.com>
2025-12-22 00:09:04 +0300

    exit silently if we are not inside a git repository

--- etc/build/copy-conjure-branch.sh
+++ etc/build/copy-conjure-branch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# exit silently if we are not inside a git repository
+if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    exit 0
+fi
+
 export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # make a copy of the executable and call it conjure-${BRANCH}
