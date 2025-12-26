7cc623a7a098429d3046b9d6ae5aba07e63348d0
Özgür Akgün <ozgurakgun@gmail.com>
2025-12-26 13:18:41 +0300

    more fiddle

--- etc/build/install-glucose.sh
+++ etc/build/install-glucose.sh
@@ -18,6 +18,24 @@ pushd tmp-install-glucose
 git clone https://github.com/audemard/glucose.git
 cd glucose
 git checkout $VERSION
+python3 - <<'PY'
+from pathlib import Path
+import re
+
+path = Path("simp/Main.cc")
+text = path.read_text()
+pattern = r"^\\s*_FPU_GETCW\\(oldcw\\);[^\\n]*$"
+replacement = (
+    "#if defined(__i386__) || defined(__x86_64__)\\n"
+    "\\g<0>\\n"
+    "#endif"
+)
+new_text, count = re.subn(pattern, replacement, text, flags=re.M)
+if count > 0:
+    path.write_text(new_text)
+else:
+    raise SystemExit("Expected _FPU_GETCW line not found in simp/Main.cc")
+PY
 echo 'CXXFLAGS += -Wno-class-memaccess -Wno-error=class-memaccess' >> mtl/template.mk
 (
     cd simp
