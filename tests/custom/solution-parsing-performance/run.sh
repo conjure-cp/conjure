42a34797387f6fcb1685e6fb74a3b4e567dd239c
Özgür Akgün <ozgurakgun@gmail.com>
2026-01-29 22:30:07 +0000

    add comment

--- tests/custom/solution-parsing-performance/run.sh
+++ tests/custom/solution-parsing-performance/run.sh
@@ -1,3 +1,4 @@
+# this command went from taking ~50 seconds to taking ~5 seconds after introducing Conjure.Language.ParserCPrime
 rm -rf conjure-output
-conjure solve test.essence --solver=minion --solutions-in-one-file --number-of-solutions=10000 --copy-solutions=no
+conjure solve test.essence --solver=minion --solutions-in-one-file --number-of-solutions=5000 --copy-solutions=no
 rm -rf conjure-output
