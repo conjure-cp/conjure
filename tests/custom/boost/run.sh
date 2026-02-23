ff23b4047486c36bf06f2353f5bd062be7beefb4
Özgür Akgün <ozgurakgun@gmail.com>
2026-02-23 17:02:11 +0000

    consistent ordering across macos and ubuntu

--- tests/custom/boost/run.sh
+++ tests/custom/boost/run.sh
@@ -2,6 +2,6 @@
 
 parallel --no-notice -k --tag --timeout 2 \
     'conjure boost {} > {.}.boost.essence ; conjure pretty {} > {.}.pretty.essence ; ! diff {.}.pretty.essence {.}.boost.essence || rm {.}.boost.essence' \
-    ::: $(find * -name '*.essence' | grep -v 'boost.essence'  | grep -v 'pretty.essence')
+    ::: $(find * -name '*.essence' | grep -v 'boost.essence'  | grep -v 'pretty.essence' | LC_ALL=C sort)
 
 git diff $(find * -name '*.essence')
